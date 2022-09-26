d93d21e9e77ded6cf55ff8397267cf2b2ada98de
Mark Schnitzius <mark.schnitzius@digital.cabinet-office.gov.uk>
2022-09-26 13:55:18 +0100

    Reformatted a file

--- src/handlers/filter/handler.ts
+++ src/handlers/filter/handler.ts
@@ -3,45 +3,45 @@ import AWS from 'aws-sdk';
 
 
 let VALID_EVENT_NAMES = new Set<string>([
-    'IPV_PASSPORT_CRI_REQUEST_SENT',
-     'IPV_PASSPORT_CRI_RESPONSE_RECEIVED',
-     'IPV_FRAUD_CRI_REQUEST_SENT',
-     'IPV_FRAUD_CRI_THIRD_PARTY_REQUEST_ENDED',
-     'IPV_ADDRESS_CRI_REQUEST_SENT',
-     'IPV_ADDRESS_CRI_END',
-     'IPV_KBV_CRI_REQUEST_SENT',
-     'IPV_KBV_CRI_THIRD_PARTY_REQUEST_ENDED'
-    ]);
+  'IPV_PASSPORT_CRI_REQUEST_SENT',
+  'IPV_PASSPORT_CRI_RESPONSE_RECEIVED',
+  'IPV_FRAUD_CRI_REQUEST_SENT',
+  'IPV_FRAUD_CRI_THIRD_PARTY_REQUEST_ENDED',
+  'IPV_ADDRESS_CRI_REQUEST_SENT',
+  'IPV_ADDRESS_CRI_END',
+  'IPV_KBV_CRI_REQUEST_SENT',
+  'IPV_KBV_CRI_THIRD_PARTY_REQUEST_ENDED'
+]);
 
-const sqs = new AWS.SQS({ apiVersion: '2012-11-05' });
+const sqs = new AWS.SQS({apiVersion: '2012-11-05'});
 type Response = { batchItemFailures: { itemIdentifier: string }[] };
 
 
 export const handler = async (event: SQSEvent) => {
 
-  const response: Response = { batchItemFailures: [] };
+  const response: Response = {batchItemFailures: []};
 
   const promises = event.Records
     .filter(record => {
-            const body = JSON.parse(record.body);
-            console.log(body.event_name);
-            return body && body.event_name && VALID_EVENT_NAMES.has(body.event_name);
-        })
+      const body = JSON.parse(record.body);
+      console.log(body.event_name);
+      return body && body.event_name && VALID_EVENT_NAMES.has(body.event_name);
+    })
     .map(async record => {
-        console.log('Got record');
-        try {
-            await doWork(record);
-        } catch (e) {
-            response.batchItemFailures.push({ itemIdentifier: record.messageId });
-        }
+      console.log('Got record');
+      try {
+        await doWork(record);
+      } catch (e) {
+        response.batchItemFailures.push({itemIdentifier: record.messageId});
+      }
     });
 
-    await Promise.all(promises);
-    return response;
+  await Promise.all(promises);
+  return response;
 }
 
 async function doWork(record: SQSRecord) {
-    console.log('sending msg ' + JSON.stringify(record));
+  console.log('sending msg ' + JSON.stringify(record));
 
   const params = {
     MessageBody: JSON.stringify(record),
@@ -49,15 +49,15 @@ async function doWork(record: SQSRecord) {
   };
 
   return new Promise((resolve, reject) => {
-      sqs.sendMessage(params, function (err: any, data: any) {
-        if (err) {
-          console.log(err, err.stack); // an error occurred
-          reject();
-        } else {
-          console.log(data);           // successful response
-          resolve('success');
-        }
-      });
+    sqs.sendMessage(params, function (err: any, data: any) {
+      if (err) {
+        console.log(err, err.stack); // an error occurred
+        reject();
+      } else {
+        console.log(data);           // successful response
+        resolve('success');
+      }
     });
+  });
 }
 
