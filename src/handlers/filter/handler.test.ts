f717f21c9361ad2a5fb5ae17c45a503a8fc3edc6
Mark Schnitzius <mark.schnitzius@digital.cabinet-office.gov.uk>
2022-09-26 13:19:04 +0100

    Cleaned up test

--- src/handlers/filter/handler.test.ts
+++ src/handlers/filter/handler.test.ts
@@ -1,9 +1,7 @@
 const mockSendMessage = jest.fn((params, callback) => { callback() });
 
-import { handler } from './handler'
-import { SQSRecord } from 'aws-lambda'
-
-import AWS from 'aws-sdk'
+import {handler} from './handler'
+import {SQSRecord} from 'aws-lambda'
 
 jest.mock('aws-sdk', () => {
     return {
@@ -13,56 +11,50 @@ jest.mock('aws-sdk', () => {
     };
 });
 
+
 describe('Filter handler tests',() => {
+
     beforeEach(() => {
         mockSendMessage.mockClear();
     })
 
+
     test('Filter handler with empty event batch', async () => {
 
-      const event = {
-        Records: []
-      };
+      const event = createEvent([]);
 
       await handler(event);
 
       expect(mockSendMessage).not.toHaveBeenCalled();
     });
 
-    test('Filter handler with valid type event', async () => {
 
-      const record = createEventWithName('IPV_PASSPORT_CRI_REQUEST_SENT');
+    test('Filter handler with some valid events and some ignored', async () => {
 
-      const event = {
-        Records: [record]
-      };
-
-      await handler(event);
-
-      expect(mockSendMessage).toHaveBeenCalled();
-    });
-
-    test('Filter handler with invalid type event', async () => {
-
-      const record = createEventWithName('SOME_OTHER_EVENT_TYPE');
-
-      const event = {
-        Records: [record]
-      };
+      const validEvent1 = createEventRecordWithName('IPV_PASSPORT_CRI_REQUEST_SENT');
+      const validEvent2 = createEventRecordWithName('IPV_ADDRESS_CRI_REQUEST_SENT');
+      const ignoredEvent = createEventRecordWithName('SOME_IGNORED_EVENT_NAME');
+      const event = createEvent([validEvent1, validEvent2, ignoredEvent]);
 
       await handler(event);
 
-      expect(mockSendMessage).not.toHaveBeenCalled();
+      expect(mockSendMessage).toHaveBeenCalledTimes(2);
     });
 
-    function createEventWithName(name: String) : SQSRecord {
-          const record : SQSRecord = {
+
+    function createEvent(records : Array<SQSRecord>) {
+        return {
+            Records: records
+        };
+    }
+
+    function createEventRecordWithName(name: String) : SQSRecord {
+        return {
               body: JSON.stringify({
-                  timestamp: 1609462861,
+                  timestamp: Date.now(),
                   event_name: name,
                   component_id: 'KBV'
               })
           } as any;
-          return record;
     }
 });
