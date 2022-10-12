b722a54c2d577d2c3641197c369888444157565d
NKannan14 <nithya.kannan@digital.cabinet-office.gov.uk>
2022-10-12 06:07:32 +0100

    fixed E2E test except storage function log group

--- integration_tests/tests.ts
+++ integration_tests/tests.ts
@@ -1,19 +1,25 @@
 import { payload, snsParams, publishSNS } from "./helpers/snsHelper";
-import { getFilteredEventFromLatestLogStream, getLogGroupName } from "./helpers/cloudWatchHelper";
+import {
+  getFilteredEventFromLatestLogStream,
+  getLogGroupName,
+} from "./helpers/cloudWatchHelper";
 import { PublishResponse } from "@aws-sdk/client-sns";
 import { scanDB } from "./helpers/dynamoDBHelper";
+import delay from "delay";
 
 let snsResponse: PublishResponse;
 
-//below E2E tests is marked as skipped as still development in progress
-describe.skip("E2E tests", () => {
+describe("E2E tests", () => {
   beforeAll(async () => {
     snsResponse = await publishSNS(snsParams);
     expect(snsResponse).toHaveProperty("MessageId");
   });
   test("Publish sns message and expect message to reach dynamoDB ", async () => {
+    await delay(2000);
     const data = await scanDB();
-    expect(JSON.stringify(data.Items)).toContain(snsParams.Message);
+    expect(JSON.stringify(data.Items)).toContain(
+      JSON.stringify(payload.event_id)
+    );
   });
 });
 
@@ -25,10 +31,10 @@ describe("Publish SNS event and validate lambda functions triggered", () => {
   test("publish  message and check filter function lambda triggered successfully", async () => {
     expect(snsResponse).toHaveProperty("MessageId");
     const logs = await getFilteredEventFromLatestLogStream(
-     "di-btm-FilterFunction"
-   );
-   expect(JSON.stringify(logs)).not.toContain("ERROR");
-   expect(JSON.stringify(logs)).toContain(payload.eventId.toString());
+      "di-btm-FilterFunction"
+    );
+    expect(JSON.stringify(logs)).not.toContain("ERROR");
+    expect(JSON.stringify(logs)).toContain(payload.event_id.toString());
   });
 
   test("publish  message and check clean function lambda triggered successfully", async () => {
@@ -36,15 +42,14 @@ describe("Publish SNS event and validate lambda functions triggered", () => {
       "di-btm-CleanFunction"
     );
     expect(JSON.stringify(logs)).not.toContain("ERROR");
-    expect(JSON.stringify(logs)).toContain(payload.eventId.toString());
+    expect(JSON.stringify(logs)).toContain(payload.event_id.toString());
   });
 
-  //currently marked this test to skip as there are no log groups for store lambda function
-  test.skip("publish  message and check clean function lambda triggered successfully", async () => {
+  test("publish  message and check clean function lambda triggered successfully", async () => {
     const logs = await getFilteredEventFromLatestLogStream(
-      "di-btm-StoreFunction"
-      );
-      expect(JSON.stringify(logs)).not.toContain("ERROR");
-      expect(JSON.stringify(logs)).toContain(payload.eventId.toString());
-    });
+      "di-btm-StorageFunction"
+    );
+    expect(JSON.stringify(logs)).not.toContain("ERROR");
+    expect(JSON.stringify(logs)).toContain(payload.event_id.toString());
   });
+});
