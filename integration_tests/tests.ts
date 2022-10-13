6eb1d681081f98993289b67a0ec49cbd0848b0ff
gareth.smyth <gareth.smyth@digital.cabinet-office.gov.uk>
2022-10-13 10:35:18 +0100

    BTM-107: Adding polling in scan db.

--- integration_tests/tests.ts
+++ integration_tests/tests.ts
@@ -1,36 +1,36 @@
 import { payload, snsParams, publishSNS } from "./helpers/snsHelper";
-import {
-  getFilteredEventFromLatestLogStream,
-  getLogGroupName,
-} from "./helpers/cloudWatchHelper";
+import {getFilteredEventFromLatestLogStream} from "./helpers/cloudWatchHelper";
 import { PublishResponse } from "@aws-sdk/client-sns";
 import { scanDB } from "./helpers/dynamoDBHelper";
-import { ScanCommandOutput } from "@aws-sdk/client-dynamodb";
 
 let snsResponse: PublishResponse;
-let result: ScanCommandOutput;
-let isEventIdExists: boolean;
+
+function waitForTrue(predicate: Function, delayMS: number, timeoutMS: number){
+  let intervalHandle: any;
+  return new Promise((resultGenerated) => {
+    const complete = (result: boolean) => {
+      clearInterval(intervalHandle);
+      resultGenerated(result);
+    }
+    intervalHandle = setInterval(() => predicate() && complete(true), delayMS);
+    setTimeout(() => complete(false), timeoutMS);
+  });
+}
 
 describe("E2E tests", () => {
   beforeAll(async () => {
     snsResponse = await publishSNS(snsParams);
     expect(snsResponse).toHaveProperty("MessageId");
   });
+
   test("Publish sns message and expect message to reach dynamoDB ", async () => {
-    let checkFlag = false;
-    while (!checkFlag) {
-      result = await scanDB();
-      isEventIdExists = JSON.stringify(result.Items).includes(
-        payload.event_id.toString()
-      );
-      if (isEventIdExists) {
-        expect(JSON.stringify(result.Items)).toContain(payload.event_id);
-      }
-      setTimeout(() => {
-        checkFlag = true;
-      }, 5000);
+    const checkEventId = async () => {
+      const result = await scanDB();
+      return JSON.stringify(result.Items).includes(payload.event_id.toString());
     }
-    expect(isEventIdExists).toBe(true);
+
+    const eventIdExists = await waitForTrue(checkEventId, 1000, 5000);
+    expect(eventIdExists).toBeTruthy();
   });
 });
 
