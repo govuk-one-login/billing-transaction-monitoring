cae643158524957a4592b2e4fb181c953f85037a
Nithya Kannan <112889496+NKannan14@users.noreply.github.com>
2022-10-14 13:55:14 +0100

    Btm 127 negative tests (#60)
    * added negative e2e tests
    
    * added async before function
    
    * extracted sns payload from helper
    
    * added negative test scenarios
    
    * fixed assertion
    
    * fixed assertion
    
    * removed error assertion
    
    * increased timeout
    
    * removed only in test

---
+++ integration_tests/payloads/snsEventPayload.ts
@@ -0,0 +1,68 @@
+
+const generateRandomNumber = () => {
+  return Math.floor(Math.random() * 10000000).toString()
+}
+
+const snsValidEventPayload = {
+    event_name: "EVENT_1",
+    event_id: generateRandomNumber(),
+    component_id: "TEST_COMP",
+    timestamp: new Date().getTime()
+  };
+
+  const snsEventWithAdditionalFieldsPayload = {
+    event_name: "EVENT_1",
+    event_id: generateRandomNumber(),
+    component_id: "TEST_COMP",
+    timestamp: new Date().getTime(),
+    someUnwantedField: "some value",
+  };
+
+  const snsInvalidEventNamePayload = {
+    event_name: "TESTGGHYJKIK",
+    event_id: generateRandomNumber(),
+    component_id: "TEST_COMP",
+    timestamp: new Date().getTime()
+  };
+
+  const snsInvalidTimeStampPayload = {
+    event_name: "EVENT_1",
+    event_id: generateRandomNumber(),
+    component_id: "TEST_COMP",
+    timestamp: "somestring"
+  };
+
+  const snsMissingEventNamePayload = {
+    event_id: generateRandomNumber(),
+    component_id: "TEST_COMP",
+    timestamp: new Date().getTime()
+  };
+
+  const snsEventMissingTimestampPayload = {
+    event_name: "EVENT_1",
+    event_id: generateRandomNumber(),
+    component_id: "TEST_COMP"
+  };
+
+  const snsEventMissingCompIdPayload = {
+    event_name: "EVENT_1",
+    event_id: generateRandomNumber(),
+    timestamp: new Date().getTime()
+  };
+
+  const snsMissingEventIdPayload = {
+    event_name: "EVENT_1",
+    component_id: "TEST_COMP",
+    timestamp: new Date().getTime()
+  };
+
+  const snsEventInvalidCompId = {
+    event_name: "EVENT_1",
+    event_id: generateRandomNumber(),
+    component_id: 5678,
+    timestamp: new Date().getTime()
+  };
+
+  export { snsValidEventPayload,snsInvalidEventNamePayload, snsMissingEventIdPayload,
+    snsEventMissingCompIdPayload,snsEventInvalidCompId,snsInvalidTimeStampPayload,
+    snsEventMissingTimestampPayload,snsEventWithAdditionalFieldsPayload,snsMissingEventNamePayload}
