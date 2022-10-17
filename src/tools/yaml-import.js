21dd5ed7cdb344c21759600b32bbaa4a01c3c69c
Gareth Smyth <110923031+gareth-smyth-gov@users.noreply.github.com>
2022-10-17 09:06:51 +0100

    Btm 105 cfn include fix (#62)
    * BTM-105: Attempting to fix build issue with cfn-include
    
    * BTM-105: Removing cfn-include as it does not support comments which are needed for checkov skips.
    
    * Update src/tools/yaml-import.js
    
    Co-authored-by: gdsgj <110972007+gdsgj@users.noreply.github.com>
    
    * Update src/tools/yaml-import.js
    
    Co-authored-by: gdsgj <110972007+gdsgj@users.noreply.github.com>
    
    Co-authored-by: gdsgj <110972007+gdsgj@users.noreply.github.com>

---
+++ src/tools/yaml-import.js
@@ -0,0 +1,30 @@
+import { Composer, Parser, visit, parseDocument } from 'yaml';
+import { readFileSync, writeFileSync } from 'fs';
+
+const sourceFile = readFileSync('./template-source.yaml', 'utf8');
+let parser = new Parser();
+
+let composer = new Composer();
+let [document] = composer.compose(parser.parse(sourceFile));
+
+let visitor = (key, node, path) => {
+  if (node.tag === '!YAMLInclude') {
+    const files = node.value.split(',');
+    let fullContents;
+    files.forEach(fileName => {
+      const file = readFileSync(`./${fileName.trim()}`, 'utf8');
+      const contents = parseDocument(file).contents;
+      if(!fullContents) {
+        fullContents = contents;
+      } else {
+        fullContents.items.push(...contents.items);
+      }
+    })
+
+    return fullContents;
+  }
+};
+visit(document, visitor)
+
+const outputContent = document.toString();
+writeFileSync('./template.yaml', outputContent);
