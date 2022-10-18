dd3235e2ab291a490a0dae6364393e758b9217b5
NKannan14 <nithya.kannan@digital.cabinet-office.gov.uk>
2022-10-18 13:46:13 +0100

    formatted sns helper

--- integration_tests/helpers/s3Helper.ts
+++ integration_tests/helpers/s3Helper.ts
@@ -1,34 +1,38 @@
-import { s3Client} from "../clients/s3Client";
-import {ListBucketsCommandOutput,ListBucketsCommand, ListObjectsCommand, ListObjectsCommandInput} from "@aws-sdk/client-s3";
-
+import { s3Client } from "../clients/s3Client";
+import {
+  ListBucketsCommandOutput,
+  ListBucketsCommand,
+  ListObjectsCommand,
+  ListObjectsCommandInput,
+} from "@aws-sdk/client-s3";
 
 async function getBucketList() {
   const params = {};
   const response: ListBucketsCommandOutput = await s3Client.send(
     new ListBucketsCommand({})
   );
-  console.log(response.Buckets ?? [])
+  console.log(response.Buckets ?? []);
   return response.Buckets ?? [];
 }
 
 async function getBucketName() {
-  const bucketList = await getBucketList()
-  const bucketName = bucketList.find(item => item.Name?.match("di-btm-storagebucket"))
-   if(bucketName!=null) {
-    console.log(bucketName.Name?.valueOf() as string)
-   return bucketName.Name?.valueOf() as string;
-   }
-   else {
-     throw Error("No matching bucket name found")
-   }
- }
+  const bucketList = await getBucketList();
+  const bucketName = bucketList.find((item) =>
+    item.Name?.match("di-btm-storagebucket")
+  );
+  if (bucketName != null) {
+    console.log(bucketName.Name?.valueOf() as string);
+    return bucketName.Name?.valueOf() as string;
+  } else {
+    throw Error("No matching bucket name found");
+  }
+}
 
- async function getS3ItemsList() {
+async function getS3ItemsList() {
   const bucketParams = {
-    Bucket: await getBucketName()
+    Bucket: await getBucketName(),
   };
-
-  console.log(bucketParams)
+  console.log(bucketParams);
   const data = await s3Client.send(new ListObjectsCommand(bucketParams));
   return data;
 }
