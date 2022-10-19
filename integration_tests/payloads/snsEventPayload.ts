c20005c2455e35aa9f6c6de631bc78c0c61477c3
Nithya Kannan <112889496+NKannan14@users.noreply.github.com>
2022-10-19 13:53:38 +0100

    refactored describe and test statements (#71)

--- integration_tests/payloads/snsEventPayload.ts
+++ integration_tests/payloads/snsEventPayload.ts
@@ -1,68 +1,83 @@
-
 const generateRandomNumber = () => {
-  return Math.floor(Math.random() * 10000000).toString()
-}
+  return Math.floor(Math.random() * 10000000).toString();
+};
 
 const snsValidEventPayload = {
-    event_name: "IPV_PASSPORT_CRI_REQUEST_SENT",
-    event_id: generateRandomNumber(),
-    component_id: "TEST_COMP",
-    timestamp: new Date().getTime()
-  };
+  event_name: "IPV_PASSPORT_CRI_REQUEST_SENT",
+  event_id: generateRandomNumber(),
+  component_id: "TEST_COMP",
+  timestamp: new Date().getTime(),
+};
 
-  const snsEventWithAdditionalFieldsPayload = {
-    event_name: "IPV_PASSPORT_CRI_REQUEST_SENT",
-    event_id: generateRandomNumber(),
-    component_id: "TEST_COMP",
-    timestamp: new Date().getTime(),
-    someUnwantedField: "some value",
-  };
+const snsEventWithAdditionalFieldsPayload = {
+  event_name: "IPV_PASSPORT_CRI_REQUEST_SENT",
+  event_id: generateRandomNumber(),
+  component_id: "TEST_COMP",
+  timestamp: new Date().getTime(),
+  someUnwantedField: "some value",
+};
 
-  const snsInvalidEventNamePayload = {
-    event_name: "TESTGGHYJKIK",
-    event_id: generateRandomNumber(),
-    component_id: "TEST_COMP",
-    timestamp: new Date().getTime()
-  };
+const snsInvalidEventNamePayload = {
+  event_name: "TESTGGHYJKIK",
+  event_id: generateRandomNumber(),
+  component_id: "TEST_COMP",
+  timestamp: new Date().getTime(),
+};
 
-  const snsInvalidTimeStampPayload = {
-    event_name: "IPV_PASSPORT_CRI_REQUEST_SENT",
-    event_id: generateRandomNumber(),
-    component_id: "TEST_COMP",
-    timestamp: "somestring"
-  };
+const snsInvalidTimeStampPayload = {
+  event_name: "IPV_PASSPORT_CRI_REQUEST_SENT",
+  event_id: generateRandomNumber(),
+  component_id: "TEST_COMP",
+  timestamp: "somestring",
+};
 
-  const snsMissingEventNamePayload = {
-    event_id: generateRandomNumber(),
-    component_id: "TEST_COMP",
-    timestamp: new Date().getTime()
-  };
+const snsMissingEventNamePayload = {
+  event_id: generateRandomNumber(),
+  component_id: "TEST_COMP",
+  timestamp: new Date().getTime(),
+};
 
-  const snsEventMissingTimestampPayload = {
-    event_name: "IPV_PASSPORT_CRI_REQUEST_SENT",
-    event_id: generateRandomNumber(),
-    component_id: "TEST_COMP"
-  };
+const snsEventMissingTimestampPayload = {
+  event_name: "IPV_PASSPORT_CRI_REQUEST_SENT",
+  event_id: generateRandomNumber(),
+  component_id: "TEST_COMP",
+};
 
-  const snsEventMissingCompIdPayload = {
-    event_name: "IPV_PASSPORT_CRI_REQUEST_SENT",
-    event_id: generateRandomNumber(),
-    timestamp: new Date().getTime()
-  };
+const snsEventMissingCompIdPayload = {
+  event_name: "IPV_PASSPORT_CRI_REQUEST_SENT",
+  event_id: generateRandomNumber(),
+  timestamp: new Date().getTime(),
+};
 
-  const snsMissingEventIdPayload = {
-    event_name: "IPV_PASSPORT_CRI_REQUEST_SENT",
-    component_id: "TEST_COMP",
-    timestamp: new Date().getTime()
-  };
+const snsMissingEventIdPayload = {
+  event_name: "IPV_PASSPORT_CRI_REQUEST_SENT",
+  component_id: "TEST_COMP",
+  timestamp: new Date().getTime(),
+};
 
-  const snsEventInvalidCompId = {
-    event_name: "IPV_PASSPORT_CRI_REQUEST_SENT",
-    event_id: generateRandomNumber(),
-    component_id: 5678,
-    timestamp: new Date().getTime()
-  };
+const snsEventInvalidCompId = {
+  event_name: "IPV_PASSPORT_CRI_REQUEST_SENT",
+  event_id: generateRandomNumber(),
+  component_id: 5678,
+  timestamp: new Date().getTime(),
+};
 
-  export { snsValidEventPayload,snsInvalidEventNamePayload, snsMissingEventIdPayload,
-    snsEventMissingCompIdPayload,snsEventInvalidCompId,snsInvalidTimeStampPayload,
-    snsEventMissingTimestampPayload,snsEventWithAdditionalFieldsPayload,snsMissingEventNamePayload}
+const snsEventMisisingEventIdValue = {
+  event_name: "IPV_PASSPORT_CRI_REQUEST_SENT",
+  event_id: null,
+  component_id: 5678,
+  timestamp: new Date().getTime(),
+};
+
+export {
+  snsValidEventPayload,
+  snsInvalidEventNamePayload,
+  snsMissingEventIdPayload,
+  snsEventMissingCompIdPayload,
+  snsEventInvalidCompId,
+  snsInvalidTimeStampPayload,
+  snsEventMissingTimestampPayload,
+  snsEventWithAdditionalFieldsPayload,
+  snsMissingEventNamePayload,
+  snsEventMisisingEventIdValue,
+};
