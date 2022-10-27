c2837ce3d9589878dc3a81c0c9a0a69743e9db21
NKannan14 <nithya.kannan@digital.cabinet-office.gov.uk>
2022-10-27 04:25:21 +0100

    added athena query tests

--- integration_tests/tests/athena-query-tests.ts
+++ integration_tests/tests/athena-query-tests.ts
@@ -1,20 +1,40 @@
 import { publishSNS } from "../helpers/snsHelper";
-import { PublishResponse } from "@aws-sdk/client-sns";
-import { startQueryExecutionCommand } from "../helpers/athenaHelper";
-
+import { PublishResponse, SNSClient } from "@aws-sdk/client-sns";
 import {
-  snsValidEventPayload
- 
-} from "../payloads/snsEventPayload";
+  getQueryResults,
+  startQueryExecutionCommand,
+} from "../helpers/athenaHelper";
+import { getS3ItemsList } from "../helpers/s3Helper";
+import { waitForTrue } from "../helpers/commonHelpers";
+import { snsValidEventPayload } from "../payloads/snsEventPayload";
 
 let snsResponse: PublishResponse;
 
-describe("\n Execute provided query in AWS athena\n",() => {
-beforeAll(async () => {
-//snsResponse = await publishSNS(snsValidEventPayload);
-});
-test.only("Query SQL generation and execution successful ", async () => {
-  startQueryExecutionCommand()
+describe("\nPublish valid sns message and excute athena query\n", () => {
+  beforeAll(async () => {
+    snsResponse = await publishSNS(snsValidEventPayload);
+    const checkEventId = async () => {
+      const result = await getS3ItemsList();
+      return JSON.stringify(result.Contents?.map((data) => data.Key)).includes(
+        snsValidEventPayload.event_id
+      );
+    };
+    const eventIdExists = await waitForTrue(checkEventId, 1000, 5000);
+    expect(eventIdExists).toBeTruthy();
+  });
+
+  test("should contain eventId in the generated query results", async () => {
+    await startQueryExecutionCommand(snsValidEventPayload.event_id);
+    const queryResult = await getQueryResults();
+    expect(queryResult).toContain(snsValidEventPayload.event_id);
   });
 });
-  
+
+describe("\nPublish invalid sns message and excute athena query\n", () => {
+  test("should not contain eventId in the generated query results", async () => {
+    const invalidEventId = "12345";
+    await startQueryExecutionCommand(invalidEventId);
+    const queryResult = await getQueryResults();
+    expect(queryResult).not.toContain(invalidEventId);
+  });
+});
