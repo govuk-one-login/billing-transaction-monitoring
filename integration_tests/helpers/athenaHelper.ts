9bc4aee99f1190d09974ef2f8965bab288068cfb
NKannan14 <nithya.kannan@digital.cabinet-office.gov.uk>
2022-10-27 04:24:21 +0100

    added athenaHelper functions

--- integration_tests/helpers/athenaHelper.ts
+++ integration_tests/helpers/athenaHelper.ts
@@ -1,47 +1,77 @@
-
-import { StartQueryExecutionCommand,ListDataCatalogsCommand,GetDatabaseCommand,ListDatabasesCommand } from "@aws-sdk/client-athena";
-  
+import {
+  StartQueryExecutionCommand,
+  ListDatabasesCommand,
+  GetQueryExecutionCommand,
+  GetQueryResultsCommand,
+  ListWorkGroupsCommand,
+} from "@aws-sdk/client-athena";
+import { waitForTrue } from "../helpers/commonHelpers";
 import { athenaClient } from "../clients/athenaClient";
 
+async function listDatabases() {
+  const params = {
+    CatalogName: "AWSDataCatalog",
+  };
+  const response = await athenaClient.send(new ListDatabasesCommand(params));
+  return response.DatabaseList;
+}
 
-  async function listDataCatalogs() {
-   const params ={}
-   const response = await athenaClient.send(new ListDataCatalogsCommand(params));
-  return response
+async function getDatabaseName() {
+  const response = await listDatabases();
+  const getDBName = response?.find((data) => data.Name?.match("btm"));
+  return getDBName?.Name?.valueOf() as string;
+}
+
+async function getWorkGroupName() {
+  const params = {};
+  const response = await athenaClient.send(new ListWorkGroupsCommand(params));
+  const name = response.WorkGroups?.find((data) => data.Name?.match("BTM"));
+  return name?.Name?.valueOf() as string;
+}
+
+let queryId: any;
+async function startQueryExecutionCommand(eventId: any) {
+  const params = {
+    QueryExecutionContext: {
+      Database: await getDatabaseName(),
+    },
+    QueryString: `SELECT * FROM \"btm-transactions\" where event_id='${eventId.toString()}'`,
+    WorkGroup: await getWorkGroupName(),
+  };
+  const response = await athenaClient.send(
+    new StartQueryExecutionCommand(params)
+  );
+  queryId = response.QueryExecutionId;
+  console.log("QueryExecutionId:", queryId);
+  return response.QueryExecutionId;
+}
+
+async function getQueryExecutionStatus() {
+  const params = {
+    QueryExecutionId: queryId,
+  };
+  const response = await athenaClient.send(
+    new GetQueryExecutionCommand(params)
+  );
+  console.log("QueryExecutionStatus:", response.QueryExecution?.Status);
+  return response.QueryExecution?.Status;
+}
+
+async function getQueryResults() {
+  const checkState = async () => {
+    const result = await getQueryExecutionStatus();
+    return result?.State?.match("SUCCEEDED");
+  };
+  const queryStatusSuccess = await waitForTrue(checkState, 5000, 10000);
+  if (queryStatusSuccess == true) {
+    const params = {
+      QueryExecutionId: queryId,
+    };
+    const response = await athenaClient.send(
+      new GetQueryResultsCommand(params)
+    );
+    return JSON.stringify(response.ResultSet?.Rows);
   }
+}
 
-
-  async function listDatabases() {
-    const params ={
-        CatalogName: "btm-transactions"
-
-    }
-    const response = await athenaClient.send(new ListDatabasesCommand(params));
-   return response
-   }
-
-  async function getDatabaseName() {
-    const params ={
-        CatalogName: "",
-        DatabaseName: ""
-    }
-    const response = await athenaClient.send(new GetDatabaseCommand(params));
-   return response
-   }
-
-   async function startQueryExecutionCommand() {
-    const params ={
-      QueryString: `SELECT * FROM "btm-transactions"`,
-      QueryExecutionContext: {
-        Catalog: "AWSDataCatalog",
-        Database: "btm-transactions",
-      },
-  }
-  const response = await athenaClient.send(new StartQueryExecutionCommand(params));
-  console.log(response)
- return response
-   }
-
-   export {startQueryExecutionCommand}
-
- 
+export { getQueryResults, startQueryExecutionCommand };
