42d6d74314ceb68b02f909b1c299379165ab04d9
GJ <110972007+gdsgj@users.noreply.github.com>
2022-10-28 10:19:14 +0100

    Fix tests

--- src/handlers/clean/handler.test.ts
+++ src/handlers/clean/handler.test.ts
@@ -1,5 +1,5 @@
 import { SQSRecord } from "aws-lambda";
-import { SQSHelper } from "../../../test-helpers/SQS";
+import { createEvent } from "../../../test-helpers/SQS";
 import { VALID_EVENT_NAMES } from "../../shared/constants";
 import { sendRecord } from "../../shared/utils";
 import { handler } from "./handler";
@@ -12,230 +12,238 @@ const oldConsoleError = console.error;
 let validRecordBodyObject: Object;
 
 beforeEach(() => {
-    process.env = {...OLD_ENV};
-    mockedSendRecord.mockClear();
-    process.env.AWS_ENV = "aws-env";
-    process.env.OUTPUT_QUEUE_URL = "output-queue-url";
+  process.env = { ...OLD_ENV };
+  mockedSendRecord.mockClear();
+  process.env.AWS_ENV = "aws-env";
+  process.env.OUTPUT_QUEUE_URL = "output-queue-url";
 
-    console.error = jest.fn();
+  console.error = jest.fn();
 
-    validRecordBodyObject = {
-        component_id: "some component ID",
-        event_name: VALID_EVENT_NAMES.values().next().value,
-        timestamp: 123,
-    };
+  validRecordBodyObject = {
+    component_id: "some component ID",
+    event_name: VALID_EVENT_NAMES.values().next().value,
+    timestamp: 123,
+  };
 });
 
 afterAll(() => {
-    process.env = OLD_ENV;
-    console.error = oldConsoleError;
+  process.env = OLD_ENV;
+  console.error = oldConsoleError;
 });
 
 test("Clean handler with empty event batch", async () => {
-    const event = SQSHelper.createEvent([]);
+  const event = createEvent([]);
 
-    const result = await handler(event);
+  const result = await handler(event);
 
-    expect(result).toEqual({
-        batchItemFailures: expect.any(Array),
-    });
-    expect(result.batchItemFailures).toHaveLength(0);
-    expect(mockedSendRecord).not.toHaveBeenCalled();
+  expect(result).toEqual({
+    batchItemFailures: expect.any(Array),
+  });
+  expect(result.batchItemFailures).toHaveLength(0);
+  expect(mockedSendRecord).not.toHaveBeenCalled();
 });
 
 test("Clean handler with event whose records have invalid bodies", async () => {
-    const recordBodyWithInvalidComponentId = JSON.stringify({
-        ...validRecordBodyObject,
-        component_id: 456,
-    });
-    const recordBodyWithInvalidEventName = JSON.stringify({
-        ...validRecordBodyObject,
-        event_name: "some event name string",
-    });
-    const recordBodyWithInvalidTimestamp = JSON.stringify({
-        ...validRecordBodyObject,
-        timestamp: "some timestamp string",
-    });
-    const event = SQSHelper.createEvent([
-        createRecord("", "message ID 1"),
-        createRecord("some non-empty string", "message ID 2"),
-        createRecord(recordBodyWithInvalidComponentId, "message ID 3"),
-        createRecord(recordBodyWithInvalidEventName, "message ID 4"),
-        createRecord(recordBodyWithInvalidTimestamp, "message ID 5"),
-    ]);
+  const recordBodyWithInvalidComponentId = JSON.stringify({
+    ...validRecordBodyObject,
+    component_id: 456,
+  });
+  const recordBodyWithInvalidEventName = JSON.stringify({
+    ...validRecordBodyObject,
+    event_name: "some event name string",
+  });
+  const recordBodyWithInvalidTimestamp = JSON.stringify({
+    ...validRecordBodyObject,
+    timestamp: "some timestamp string",
+  });
+  const event = createEvent([
+    createRecord("", "message ID 1"),
+    createRecord("some non-empty string", "message ID 2"),
+    createRecord(recordBodyWithInvalidComponentId, "message ID 3"),
+    createRecord(recordBodyWithInvalidEventName, "message ID 4"),
+    createRecord(recordBodyWithInvalidTimestamp, "message ID 5"),
+  ]);
 
-    const result = await handler(event);
+  const result = await handler(event);
 
-    expect(result).toEqual({
-        batchItemFailures: expect.any(Array),
-    });
-    expect(result.batchItemFailures).toHaveLength(5);
-    expect(result.batchItemFailures[0]).toEqual({
-        itemIdentifier: "message ID 1",
-    });
-    expect(result.batchItemFailures[1]).toEqual({
-        itemIdentifier: "message ID 2",
-    });
-    expect(result.batchItemFailures[2]).toEqual({
-        itemIdentifier: "message ID 3",
-    });
-    expect(result.batchItemFailures[3]).toEqual({
-        itemIdentifier: "message ID 4",
-    });
-    expect(result.batchItemFailures[4]).toEqual({
-        itemIdentifier: "message ID 5",
-    });
-    expect(mockedSendRecord).not.toHaveBeenCalled();
+  expect(result).toEqual({
+    batchItemFailures: expect.any(Array),
+  });
+  expect(result.batchItemFailures).toHaveLength(5);
+  expect(result.batchItemFailures[0]).toEqual({
+    itemIdentifier: "message ID 1",
+  });
+  expect(result.batchItemFailures[1]).toEqual({
+    itemIdentifier: "message ID 2",
+  });
+  expect(result.batchItemFailures[2]).toEqual({
+    itemIdentifier: "message ID 3",
+  });
+  expect(result.batchItemFailures[3]).toEqual({
+    itemIdentifier: "message ID 4",
+  });
+  expect(result.batchItemFailures[4]).toEqual({
+    itemIdentifier: "message ID 5",
+  });
+  expect(mockedSendRecord).not.toHaveBeenCalled();
 });
 
 test("Clean handler with minimal valid event record", async () => {
-    const recordBody = JSON.stringify(validRecordBodyObject);
-    const record = createRecord(recordBody);
-    const event = SQSHelper.createEvent([record]);
+  const recordBody = JSON.stringify(validRecordBodyObject);
+  const record = createRecord(recordBody);
+  const event = createEvent([record]);
 
-    expect(mockedSendRecord).not.toHaveBeenCalled();
+  expect(mockedSendRecord).not.toHaveBeenCalled();
 
-    const result = await handler(event);
+  const result = await handler(event);
 
-    expect(result).toEqual({
-        batchItemFailures: expect.any(Array),
-    });
-    expect(result.batchItemFailures).toHaveLength(0);
-    expect(mockedSendRecord).toHaveBeenCalledTimes(1);
-    expect(mockedSendRecord).toHaveBeenCalledWith(process.env.OUTPUT_QUEUE_URL, recordBody);
+  expect(result).toEqual({
+    batchItemFailures: expect.any(Array),
+  });
+  expect(result.batchItemFailures).toHaveLength(0);
+  expect(mockedSendRecord).toHaveBeenCalledTimes(1);
+  expect(mockedSendRecord).toHaveBeenCalledWith(
+    process.env.OUTPUT_QUEUE_URL,
+    recordBody
+  );
 });
 
 test("Clean handler with valid event record that has unwanted field", async () => {
-    const recordBody = JSON.stringify({
-        ...validRecordBodyObject,
-        someUnwantedField: "some value",
-    });
-    const record = createRecord(recordBody);
-    const event = SQSHelper.createEvent([record]);
+  const recordBody = JSON.stringify({
+    ...validRecordBodyObject,
+    someUnwantedField: "some value",
+  });
+  const record = createRecord(recordBody);
+  const event = createEvent([record]);
 
-    expect(mockedSendRecord).not.toHaveBeenCalled();
+  expect(mockedSendRecord).not.toHaveBeenCalled();
 
-    const result = await handler(event);
+  const result = await handler(event);
 
-    expect(result).toEqual({
-        batchItemFailures: expect.any(Array),
-    });
-    expect(result.batchItemFailures).toHaveLength(0);
-    expect(mockedSendRecord).toHaveBeenCalledTimes(1);
-    expect(mockedSendRecord).toHaveBeenCalledWith(process.env.OUTPUT_QUEUE_URL, JSON.stringify(validRecordBodyObject));
+  expect(result).toEqual({
+    batchItemFailures: expect.any(Array),
+  });
+  expect(result.batchItemFailures).toHaveLength(0);
+  expect(mockedSendRecord).toHaveBeenCalledTimes(1);
+  expect(mockedSendRecord).toHaveBeenCalledWith(
+    process.env.OUTPUT_QUEUE_URL,
+    JSON.stringify(validRecordBodyObject)
+  );
 });
 
 test("Clean handler with valid event record that has invalid optional values", async () => {
-    const recordBody = JSON.stringify({
-        ...validRecordBodyObject,
-        client_id: 123,
-        event_id: 234,
-        extensions: "some string",
-        timestamp_formatted: 345,
-        user: {
-            transaction_id: 456,
-        },
-    });
-    const record = createRecord(recordBody);
-    const event = SQSHelper.createEvent([record]);
+  const recordBody = JSON.stringify({
+    ...validRecordBodyObject,
+    client_id: 123,
+    event_id: 234,
+    extensions: "some string",
+    timestamp_formatted: 345,
+    user: {
+      transaction_id: 456,
+    },
+  });
+  const record = createRecord(recordBody);
+  const event = createEvent([record]);
 
-    expect(mockedSendRecord).not.toHaveBeenCalled();
+  expect(mockedSendRecord).not.toHaveBeenCalled();
 
-    const result = await handler(event);
+  const result = await handler(event);
 
-    expect(result).toEqual({
-        batchItemFailures: expect.any(Array),
-    });
-    expect(result.batchItemFailures).toHaveLength(0);
-    expect(mockedSendRecord).toHaveBeenCalledTimes(1);
-    expect(mockedSendRecord).toHaveBeenCalledTimes(1);
-    expect(mockedSendRecord).toHaveBeenCalledWith(process.env.OUTPUT_QUEUE_URL,
-        JSON.stringify({...validRecordBodyObject, user: {}}));
+  expect(result).toEqual({
+    batchItemFailures: expect.any(Array),
+  });
+  expect(result.batchItemFailures).toHaveLength(0);
+  expect(mockedSendRecord).toHaveBeenCalledTimes(1);
+  expect(mockedSendRecord).toHaveBeenCalledTimes(1);
+  expect(mockedSendRecord).toHaveBeenCalledWith(
+    process.env.OUTPUT_QUEUE_URL,
+    JSON.stringify({ ...validRecordBodyObject, user: {} })
+  );
 });
 
 test("Clean handler with valid event record that has valid optional values", async () => {
-    const recordBodyObject = {
-        ...validRecordBodyObject,
-        client_id: "some client ID",
-        event_id: "some event ID",
-        extensions: {
-            iss: "some ISS",
-        },
-        timestamp_formatted: "some formatted timestamp",
-        user: {
-            transaction_id: "some transaction ID",
-        },
-    };
-    const recordBody = JSON.stringify(recordBodyObject);
-    const record = createRecord(recordBody);
-    const event = SQSHelper.createEvent([record]);
+  const recordBodyObject = {
+    ...validRecordBodyObject,
+    client_id: "some client ID",
+    event_id: "some event ID",
+    extensions: {
+      iss: "some ISS",
+    },
+    timestamp_formatted: "some formatted timestamp",
+    user: {
+      transaction_id: "some transaction ID",
+    },
+  };
+  const recordBody = JSON.stringify(recordBodyObject);
+  const record = createRecord(recordBody);
+  const event = createEvent([record]);
 
-    expect(mockedSendRecord).not.toHaveBeenCalled();
+  expect(mockedSendRecord).not.toHaveBeenCalled();
 
-    const result = await handler(event);
+  const result = await handler(event);
 
-    expect(result).toEqual({
-        batchItemFailures: expect.any(Array),
-    });
-    expect(result.batchItemFailures).toHaveLength(0);
-    expect(mockedSendRecord).toHaveBeenCalledTimes(1);
-    const resultSendRecordArguments = mockedSendRecord.mock.calls[0];
-    expect(resultSendRecordArguments).toHaveLength(2);
-    const resultRecordBodyObject = JSON.parse(resultSendRecordArguments[1]);
-    expect(resultRecordBodyObject).toEqual(recordBodyObject);
-    expect(resultSendRecordArguments[0]).toBe(process.env.OUTPUT_QUEUE_URL);
+  expect(result).toEqual({
+    batchItemFailures: expect.any(Array),
+  });
+  expect(result.batchItemFailures).toHaveLength(0);
+  expect(mockedSendRecord).toHaveBeenCalledTimes(1);
+  const resultSendRecordArguments = mockedSendRecord.mock.calls[0];
+  expect(resultSendRecordArguments).toHaveLength(2);
+  const resultRecordBodyObject = JSON.parse(resultSendRecordArguments[1]);
+  expect(resultRecordBodyObject).toEqual(recordBodyObject);
+  expect(resultSendRecordArguments[0]).toBe(process.env.OUTPUT_QUEUE_URL);
 });
 
 test("Clean handler with valid event record that has extensions but no iss", async () => {
-    const recordBodyObject = {
-        ...validRecordBodyObject,
-        extensions: {
-          // no iss field here
-        }
-    };
-    const recordBody = JSON.stringify(recordBodyObject);
-    const record = createRecord(recordBody);
-    const event = SQSHelper.createEvent([record]);
+  const recordBodyObject = {
+    ...validRecordBodyObject,
+    extensions: {
+      // no iss field here
+    },
+  };
+  const recordBody = JSON.stringify(recordBodyObject);
+  const record = createRecord(recordBody);
+  const event = createEvent([record]);
 
-    expect(mockedSendRecord).not.toHaveBeenCalled();
+  expect(mockedSendRecord).not.toHaveBeenCalled();
 
-    const result = await handler(event);
+  const result = await handler(event);
 
-    expect(result).toEqual({
-        batchItemFailures: expect.any(Array),
-    });
-    expect(result.batchItemFailures).toHaveLength(0);
-    expect(mockedSendRecord).toHaveBeenCalledTimes(1);
-    const resultSendRecordArguments = mockedSendRecord.mock.calls[0];
-    expect(resultSendRecordArguments).toHaveLength(2);
-    const resultRecordBodyObject = JSON.parse(resultSendRecordArguments[1]);
-    expect(resultRecordBodyObject).toEqual(recordBodyObject);
-    expect(resultSendRecordArguments[0]).toBe(process.env.OUTPUT_QUEUE_URL);
+  expect(result).toEqual({
+    batchItemFailures: expect.any(Array),
+  });
+  expect(result.batchItemFailures).toHaveLength(0);
+  expect(mockedSendRecord).toHaveBeenCalledTimes(1);
+  const resultSendRecordArguments = mockedSendRecord.mock.calls[0];
+  expect(resultSendRecordArguments).toHaveLength(2);
+  const resultRecordBodyObject = JSON.parse(resultSendRecordArguments[1]);
+  expect(resultRecordBodyObject).toEqual(recordBodyObject);
+  expect(resultSendRecordArguments[0]).toBe(process.env.OUTPUT_QUEUE_URL);
 });
 
 test("SQS output queue not defined", async () => {
-    process.env.OUTPUT_QUEUE_URL = undefined;
+  process.env.OUTPUT_QUEUE_URL = undefined;
 
-    const messageId = "some message ID";
-    const recordBody = JSON.stringify(validRecordBodyObject);
-    const record = createRecord(recordBody, messageId);
-    const event = SQSHelper.createEvent([record]);
+  const messageId = "some message ID";
+  const recordBody = JSON.stringify(validRecordBodyObject);
+  const record = createRecord(recordBody, messageId);
+  const event = createEvent([record]);
 
-    const result = await handler(event);
+  const result = await handler(event);
 
-    expect(result).toEqual({
-        batchItemFailures: expect.any(Array),
-    });
-    expect(result.batchItemFailures).toHaveLength(1);
-    expect(result.batchItemFailures[0]).toEqual({
-        itemIdentifier: messageId,
-    });
-    expect(mockedSendRecord).not.toHaveBeenCalled();
+  expect(result).toEqual({
+    batchItemFailures: expect.any(Array),
+  });
+  expect(result.batchItemFailures).toHaveLength(1);
+  expect(result.batchItemFailures[0]).toEqual({
+    itemIdentifier: messageId,
+  });
+  expect(mockedSendRecord).not.toHaveBeenCalled();
 });
 
 function createRecord(body: String, messageId?: string): SQSRecord {
-    return {
-        body,
-        messageId
-    } as any;
+  return {
+    body,
+    messageId,
+  } as any;
 }
--- src/handlers/filter/handler.test.ts
+++ src/handlers/filter/handler.test.ts
@@ -1,82 +1,117 @@
-import {handler} from './handler'
-import {SQSHelper} from '../../../test-helpers/SQS'
-import {sendRecord} from '../../shared/utils'
+import { handler } from "./handler";
+import {
+  createEvent,
+  createEventRecordWithName,
+} from "../../../test-helpers/SQS";
+import { sendRecord } from "../../shared/utils";
 
-jest.mock('../../shared/utils');
-const mockedSendRecord = sendRecord as jest.MockedFunction<typeof sendRecord>
+jest.mock("../../shared/utils");
+const mockedSendRecord = sendRecord as jest.MockedFunction<typeof sendRecord>;
 
-describe('Filter handler tests', () => {
-
+describe("Filter handler tests", () => {
   const OLD_ENV = process.env;
+  const oldConsoleError = console.error;
+  const oldConsoleLog = console.log;
 
   beforeEach(() => {
     process.env = { ...OLD_ENV };
+    console.error = jest.fn();
+    console.log = jest.fn();
     mockedSendRecord.mockClear();
-    process.env.OUTPUT_QUEUE_URL = 'output-queue-url';
-  })
+    process.env.OUTPUT_QUEUE_URL = "output-queue-url";
+  });
 
   afterAll(() => {
     process.env = OLD_ENV;
+    console.error = oldConsoleError;
+    console.log = oldConsoleLog;
   });
 
-
-  test('Filter handler with empty event batch', async () => {
-
-    const event = SQSHelper.createEvent([]);
+  test("Filter handler with empty event batch", async () => {
+    const event = createEvent([]);
 
     await handler(event);
 
     expect(mockedSendRecord).not.toHaveBeenCalled();
   });
 
-
-  test('Filter handler with some valid events and some ignored', async () => {
-
-    const validRecord1 = SQSHelper.createEventRecordWithName('IPV_PASSPORT_CRI_REQUEST_SENT', 1);
-    const validRecord2 = SQSHelper.createEventRecordWithName('IPV_ADDRESS_CRI_REQUEST_SENT', 2);
-    const ignoredRecord = SQSHelper.createEventRecordWithName('SOME_IGNORED_EVENT_NAME', 3);
-    const event = SQSHelper.createEvent([validRecord1, validRecord2, ignoredRecord]);
+  test("Filter handler with some valid events and some ignored", async () => {
+    const validRecord1 = createEventRecordWithName(
+      "IPV_PASSPORT_CRI_REQUEST_SENT",
+      1
+    );
+    const validRecord2 = createEventRecordWithName(
+      "IPV_ADDRESS_CRI_REQUEST_SENT",
+      2
+    );
+    const ignoredRecord = createEventRecordWithName(
+      "SOME_IGNORED_EVENT_NAME",
+      3
+    );
+    const event = createEvent([validRecord1, validRecord2, ignoredRecord]);
 
     await handler(event);
 
     expect(mockedSendRecord).toHaveBeenCalledTimes(2);
-    expect(mockedSendRecord).toHaveBeenNthCalledWith(1, 'output-queue-url', validRecord1.body);
-    expect(mockedSendRecord).toHaveBeenNthCalledWith(2,'output-queue-url', validRecord2.body);
+    expect(mockedSendRecord).toHaveBeenNthCalledWith(
+      1,
+      "output-queue-url",
+      validRecord1.body
+    );
+    expect(mockedSendRecord).toHaveBeenNthCalledWith(
+      2,
+      "output-queue-url",
+      validRecord2.body
+    );
   });
 
-
-  test('SQS output queue not defined', async () => {
-
+  test("SQS output queue not defined", async () => {
     process.env.OUTPUT_QUEUE_URL = undefined;
 
-    const validRecord = SQSHelper.createEventRecordWithName('IPV_PASSPORT_CRI_REQUEST_SENT', 1);
+    const validRecord = createEventRecordWithName(
+      "IPV_PASSPORT_CRI_REQUEST_SENT",
+      1
+    );
 
-    const event = SQSHelper.createEvent([validRecord]);
+    const event = createEvent([validRecord]);
 
     const result = await handler(event);
     expect(result.batchItemFailures.length).toEqual(1);
-    expect(result.batchItemFailures[0].itemIdentifier).toEqual(1);
+    expect(result.batchItemFailures[0].itemIdentifier).toEqual("1");
   });
 
+  test("Failing send message", async () => {
+    const validRecord = createEventRecordWithName(
+      "IPV_PASSPORT_CRI_REQUEST_SENT",
+      1
+    );
+    const invalidRecord = createEventRecordWithName(
+      "IPV_ADDRESS_CRI_REQUEST_SENT",
+      2
+    );
 
-  test('Failing send message', async () => {
-    const validRecord = SQSHelper.createEventRecordWithName('IPV_PASSPORT_CRI_REQUEST_SENT',1);
-    const invalidRecord = SQSHelper.createEventRecordWithName('IPV_ADDRESS_CRI_REQUEST_SENT', 2);
-
-    const event = SQSHelper.createEvent([validRecord, invalidRecord]);
+    const event = createEvent([validRecord, invalidRecord]);
 
     mockedSendRecord
-        .mockImplementationOnce(async () => {})
-        .mockImplementationOnce(async () => {
-          throw new Error('error');
-        });
+      .mockImplementationOnce(async () => {})
+      .mockImplementationOnce(async () => {
+        throw new Error("error");
+      });
 
     const result = await handler(event);
 
     expect(mockedSendRecord).toHaveBeenCalledTimes(2);
-    expect(mockedSendRecord).toHaveBeenNthCalledWith(1, 'output-queue-url', validRecord.body);
-    expect(mockedSendRecord).toHaveBeenNthCalledWith(2,'output-queue-url', invalidRecord.body);
+    expect(mockedSendRecord).toHaveBeenNthCalledWith(
+      1,
+      "output-queue-url",
+      validRecord.body
+    );
+    expect(mockedSendRecord).toHaveBeenNthCalledWith(
+      2,
+      "output-queue-url",
+      invalidRecord.body
+    );
     expect(result.batchItemFailures.length).toEqual(1);
-    expect(result.batchItemFailures[0].itemIdentifier).toEqual(2);
+    expect(result.batchItemFailures[0].itemIdentifier).toEqual("2");
   });
 });
