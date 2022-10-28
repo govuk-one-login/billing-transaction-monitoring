baf8850ea9715eb7449865cd00119a974b9dadea
gdsgj <110972007+gdsgj@users.noreply.github.com>
2022-10-28 08:50:08 +0100

    Add VAT (#84)

--- src/shared/utils/s3.test.ts
+++ src/shared/utils/s3.test.ts
@@ -1,16 +1,21 @@
-import {mockClient} from "aws-sdk-client-mock";
-import {S3Client, PutObjectCommand} from "@aws-sdk/client-s3";
-import {putS3} from "./s3";
+import { mockClient } from "aws-sdk-client-mock";
+import {
+  S3Client,
+  PutObjectCommand,
+  DeleteObjectCommand,
+} from "@aws-sdk/client-s3";
+import { deleteS3, putS3 } from "./s3";
 
-const s3Mock = mockClient(S3Client);
+let s3Mock: ReturnType<typeof mockClient>;
 
 const oldConsoleLog = console.log;
 const bucket = "given-bucket-name";
 const key = "given/key";
-const record = {record: "given record"};
+const record = { record: "given record" };
 
 beforeEach(() => {
   console.log = jest.fn();
+  s3Mock = mockClient(S3Client);
 });
 
 afterAll(() => {
@@ -18,9 +23,11 @@ afterAll(() => {
 });
 
 test("Put object with callback error", async () => {
-  s3Mock.on(PutObjectCommand).rejects('An error');
+  s3Mock.on(PutObjectCommand).rejects("An error");
 
-  await expect(putS3(bucket, key, record)).rejects.toMatchObject({message: 'An error'});
+  await expect(putS3(bucket, key, record)).rejects.toMatchObject({
+    message: "An error",
+  });
   expect(s3Mock.calls()[0].firstArg.input).toEqual({
     Body: JSON.stringify(record),
     Key: key,
@@ -35,6 +42,29 @@ test("Put object without callback error", async () => {
 
   expect(s3Mock.calls()[0].firstArg.input).toEqual({
     Body: JSON.stringify(record),
+    Key: key,
+    Bucket: bucket,
+  });
+});
+
+test("Delete object with callback error", async () => {
+  s3Mock.on(DeleteObjectCommand).rejects("An error");
+
+  await expect(deleteS3(bucket, key)).rejects.toMatchObject({
+    message: "An error",
+  });
+  expect(s3Mock.calls()[0].firstArg.input).toEqual({
+    Key: key,
+    Bucket: bucket,
+  });
+});
+
+test("Delete object without callback error", async () => {
+  s3Mock.on(DeleteObjectCommand).resolves({});
+
+  await deleteS3(bucket, key);
+
+  expect(s3Mock.calls()[0].firstArg.input).toEqual({
     Key: key,
     Bucket: bucket,
   });
