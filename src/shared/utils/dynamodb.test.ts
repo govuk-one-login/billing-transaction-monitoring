681bd267110f79fb2f82bc0d4d1601d20d0e29cd
gdsgj <110972007+gdsgj@users.noreply.github.com>
2022-10-28 14:13:13 +0100

    Fix tests (#87)

--- src/shared/utils/dynamodb.test.ts
+++ src/shared/utils/dynamodb.test.ts
@@ -1,29 +1,40 @@
-import {mockClient} from "aws-sdk-client-mock";
+import { mockClient } from "aws-sdk-client-mock";
 import { DynamoDBDocumentClient, PutCommand } from "@aws-sdk/lib-dynamodb";
-import {putDDB} from "./dynamodb";
+import { putDDB } from "./dynamodb";
 
 const dynamoDBDocumentMock = mockClient(DynamoDBDocumentClient);
 
-const tableName = 'A Table';
+const oldConsoleLog = console.log;
+const tableName = "A Table";
 const item = { id: 1234 };
 
-test('log message when error happens', async () => {
-    dynamoDBDocumentMock.on(PutCommand).rejects('An error');
-
-    await expect(putDDB(tableName, item)).rejects.toMatchObject({message: 'An error'});
-    expect(dynamoDBDocumentMock.calls()[0].firstArg.input).toEqual({
-        TableName: tableName,
-        Item: item,
-    });
+beforeEach(() => {
+  console.log = jest.fn();
 });
 
-test('Handle successful put', async () => {
-    dynamoDBDocumentMock.on(PutCommand).resolves({});
+afterAll(() => {
+  console.log = oldConsoleLog;
+});
 
-    await putDDB(tableName, item);
+test("log message when error happens", async () => {
+  dynamoDBDocumentMock.on(PutCommand).rejects("An error");
 
-    expect(dynamoDBDocumentMock.calls()[0].firstArg.input).toEqual({
-        TableName: tableName,
-        Item: item,
-    });
+  await expect(putDDB(tableName, item)).rejects.toMatchObject({
+    message: "An error",
+  });
+  expect(dynamoDBDocumentMock.calls()[0].firstArg.input).toEqual({
+    TableName: tableName,
+    Item: item,
+  });
+});
+
+test("Handle successful put", async () => {
+  dynamoDBDocumentMock.on(PutCommand).resolves({});
+
+  await putDDB(tableName, item);
+
+  expect(dynamoDBDocumentMock.calls()[0].firstArg.input).toEqual({
+    TableName: tableName,
+    Item: item,
+  });
 });
--- test-helpers/SQS.ts
+++ test-helpers/SQS.ts
@@ -1,20 +1,18 @@
-import {SQSRecord} from "aws-lambda";
+import { SQSEvent, SQSRecord } from "aws-lambda";
 
-export class SQSHelper {
-    static createEvent(records: Array<SQSRecord>) {
-        return {
-            Records: records
-        };
-    }
+export const createEvent = (records: SQSRecord[]): SQSEvent => ({
+  Records: records,
+});
 
-    static createEventRecordWithName(name: String, messageId: Number): SQSRecord {
-        return {
-            body: JSON.stringify({
-                event_name: name,
-                timestamp: Date.now(),
-                event_id: Math.floor(Math.random() * 100000)
-            }),
-            messageId,
-        } as any;
-    }
-}
+export const createEventRecordWithName = (
+  name: String,
+  messageId: Number
+): SQSRecord =>
+  ({
+    body: JSON.stringify({
+      event_name: name,
+      timestamp: Date.now(),
+      event_id: String(Math.floor(Math.random() * 100000)),
+    }),
+    messageId: String(messageId),
+  } as any);
