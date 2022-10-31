3dddba058e807b5915eff1ce2cd40051cfba9dfd
NKannan14 <nithya.kannan@digital.cabinet-office.gov.uk>
2022-10-31 03:26:46 +0000

    added test for vat config

--- integration_tests/tests/s3-vat-tests.ts
+++ integration_tests/tests/s3-vat-tests.ts
@@ -1,9 +1,16 @@
-import { getS3ItemsList } from "../helpers/s3Helper";
+import { getS3ItemsList, getS3Object } from "../helpers/s3Helper";
+import { default as expectedVat } from "../../cloudformation/uk-vat.json";
 
-
-describe( "\n Verify VAT details exists in S3 config bucket\n",() => {
-    test("S3 config bucket should contain VAT details", async () => {
-        const result = await getS3ItemsList("di-btm-configbucket");
-        console.log(result)
-    });
+describe("\n Verify VAT details exists in S3 config bucket\n", () => {
+  test.only("S3 config bucket should contain VAT details matches with expected vat config file ", async () => {
+    const bucketNameMatchString = "di-btm-configbucket";
+    const result = await getS3ItemsList(bucketNameMatchString);
+    const bucketKey = result.Contents?.find((x) => x.Key?.match("vat"))
+      ?.Key?.valueOf()
+      .toString();
+    const response = await getS3Object(bucketNameMatchString, bucketKey);
+    expect(response?.replaceAll('"', "")).toEqual(
+      JSON.stringify(expectedVat).replaceAll('"', "")
+    );
+  });
 });
