9799decb92a13015d772abc8eeedb190e2019035
GJ <110972007+gdsgj@users.noreply.github.com>
2022-11-01 11:32:19 +0000

    Fix VAT JSON bug

--- src/tools/yaml-import.js
+++ src/tools/yaml-import.js
@@ -2,33 +2,39 @@
  * This script pulls together the different parts of the template.yaml SAM file.
  */
 
-import { Composer, Parser, visit, parseDocument } from 'yaml';
-import { readFileSync, writeFileSync } from 'fs';
+import { Composer, Parser, Scalar, visit, parseDocument } from "yaml";
+import { readFileSync, writeFileSync } from "fs";
 
-const sourceFile = readFileSync('./template-source.yaml', 'utf8');
-let parser = new Parser();
+const sourceFile = readFileSync("./template-source.yaml", "utf8");
+const parser = new Parser();
 
-let composer = new Composer();
-let [document] = composer.compose(parser.parse(sourceFile));
+const composer = new Composer();
+const [document] = composer.compose(parser.parse(sourceFile));
 
-let visitor = (key, node) => {
-  if (node.tag === '!YAMLInclude') {
-    const files = node.value.split(',');
+const visitor = (key, node) => {
+  if (node.tag === "!YAMLInclude") {
+    const files = node.value.split(",");
     let fullContents;
-    files.forEach(fileName => {
-      const file = readFileSync(`./${fileName.trim()}`, 'utf8');
+    files.forEach((fileName) => {
+      const file = readFileSync(`./${fileName.trim()}`, "utf8");
       const contents = parseDocument(file).contents;
-      if(!fullContents) {
+      if (!fullContents) {
         fullContents = contents;
       } else {
         fullContents.items.push(...contents.items);
       }
-    })
+    });
+
+    if (node.tag === "!JSONTextInclude") {
+      const file = readFileSync(`./${node.value.trim()}`, "utf8");
+      const yamlObject = new Scalar(file);
+      return yamlObject;
+    }
 
     return fullContents;
   }
 };
-visit(document, visitor)
+visit(document, visitor);
 
 const outputContent = document.toString();
-writeFileSync('./template.yaml', outputContent);
+writeFileSync("./template.yaml", outputContent);
