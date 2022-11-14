e42c9f27a8b07b690ca76fd025020de5a2194e99
Melissa Pereira <melissa.pereira@digital.cabinet-office.gov.uk>
2022-11-14 17:47:20 +0000

    btm-212 WIP Write lambda function

--- src/handlers/extract/handler.ts
+++ src/handlers/extract/handler.ts
@@ -1,12 +1,54 @@
-export const handler = async (): Promise<string | undefined> => {
-  if (
-    String(process.env.EXTRACTION_HANDLER_THROW_ERROR).toLowerCase() === "true"
-  ) {
-    const message =
-      "Environment variable EXTRACTION_HANDLER_THROW_ERROR is true";
-    console.error(message);
-    throw new Error(message);
+import {
+  TextractClient,
+  StartExpenseAnalysisCommand,
+  Textract,
+} from "@aws-sdk/client-textract";
+import { S3Event, SQSEvent } from "aws-lambda";
+
+interface Response {
+  JobId: "string";
+}
+
+// Event should be SQSEvent? How to extract bucket name/filename?
+
+export const handler = async (event: S3Event): Promise<Response> => {
+  // Set up
+  const textract = new TextractClient({});
+
+  // Get Bucket and filename from the event
+  const bucket = event.Records[0].s3.bucket.name;
+  const fileName = event.Records[0].s3.object.key;
+
+  // Throw error if filename isn't defined
+  if (!fileName) {
+    throw new Error("ERROR - no filename found in S3 event");
   }
 
-  return process.env.EXTRACTION_HANDLER_RETURN_VALUE;
+  // Logs fileName
+  console.log("File Name: " + fileName);
+
+  // Define params for Textract API call
+  const params = {
+    DocumentLocation: {
+      S3Object: {
+        Bucket: bucket,
+        Name: fileName,
+      },
+    },
+    NotificationChannel: {
+      RoleArn: process.env.TEXT_EXTRACT_ROLE,
+      SNSTopicArn: process.env.SNS_TOPIC,
+    },
+  };
+
+  // Invoke textract function
+  try {
+    const response = await textract.send(
+      new StartExpenseAnalysisCommand(params)
+    );
+
+    return { JobId: response.JobId };
+  } catch (err) {
+    console.log(err);
+  }
 };
