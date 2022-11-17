f073c37f3d62102c6e569470cb84ef056af56d70
Nithya Kannan <112889496+NKannan14@users.noreply.github.com>
2022-11-17 12:27:41 +0000

    Btm 201 s3 extraction tests (#115)
    * added s3 extraction tests
    
    * upload file to s3 tests
    
    * renamed cloudwatch filterlog function
    
    * added sample pdf file
    
    * added s3 delete object fn
    
    * added afterAll and deletObject
    
    * rephrased test
    
    * addressed review comments

---
+++ integration_tests/tests/s3-rawinvoice-extract-function-tests.ts
@@ -0,0 +1,32 @@
+import { putObjectToS3, deleteObjectInS3 } from "../helpers/s3Helper";
+import { checkGivenStringExistsInLogs } from "../helpers/cloudWatchHelper";
+import path from "path";
+import fs from "fs";
+
+describe("\n Upload file to s3 bucket and validate extract lambda executed successfully \n", () => {
+  const bucketNameMatchString = "di-btm-rawinvoicepdfbucket";
+  const key = "payloads/sample.pdf";
+
+  afterAll(async () => {
+    await deleteObjectInS3(bucketNameMatchString, key);
+    console.log("deleted the file from s3");
+  });
+
+  test("extract lambda function should be executed without errors upon uploading the file to s3 raw invoice pdf bucket", async () => {
+    const file = "../payloads/sample.pdf";
+    const filename = path.join(__dirname, file);
+    const fileStream = fs.createReadStream(filename);
+    const response = await putObjectToS3(
+      bucketNameMatchString,
+      key,
+      fileStream
+    );
+    expect(response.$metadata.httpStatusCode).toEqual(200);
+    console.log("successfully uploaded the file to s3");
+    const givenStringExistsInLogs = await checkGivenStringExistsInLogs(
+      "di-btm-ExtractFunction-",
+      "ERROR"
+    );
+    expect(givenStringExistsInLogs).toBeFalsy();
+  });
+});
