2551feaecaf9f121c07f5a4e902c74104b3a398b
GJ <110972007+gdsgj@users.noreply.github.com>
2022-11-18 11:46:39 +0000

    Add placeholder function input logging

--- src/handlers/extract/handler.ts
+++ src/handlers/extract/handler.ts
@@ -1,4 +1,6 @@
-export const handler = async (): Promise<string | undefined> => {
+export const handler = async (event?: unknown): Promise<string | undefined> => {
+  console.log("event:", event);
+
   if (
     String(process.env.EXTRACTION_HANDLER_THROW_ERROR).toLowerCase() === "true"
   ) {
