2e19bb6cda92b4b3abed542e9803c1ab6a579c3e
NKannan14 <nithya.kannan@digital.cabinet-office.gov.uk>
2022-11-24 09:25:15 +0000

    added extract function test

--- integration_tests/tests/s3-rawinvoice-extract-function-tests.ts
+++ integration_tests/tests/s3-rawinvoice-extract-function-tests.ts
@@ -1,33 +1,40 @@
-import { putObjectToS3, deleteObjectInS3 } from "../helpers/s3Helper";
+import {
+  putObjectToS3,
+  deleteObjectInS3,
+  copyObject,
+  checkIfFileExists,
+} from "../helpers/s3Helper";
 import { checkGivenStringExistsInLogs } from "../helpers/cloudWatchHelper";
-import path from "path";
-import fs from "fs";
-import {resourcePrefix} from "../helpers/envHelper";
+import { resourcePrefix } from "../helpers/envHelper";
 
 const testStartTime = new Date().getTime();
 const prefix = resourcePrefix();
 
 describe("\n Upload file to s3 bucket and validate extract lambda executed successfully \n", () => {
   const bucketName = `${prefix}-raw-invoice-pdf`;
-  const key = "payloads/sample.pdf";
+  const key = "IP_Invoice.pdf";
 
   afterAll(async () => {
     await deleteObjectInS3(bucketName, key);
     console.log("deleted the file from s3");
   });
 
-  test("extract lambda function should be executed without errors upon uploading the file to s3 raw invoice pdf bucket", async () => {
-    const file = "../payloads/sample.pdf";
-    const filename = path.join(__dirname, file);
-    const fileStream = fs.createReadStream(filename);
-    const response = await putObjectToS3(bucketName, key, fileStream);
-    expect(response.$metadata.httpStatusCode).toEqual(200);
-    console.log("successfully uploaded the file to s3");
+  test("extract lambda function should be executed without errors and contains JobId upon uploading the file to s3 raw invoice pdf bucket", async () => {
+    await copyObject(
+      `${prefix}-raw-invoice-pdf`,
+      `${prefix}-test-invoice-pdf/IP_Invoice.pdf`,
+      "IP_Invoice.pdf"
+    );
+    const fileExists = await checkIfFileExists(
+      `${prefix}-raw-invoice-pdf`,
+      "IP_Invoice.pdf"
+    );
+    expect(fileExists).toBeTruthy();
     const givenStringExistsInLogs = await checkGivenStringExistsInLogs(
       `${prefix}-extract-function`,
-      "ERROR",
+      "Job ID",
       testStartTime
     );
-    expect(givenStringExistsInLogs).toBeFalsy();
+    expect(givenStringExistsInLogs).toBeTruthy();
   });
 });
