4c7934ef2a874329f43c549d8cd3075b37736f06
GJ <110972007+gdsgj@users.noreply.github.com>
2022-11-30 14:22:33 +0000

    Fix Textract storage function

--- src/handlers/store-raw-invoice-textract-data/handler.ts
+++ src/handlers/store-raw-invoice-textract-data/handler.ts
@@ -3,20 +3,16 @@ import { Response } from "../../shared/types";
 import { storeExpenseDocuments } from "./store-expense-documents";
 
 export const handler = async (event: SQSEvent): Promise<Response> => {
-  const pdfBucket = process.env.PDF_BUCKET;
-  const textractBucket = process.env.TEXTRACT_BUCKET;
+  const destinationBucket = process.env.DESTINATION_BUCKET;
 
-  if (pdfBucket === undefined || pdfBucket.length === 0)
-    throw new Error("PDF bucket not set.");
-
-  if (textractBucket === undefined || textractBucket.length === 0)
-    throw new Error("Textract bucket not set.");
+  if (destinationBucket === undefined || destinationBucket.length === 0)
+    throw new Error("Destination bucket not set.");
 
   const response: Response = { batchItemFailures: [] };
 
   const promises = event.Records.map(async (record) => {
     try {
-      await storeExpenseDocuments(record, pdfBucket, textractBucket);
+      await storeExpenseDocuments(record, destinationBucket);
     } catch (e) {
       console.error(e);
       response.batchItemFailures.push({ itemIdentifier: record.messageId });
