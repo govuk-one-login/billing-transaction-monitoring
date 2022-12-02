b3f6a143fd313756554b4a53ab352959e433c223
MelissaPereira <116570124+MelAtGDS@users.noreply.github.com>
2022-12-02 16:48:58 +0000

    BTM-215 Fix date and month formatting to be 2-digits to index data for athena (#137)
    * Fix date and month formatting to be 2-digits to run athena queries

---
+++ src/shared/utils/date-utils.test.ts
@@ -0,0 +1,23 @@
+import { formatDate } from "./date-utils";
+
+test("Date is formatted to yyyy-mm-dd with a month that is single digits", async () => {
+  const date: Date = new Date("2022-01-16");
+  const formattedDate = formatDate(date);
+  expect(formattedDate).toEqual("2022-01-16");
+});
+
+test("Date is formatted to yyyy-mm-dd with a date that is single digits", async () => {
+  const date: Date = new Date("2022-10-01");
+  console.log(date);
+  const formattedDate = formatDate(date);
+  console.log(formattedDate);
+  expect(formattedDate).toEqual("2022-10-01");
+});
+
+test("Date is formatted to yyyy-mm-dd with a date and month that is not single digits", async () => {
+  const date: Date = new Date("2022-12-25");
+  console.log(date);
+  const formattedDate = formatDate(date);
+  console.log(formattedDate);
+  expect(formattedDate).toEqual("2022-12-25");
+});
