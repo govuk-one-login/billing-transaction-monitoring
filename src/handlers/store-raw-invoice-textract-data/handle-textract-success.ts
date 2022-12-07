50729fe9a521cefa577c85aa8769a388857c00f5
Melissa Pereira <melissa.pereira@digital.cabinet-office.gov.uk>
2022-12-07 14:07:12 +0000

    Add OUTPUT_QUEUE_URL for store-raw-invoice-textract-data to trigger SQS on success

--- src/handlers/store-raw-invoice-textract-data/handle-textract-success.ts
+++ src/handlers/store-raw-invoice-textract-data/handle-textract-success.ts
@@ -1,6 +1,6 @@
 import { Textract } from "aws-sdk";
 import { RAW_INVOICE_TEXTRACT_DATA_FOLDER_SUCCESS } from "../../shared/constants";
-import { moveToFolderS3, putS3 } from "../../shared/utils";
+import { moveToFolderS3, putS3, sendRecord } from "../../shared/utils";
 import { handleTextractFailure } from "./handle-textract-failure";
 
 export async function handleTextractSuccess(
@@ -10,6 +10,15 @@ export async function handleTextractSuccess(
   resultsFileName: string,
   results: Textract.ExpenseDocument[]
 ): Promise<void> {
+  if (
+    process.env.OUTPUT_QUEUE_URL === undefined ||
+    process.env.OUTPUT_QUEUE_URL.length === 0
+  ) {
+    const message = "Output queue URL not set.";
+    console.error(message);
+    throw new Error(message);
+  }
+
   try {
     await putS3(resultsBucket, resultsFileName, results);
   } catch (error) {
@@ -22,4 +31,6 @@ export async function handleTextractSuccess(
     sourceFileName,
     RAW_INVOICE_TEXTRACT_DATA_FOLDER_SUCCESS
   );
+
+  await sendRecord(process.env.OUTPUT_QUEUE_URL, JSON.stringify(results));
 }
