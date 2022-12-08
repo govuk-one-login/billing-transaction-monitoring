d9014b27d4fe07f8044a046f5bdaa62e74165987
NKannan14 <nithya.kannan@digital.cabinet-office.gov.uk>
2022-12-08 13:40:51 +0000

    fixed regex review comments

--- integration_tests/tests/rate-table-athena-tests.ts
+++ integration_tests/tests/rate-table-athena-tests.ts
@@ -16,16 +16,13 @@ describe("\n Execute athena query to retrieve rate details\n", () => {
     const queryResult = await formattedQueryResults(queryId);
     const queryResultToString = JSON.stringify(queryResult)
       .replace(/\s00:00:00.000/g, "")
-      .replace(/\b(\d+)(?:(\.\d*?[1-9]+)0*|\.0*)/g, "$1$2"); // regex removes trailing zeros after decimal places eg 1.00 to 1
+      .replace(/"(\d+)(?:(\.\d*?[1-9]+)0*|\.0*)"/g, '"$1$2"'); // regex removes trailing zeros after decimal places eg 1.00 to 1
     const queryJsonObj = JSON.parse(queryResultToString);
     const csvFilePath = "../../cloudformation/prices.csv";
     const file = path.join(__dirname, csvFilePath);
     const csvData = await csvjson().fromFile(file);
     const csvFormattedData = JSON.parse(
-      JSON.stringify(csvData).replace(
-        /\b(\d+)(?:(\.\d*?[1-9]+)0*|\.0*)/g,
-        "$1$2"
-      ),
+      JSON.stringify(csvData).replace(/"(\d+)(?:(\.\d*?[1-9]+)0*|\.0*)"/g, '"$1$2"'),
       (key, value) => (value === null || value === "" ? undefined : value)
     );  // regex removes trailing zeros after decimal places eg 9.00 to 9,7.30 to 7.3
     expect(csvFormattedData).toEqual(queryJsonObj);
