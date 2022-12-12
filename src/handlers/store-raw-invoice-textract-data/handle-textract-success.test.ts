8e215291c4d0d729d81b5c54524ce5b6486da075
Melissa Pereira <melissa.pereira@digital.cabinet-office.gov.uk>
2022-12-12 10:28:29 +0000

    Change SQS notification to come from S3 bucket

--- src/handlers/store-raw-invoice-textract-data/handle-textract-success.test.ts
+++ src/handlers/store-raw-invoice-textract-data/handle-textract-success.test.ts
@@ -1,19 +1,17 @@
 import { Textract } from "aws-sdk";
 import { RAW_INVOICE_TEXTRACT_DATA_FOLDER_SUCCESS } from "../../shared/constants";
-import { moveToFolderS3, putS3, sendRecord } from "../../shared/utils";
+import { moveToFolderS3, putS3 } from "../../shared/utils";
 import { handleTextractFailure } from "./handle-textract-failure";
 import { handleTextractSuccess } from "./handle-textract-success";
 
 jest.mock("../../shared/utils");
 const mockedMoveToFolderS3 = moveToFolderS3 as jest.Mock;
 const mockedPutS3 = putS3 as jest.Mock;
-const mockedSendRecord = sendRecord as jest.MockedFunction<typeof sendRecord>;
 
 jest.mock("./handle-textract-failure");
 const mockedHandleTextractFailure = handleTextractFailure as jest.Mock;
 
 describe("Textract success handler", () => {
-  const OLD_ENV = process.env;
   const oldConsoleError = console.error;
   let givenResults: Textract.ExpenseDocument[];
   let givenResultsBucket: string;
@@ -29,32 +27,10 @@ describe("Textract success handler", () => {
     givenResultsFileName = "given results file name";
     givenSourceBucket = "given source bucket";
     givenSourceFileName = "given source file name";
-    process.env = { ...OLD_ENV, OUTPUT_QUEUE_URL: "output-queue-url" };
   });
 
   afterAll(() => {
-    process.env = OLD_ENV;
     console.error = oldConsoleError;
-  });
-
-  test("Textract success handler with SQS output queue not defined", async () => {
-    delete process.env.OUTPUT_QUEUE_URL;
-
-    let resultError;
-    try {
-      await handleTextractSuccess(
-        givenSourceBucket,
-        givenSourceFileName,
-        givenResultsBucket,
-        givenResultsFileName,
-        givenResults
-      );
-    } catch (error) {
-      resultError = error;
-    }
-
-    expect(resultError).toBeInstanceOf(Error);
-    expect(mockedSendRecord).not.toHaveBeenCalled();
   });
 
   test("Textract success handler with S3 put error", async () => {
@@ -80,7 +56,6 @@ describe("Textract success handler", () => {
       givenSourceFileName
     );
     expect(mockedMoveToFolderS3).not.toHaveBeenCalled();
-    expect(mockedSendRecord).not.toHaveBeenCalled();
   });
 
   test("Textract success handler with S3 move error", async () => {
@@ -108,7 +83,6 @@ describe("Textract success handler", () => {
       givenSourceFileName,
       RAW_INVOICE_TEXTRACT_DATA_FOLDER_SUCCESS
     );
-    expect(mockedSendRecord).not.toHaveBeenCalled();
   });
 
   test("Textract success handler with no error", async () => {
@@ -126,11 +100,6 @@ describe("Textract success handler", () => {
       givenSourceBucket,
       givenSourceFileName,
       RAW_INVOICE_TEXTRACT_DATA_FOLDER_SUCCESS
-    );
-    expect(mockedSendRecord).toHaveBeenCalledTimes(1);
-    expect(mockedSendRecord).toHaveBeenCalledWith(
-      process.env.OUTPUT_QUEUE_URL,
-      JSON.stringify(givenResults)
     );
   });
 });
