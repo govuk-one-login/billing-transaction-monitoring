80856747b2adff23a05cb8898d44d8fdbc7a27f2
Melissa Pereira <melissa.pereira@digital.cabinet-office.gov.uk>
2022-12-14 15:47:30 +0000

    update test

--- integration_tests/tests/rate-table-athena-tests.ts
+++ integration_tests/tests/rate-table-athena-tests.ts
@@ -11,7 +11,7 @@ const prefix = resourcePrefix();
 describe("\n Execute athena query to retrieve rate details\n", () => {
   test("retrieved rate details should matches with rate csv uploaded in s3 config bucket ", async () => {
     const databaseName = `${prefix}-calculations`;
-    const queryString = `SELECT * FROM \"btm_rate_tables\"`;
+    const queryString = `SELECT * FROM \"btm_rates_standardised\"`;
     const queryId = await startQueryExecutionCommand(databaseName, queryString);
     const queryResult = await formattedQueryResults(queryId);
     const queryResultToString = JSON.stringify(queryResult)
@@ -22,9 +22,12 @@ describe("\n Execute athena query to retrieve rate details\n", () => {
     const file = path.join(__dirname, csvFilePath);
     const csvData = await csvjson().fromFile(file);
     const csvFormattedData = JSON.parse(
-      JSON.stringify(csvData).replace(/"(\d+)(?:(\.\d*?[1-9]+)0*|\.0*)"/g, '"$1$2"'),
+      JSON.stringify(csvData).replace(
+        /"(\d+)(?:(\.\d*?[1-9]+)0*|\.0*)"/g,
+        '"$1$2"'
+      ),
       (key, value) => (value === null || value === "" ? undefined : value)
-    );  // regex removes trailing zeros after decimal places eg 9.00 to 9,7.30 to 7.3
+    ); // regex removes trailing zeros after decimal places eg 9.00 to 9,7.30 to 7.3
     expect(csvFormattedData).toEqual(queryJsonObj);
   });
 });
