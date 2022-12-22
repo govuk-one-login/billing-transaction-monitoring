2426f33e15d948b9080da4de18211944f3c36aac
NKannan14 <nithya.kannan@digital.cabinet-office.gov.uk>
2022-12-22 14:14:58 +0000

    fixed test to use the mock pdf

--- integration_tests/tests/s3-standardised-lambda-tests.ts
+++ integration_tests/tests/s3-standardised-lambda-tests.ts
@@ -7,43 +7,44 @@ import {
 import { resourcePrefix } from "../helpers/envHelper";
 import { checkGivenStringExistsInLogs } from "../helpers/cloudWatchHelper";
 import { waitForTrue } from "../helpers/commonHelpers";
+import {
+  makeInvoicePDF,
+  orchestrator,
+  randomInvoice,
+} from "../helpers/mock-data/invoice";
+import { writeInvoiceToS3 } from "../helpers/mock-data/writer";
 
-const testStartTime = new Date().getTime();
 const prefix = resourcePrefix();
-const rawInvoiceBucketName = `${prefix}-raw-invoice-pdf`;
+
 
 describe("\n Happy path S3 standardised-invoice-storage-function test\n", () => {
   test("standardised-invoice-storage-function should be executed without errors upon uploading the file to s3 raw invoice pdf bucket", async () => {
-    const uniqueString = Math.random().toString(36).substring(2, 7);
-    const rawInvoiceBucketKey = `raw-Invoice-${uniqueString}-validFile.pdf`;
-    await copyObject(
-      rawInvoiceBucketName,
-      `${prefix}-test-invoice-pdf/Invoice.pdf`,
-      rawInvoiceBucketKey
+    const testStartTime = new Date();
+    const invoice = randomInvoice();
+    const { bucketName, path } = await makeInvoicePDF(writeInvoiceToS3)(
+      invoice
     );
-    const checkRawPdfFileExists = await checkIfFileExists(
-      rawInvoiceBucketName,
-      rawInvoiceBucketKey
-    );
+
+    const checkRawPdfFileExists = await checkIfFileExists(bucketName, path);
     expect(checkRawPdfFileExists).toBeTruthy();
     console.log("file exists in raw invoice pdf bucket");
 
     const givenStringExistsInLogs = await checkGivenStringExistsInLogs(
       `${prefix}-standardised-invoice-storage-function`,
       "ERROR",
-      testStartTime
+      testStartTime.getTime()
     ); // checks for ERROR string in standardised invoice storage function cloudwatch logs
 
     expect(givenStringExistsInLogs).toBeFalsy();
 
     const deleteFileAfterTest = async () => {
-      const result = await getS3ItemsList(rawInvoiceBucketName, "successful");
+      const result = await getS3ItemsList(bucketName, "successful");
       if (
-        result.Contents?.filter((t) => t.Key?.includes(rawInvoiceBucketKey))
+        result.Contents?.filter((t) => t.Key?.includes(path))
       ) {
         await deleteObjectInS3(
-          rawInvoiceBucketName,
-          "successful/" + rawInvoiceBucketKey
+          bucketName,
+          "successful/" + path
         );
         console.log("deleted the file from s3");
         return true;
