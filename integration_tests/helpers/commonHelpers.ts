0d47a0cc508f17717731a6bb4ab4d06be3cf12eb
NKannan14 <nithya.kannan@digital.cabinet-office.gov.uk>
2022-12-23 15:38:40 +0000

    fixed review comments

--- integration_tests/helpers/commonHelpers.ts
+++ integration_tests/helpers/commonHelpers.ts
@@ -1,17 +1,21 @@
-import { generateRandomNumber, snsValidEventPayload } from "../payloads/snsEventPayload";
+import {
+  generateRandomNumber,
+  snsValidEventPayload,
+} from "../payloads/snsEventPayload";
 import { deleteObjectInS3, getS3ItemsList } from "./s3Helper";
 import { publishSNS } from "./snsHelper";
 import { resourcePrefix } from "../helpers/envHelper";
+import { validTimestamp } from '../payloads/snsEventPayload';
 
 const prefix = resourcePrefix();
 const objectsPrefix = "btm_transactions";
 const details: string[] = [];
 
-async function waitForTrue(
+export const waitForTrue = async (
   predicate: Function,
   delayMS: number,
   timeoutMS: number
-): Promise<boolean> {
+): Promise<boolean> => {
   let intervalHandle: any;
   return await new Promise((resolve) => {
     const complete = (result: boolean): void => {
@@ -19,7 +23,7 @@ async function waitForTrue(
       resolve(result);
     };
     const callPredicateAndComplete = async (): Promise<void> => {
-      (await predicate()) && complete(true)
+      (await predicate()) && complete(true);
     };
     intervalHandle = setInterval(() => {
       void callPredicateAndComplete();
@@ -28,46 +32,41 @@ async function waitForTrue(
   });
 }
 
-async function generateTestEventsAndValidateEventExists(
-  numberOfTestEvents: number,
-  eventName: string,
-  clientId: string
-): Promise<void> {
-  for (let i = 0; i < numberOfTestEvents; i++) {
+export const generateTestEvents = async (eventName:string,
+  clientId:string): Promise<Object> => {
+
     snsValidEventPayload.event_name = eventName;
     snsValidEventPayload.event_id = generateRandomNumber();
     details.push(snsValidEventPayload.event_id); // storing event_ids in array to delete from s3 later on
     snsValidEventPayload.client_id = clientId;
-    await publishSNS(snsValidEventPayload);
+    return snsValidEventPayload;
+};
 
-    const checkEventId = async (): Promise<boolean> => {
-      const result = await getS3ItemsList(`${prefix}-storage`, objectsPrefix);
-      if (result.Contents === undefined) {
-        console.log("Storage bucket contents empty");
-        return false;
-      }
-      return JSON.stringify(result.Contents.map((data) => data.Key)).includes(
-        snsValidEventPayload.event_id
-      );
-    };
-    const eventIdExists = await waitForTrue(checkEventId, 1000, 10000);
-    expect(eventIdExists).toBeTruthy();
-  }
+export const publishAndValidateEvents = async(events: object) => {
+  await publishSNS(events);
+  const checkEventId = async (): Promise<boolean> => {
+    const result = await getS3ItemsList(`${prefix}-storage`, objectsPrefix);
+    if (result.Contents === undefined) {
+      console.log("Storage bucket contents empty");
+      return false;
+    }
+    return JSON.stringify(result.Contents.map((data) => data.Key)).includes(
+      snsValidEventPayload.event_id
+    );
+  };
+  const eventIdExists = await waitForTrue(checkEventId, 1000, 10000);
+  expect(eventIdExists).toBeTruthy();
 }
 
-
-async function deleteS3Event(): Promise<boolean> {
+export const deleteS3Event = async(): Promise<boolean> =>{
   const bucketName = `${prefix}-storage`;
   const date = new Date().toISOString().slice(0, 10);
   for (let i = 0; i < details.length; i++) {
     await deleteObjectInS3(
       bucketName,
-      `btm_transactions/${date}/${details[i]}.json` 
+      `btm_transactions/${date}/${details[i]}.json`
     );
   }
   console.log("deleted the file from s3");
   return true;
-};
-
-
-export { waitForTrue, deleteS3Event, generateTestEventsAndValidateEventExists  };
+}
--- integration_tests/tests/transaction-view-athena-tests.ts
+++ integration_tests/tests/transaction-view-athena-tests.ts
@@ -6,57 +6,68 @@ import {
 
 import {
   deleteS3Event,
-  generateTestEventsAndValidateEventExists,
   } from "../helpers/commonHelpers";
 import { publishSNS } from "../helpers/snsHelper";
-import { snsInvalidEventNamePayload } from "../payloads/snsEventPayload";
+import { snsInvalidEventNamePayload } from '../payloads/snsEventPayload';
+import { generateTestEvents, publishAndValidateEvents } from '../helpers/commonHelpers';
 
 const prefix = resourcePrefix();
 const databaseName = `${prefix}-calculations`;
 
 describe("\nExecute athena query to retrive transaction data\n", () => {
   test("price retrived from billing_curated athena view query should match with expected calculated price for 2 events", async () => {
-    const expectedCalculatedPrice = (2 * 6.5).toFixed(4); // fake_prices.csv indicates these should be charged at £6.50 each
-    await generateTestEventsAndValidateEventExists(
-      2,
-      "IPV_ADDRESS_CRI_END",
-      "client3"
+    const expectedCalculatedPrice = (2 * 3.99).toFixed(4);
+    const numberofTestEvents = 2 // fake_prices.csv indicates these should be charged at £6.50 each
+    for (let i = 0; i < numberofTestEvents; i++) {
+    const events=  await generateTestEvents(
+      "IPV_KBV_CRI_REQUEST_SENT",
+      "client2" 
     );
+    console.log(events)
+    await publishAndValidateEvents(events)
+    }
     const response = await queryResults();
     expect(expectedCalculatedPrice).toEqual(response[0].price);
-  });
+    await deleteS3Event()
+    });
 
   test("price retrived from billing_curated athena view query should match with expected calculated price for 7 events", async () => {
-    const expectedCalculatedPrice = (7 * 0.25).toFixed(4); // fake_prices.csv indicates these should be charged at £0.25 each
-    await generateTestEventsAndValidateEventExists(
-      7,
-      "IPV_ADDRESS_CRI_END",
+    const expectedCalculatedPrice = (7 * 4.00).toFixed(4); // fake_prices.csv indicates these should be charged at £0.25 each
+    const numberofTestEvents = 7 
+    for (let i = 0; i < numberofTestEvents; i++) {
+    const events = await generateTestEvents(
+      "IPV_PASSPORT_CRI_REQUEST_SENT",
       "client3"
     );
+    console.log(events)
+    await publishAndValidateEvents(events)
+    }
     const response = await queryResults();
     expect(expectedCalculatedPrice).toEqual(response[0].price);
+    await deleteS3Event()
   });
 
   test("price retrived from billing_curated athena view query should match with expected calculated price for 14 events", async () => {
     const expectedCalculatedPrice = (14 * 8.88).toFixed(4); // fake_prices.csv indicates these should be charged at £8.88 each
-    await generateTestEventsAndValidateEventExists(
-      14,
+    const numberofTestEvents = 14 
+    for (let i = 0; i < numberofTestEvents; i++) {
+    const events = await generateTestEvents(
       "IPV_ADDRESS_CRI_END",
       "client3"
     );
+    console.log(events)
+    await publishAndValidateEvents(events)
+    }
     const response = await queryResults();
     expect(expectedCalculatedPrice).toEqual(response[0].price);
+    await deleteS3Event()
   });
 
   test("no results returned from billing_curated athena view query when the event payload has invalid eventName", async () => {
     await publishSNS(snsInvalidEventNamePayload);
     const queryRes = await queryResults();
     expect(queryRes.length).not.toBeGreaterThan(0);
-  });
-
-  afterEach(async () => {
-    await deleteS3Event();
-    console.log("Deleted s3 events after each test");
+    await deleteS3Event()
   });
 });
 
