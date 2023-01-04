b37498c5bdba456efedb1b09109cbad9236ae8fd
MelissaPereira <116570124+MelAtGDS@users.noreply.github.com>
2023-01-04 16:25:20 +0000

    BTM-241 expected p time q (#165)
    Co-authored-by: GJ <110972007+gdsgj@users.noreply.github.com>
    Co-authored-by: Nithya Kannan <112889496+NKannan14@users.noreply.github.com>
    Co-authored-by: Mark Schnitzius <mark.schnitzius@digital.cabinet-office.gov.uk>
    Co-authored-by: Mark Potter <si3rr4wow@gmail.com>

--- integration_tests/helpers/commonHelpers.ts
+++ integration_tests/helpers/commonHelpers.ts
@@ -1,23 +1,107 @@
-async function waitForTrue(
-  predicate: Function,
+import {
+  ClientId,
+  SNSEventPayload,
+  EventName,
+} from "../payloads/snsEventPayload";
+import { deleteObjectInS3, getS3ItemsList } from "./s3Helper";
+import { publishSNS } from "./snsHelper";
+import { resourcePrefix } from "../helpers/envHelper";
+
+const prefix = resourcePrefix();
+const objectsPrefix = "btm_transactions";
+
+export const generateRandomId = (): string => {
+  return Math.floor(Math.random() * 10000000).toString();
+};
+
+export const validTimestamp = (): number => {
+  return new Date().getTime() / 1000;
+};
+
+export const waitForTrue = async (
+  predicate: () => Promise<boolean | undefined | Object[]>,
   delayMS: number,
   timeoutMS: number
-): Promise<boolean> {
+): Promise<boolean> => {
   let intervalHandle: any;
   return await new Promise((resolve) => {
     const complete = (result: boolean): void => {
       clearInterval(intervalHandle);
       resolve(result);
     };
+
     const callPredicateAndComplete = async (): Promise<void> => {
-      (await predicate()) && complete(true)
+      ((await predicate()) as boolean) && complete(true);
     };
     intervalHandle = setInterval(() => {
       void callPredicateAndComplete();
     }, delayMS);
     setTimeout(() => complete(false), timeoutMS);
   });
-}
+};
 
+export const generateTestEvent = async (
+  eventName: EventName,
+  clientId: ClientId
+): Promise<SNSEventPayload> => {
+  return {
+    event_name: eventName,
+    event_id: generateRandomId(),
+    component_id: "TEST_COMP",
+    timestamp: validTimestamp(),
+    client_id: clientId,
+  };
+};
 
-export { waitForTrue };
+export const publishAndValidateEvent = async (
+  event: SNSEventPayload
+): Promise<void> => {
+  await publishSNS(event);
+  const checkEventId = async (): Promise<boolean> => {
+    const result = await getS3ItemsList(`${prefix}-storage`, objectsPrefix);
+    if (result.Contents === undefined) {
+      console.log("Storage bucket contents empty");
+      return false;
+    }
+    return result.Contents.some((data) => data.Key?.match(event.event_id));
+  };
+  const eventIdExists = await waitForTrue(checkEventId, 1000, 10000);
+  expect(eventIdExists).toBeTruthy();
+};
+
+export const generatePublishAndValidateEvents = async ({
+  numberOfTestEvents,
+  eventName,
+  clientId,
+}: {
+  numberOfTestEvents: number;
+  eventName: EventName;
+  clientId: ClientId;
+}): Promise<string[]> => {
+  const eventIds: string[] = [];
+  for (let i = 0; i < numberOfTestEvents; i++) {
+    const event = await generateTestEvent(eventName, clientId);
+    await publishAndValidateEvent(event);
+    eventIds.push(event.event_id); // storing event_ids in array to delete from s3 later on
+  }
+  return eventIds;
+};
+
+export const deleteS3Event = async (eventId: string): Promise<boolean> => {
+  const bucketName = `${prefix}-storage`;
+  const date = new Date().toISOString().slice(0, 10);
+  await deleteObjectInS3({
+    bucket: bucketName,
+    key: `btm_transactions/${date}/${eventId}.json`,
+  });
+  console.log("deleted the file from s3");
+  return true;
+};
+
+export const deleteS3Events = async (eventIds: string[]): Promise<boolean> => {
+  for (const eventId of eventIds) {
+    await deleteS3Event(eventId);
+  }
+  console.log("deleted the files from s3");
+  return true;
+};
--- integration_tests/payloads/snsEventPayload.ts
+++ integration_tests/payloads/snsEventPayload.ts
@@ -1,86 +1,114 @@
-const generateRandomNumber = (): string => {
-  return Math.floor(Math.random() * 10000000).toString();
+import { generateRandomId, validTimestamp } from "../helpers/commonHelpers";
+
+export enum ClientId {
+  client1 = "client1",
+  client2 = "client2",
+  client3 = "client3",
+}
+
+export enum EventName {
+  EVENT_1 = "EVENT_1",
+  EVENT_3 = "EVENT_3",
+  EVENT_7 = "EVENT_7",
+  EVENT_6 = "EVENT_6",
+}
+
+export interface SNSEventPayload {
+  event_name: EventName;
+  event_id: string;
+  component_id: string;
+  timestamp: number;
+  client_id: ClientId;
+}
+
+export const prettyClientNameMap = {
+  client1: "Client One",
+  client2: "Client Two",
+  client3: "Client Three",
 };
 
-const validTimestamp = (): number => {
-  return new Date().getTime() / 1000;
+export const prettyEventNameMap = {
+  EVENT_1: "Passport check",
+  EVENT_3: "Fraud check",
+  EVENT_7: "Kbv check",
+  EVENT_6: "Address check",
 };
 
-const snsValidEventPayload = {
-  event_name: "EVENT_1",
-  event_id: generateRandomNumber(),
+const snsValidEventPayload: SNSEventPayload = {
+  event_name: EventName.EVENT_1,
+  event_id: generateRandomId(),
   component_id: "TEST_COMP",
   timestamp: validTimestamp(),
-  client_id: "client1",
+  client_id: ClientId.client1,
 };
 
-const snsEventWithAdditionalFieldsPayload = {
-  event_name: "EVENT_1",
-  event_id: generateRandomNumber(),
+const snsEventWithAdditionalFieldsPayload: SNSEventPayload = {
+  event_name: EventName.EVENT_1,
+  event_id: generateRandomId(),
   component_id: "TEST_COMP",
   timestamp: validTimestamp(),
   someUnwantedField: "some value",
-  client_id: "client1",
-};
+  client_id: ClientId.client1,
+} as unknown as SNSEventPayload;
 
-const snsInvalidEventNamePayload = {
-  event_name: "TESTGGHYJKIK",
-  event_id: generateRandomNumber(),
+const snsInvalidEventNamePayload: SNSEventPayload = {
+  event_name: "TESTGGHYJKIK" as EventName,
+  event_id: generateRandomId(),
   component_id: "TEST_COMP",
   timestamp: validTimestamp(),
-  client_id: "client1",
+  client_id: ClientId.client1,
 };
 
-const snsInvalidTimeStampPayload = {
-  event_name: "EVENT_1",
-  event_id: generateRandomNumber(),
+const snsInvalidTimeStampPayload: SNSEventPayload = {
+  event_name: EventName.EVENT_1,
+  event_id: generateRandomId(),
   component_id: "TEST_COMP",
-  timestamp: "somestring",
-  client_id: "client1",
+  timestamp: "somestring" as unknown as number,
+  client_id: ClientId.client1,
 };
 
-const snsMissingEventNamePayload = {
-  event_id: generateRandomNumber(),
-  component_id: "TEST_COMP",
-  timestamp: validTimestamp(),
-  client_id: "client1",
-};
-
-const snsEventMissingTimestampPayload = {
-  event_name: "EVENT_1",
-  event_id: generateRandomNumber(),
-  component_id: "TEST_COMP",
-  client_id: "client1",
-};
-
-const snsEventMissingCompIdPayload = {
-  event_name: "EVENT_1",
-  event_id: generateRandomNumber(),
-  timestamp: validTimestamp(),
-  client_id: "client1",
-};
-
-const snsMissingEventIdPayload = {
-  event_name: "EVENT_1",
+const snsMissingEventNamePayload: SNSEventPayload = {
+  event_id: generateRandomId(),
   component_id: "TEST_COMP",
   timestamp: validTimestamp(),
-  client_id: "client1",
+  client_id: ClientId.client1,
+} as unknown as SNSEventPayload;
+
+const snsEventMissingTimestampPayload: SNSEventPayload = {
+  event_name: EventName.EVENT_1,
+  event_id: generateRandomId(),
+  component_id: "TEST_COMP",
+  client_id: ClientId.client1,
+} as unknown as SNSEventPayload;
+
+const snsEventMissingCompIdPayload: SNSEventPayload = {
+  event_name: EventName.EVENT_1,
+  event_id: generateRandomId(),
+  timestamp: validTimestamp(),
+  client_id: ClientId.client1,
+} as unknown as SNSEventPayload;
+
+const snsMissingEventIdPayload: SNSEventPayload = {
+  event_name: EventName.EVENT_1,
+  component_id: "TEST_COMP",
+  timestamp: validTimestamp(),
+  client_id: ClientId.client1,
+} as unknown as SNSEventPayload;
+
+const snsEventInvalidCompId: SNSEventPayload = {
+  event_name: EventName.EVENT_1,
+  event_id: generateRandomId(),
+  component_id: 5678 as unknown as string,
+  timestamp: validTimestamp(),
+  client_id: ClientId.client1,
 };
 
-const snsEventInvalidCompId = {
-  event_name: "EVENT_1",
-  event_id: generateRandomNumber(),
-  component_id: 5678,
+const snsEventMissingEventIdValue: SNSEventPayload = {
+  event_name: EventName.EVENT_1,
+  event_id: null as unknown as string,
+  component_id: 5678 as unknown as string,
   timestamp: validTimestamp(),
-  client_id: "client1",
-};
-
-const snsEventMisisingEventIdValue = {
-  event_name: "EVENT_1",
-  event_id: null,
-  component_id: 5678,
-  timestamp: validTimestamp(),
-  client_id: "client1",
+  client_id: ClientId.client1,
 };
 
 export {
@@ -93,5 +121,6 @@ export {
   snsEventMissingTimestampPayload,
   snsEventWithAdditionalFieldsPayload,
   snsMissingEventNamePayload,
-  snsEventMisisingEventIdValue,
+  snsEventMissingEventIdValue,
+  generateRandomId,
 };
