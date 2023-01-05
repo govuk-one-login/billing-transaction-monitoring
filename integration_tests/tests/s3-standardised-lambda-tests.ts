8370f2b0497262263370d288bf31ec4f15e6902c
Mark Potter <35689158+Si3rr4wow@users.noreply.github.com>
2023-01-05 09:54:32 +0000

    Btm 319 fake pdf (#166)
    * randomish quantities spat out tabularly
    
    * BTM-319 adjust test to use random invoice
    
    * added invoice number to the pdf
    
    * fixed test to use the mock pdf
    
    * removed invoice pdf upload inst from readme
    
    * add more deets to invoice pdf
    
    * lint
    
    * tidier folder interface
    
    * removed test invoice bucket
    
    * fix standardised test imports and lint
    
    * cleaner nullish coalescence
    
    * fixed imports in writer.ts
    
    Co-authored-by: NKannan14 <nithya.kannan@digital.cabinet-office.gov.uk>

--- integration_tests/tests/s3-standardised-lambda-tests.ts
+++ integration_tests/tests/s3-standardised-lambda-tests.ts
@@ -1,54 +1,52 @@
 import {
-  copyObject,
   checkIfS3ObjectExists,
   deleteObjectInS3,
   getS3ItemsList,
-  S3Object,
 } from "../helpers/s3Helper";
 import { resourcePrefix } from "../helpers/envHelper";
 import { checkGivenStringExistsInLogs } from "../helpers/cloudWatchHelper";
 import { waitForTrue } from "../helpers/commonHelpers";
+import {
+  makeMockInvoicePDF,
+  randomInvoice,
+  writeInvoiceToS3,
+} from "../helpers/mock-data/invoice";
 
-const testStartTime = new Date().getTime();
 const prefix = resourcePrefix();
-const testinvoice: S3Object = {
-  bucket: "di-btm-dev-raw-invoice-pdf",
-  key: "Invoice.pdf",
-};
 
 describe("\n Happy path S3 standardised-invoice-storage-function test\n", () => {
   test("standardised-invoice-storage-function should be executed without errors upon uploading the file to s3 raw invoice pdf bucket", async () => {
-    const uniqueString = Math.random().toString(36).substring(2, 7);
-    const rawInvoice: S3Object = {
-      bucket: `${prefix}-raw-invoice-pdf`,
-      key: `raw-Invoice-${uniqueString}-validFile.pdf`,
-    };
-    await copyObject(testinvoice, rawInvoice);
-    const checkRawPdfFileExists = await checkIfS3ObjectExists(rawInvoice);
+    const testStartTime = new Date();
+    const invoice = randomInvoice();
+    const { bucketName, path } = await makeMockInvoicePDF(writeInvoiceToS3)(
+      invoice
+    );
+
+    const checkRawPdfFileExists = await checkIfS3ObjectExists({
+      bucket: bucketName,
+      key: path,
+    });
     expect(checkRawPdfFileExists).toBeTruthy();
     console.log("file exists in raw invoice pdf bucket");
 
     const givenStringExistsInLogs = await checkGivenStringExistsInLogs(
       `${prefix}-standardised-invoice-storage-function`,
       "ERROR",
-      testStartTime
+      testStartTime.getTime()
     ); // checks for ERROR string in standardised invoice storage function cloudwatch logs
 
     expect(givenStringExistsInLogs).toBeFalsy();
 
     const deleteFileAfterTest = async (): Promise<boolean> => {
-      const result = await getS3ItemsList(rawInvoice.bucket, "successful");
-      if (
-        result?.Contents?.filter((t) => t.Key?.includes(rawInvoice.key)) != null
-      ) {
-        await deleteObjectInS3({
-          bucket: rawInvoice.bucket,
-          key: "successful/" + rawInvoice.key,
-        });
-        console.log("deleted the file from s3");
-        return true;
-      }
-      return false;
+      const result = await getS3ItemsList(bucketName, "successful");
+      const pathIsNotInBucket = !(
+        result.Contents?.some((t) => t.Key?.includes(path)) ?? false
+      );
+      if (pathIsNotInBucket) return false;
+
+      await deleteObjectInS3({ bucket: bucketName, key: "successful/" + path });
+      console.log("deleted the file from s3");
+      return true;
     };
     const fileDeleted = await waitForTrue(deleteFileAfterTest, 1000, 5000);
     expect(fileDeleted).toBeTruthy();
