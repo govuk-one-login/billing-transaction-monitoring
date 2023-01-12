0827f7e312914ebf20bf189c081f7d2c712e4117
Melissa Pereira <melissa.pereira@digital.cabinet-office.gov.uk>
2023-01-12 15:48:32 +0000

    WIP Need to resolve circular dependency error

--- src/handlers/transformation/handler.ts
+++ src/handlers/transformation/handler.ts
@@ -1,5 +1,19 @@
 import { S3Event } from "aws-lambda";
+import * as AWS from "aws-sdk";
+import csv from "csvtojson";
 
 export const handler = async (event: S3Event): Promise<void> => {
-  console.log("event:", JSON.stringify(event.Records[0].s3));
+  const S3 = new AWS.S3();
+
+  const params = {
+    Bucket: event.Records[0].s3.bucket.name,
+    Key: event.Records[0].s3.object.key,
+  };
+
+  console.log("** params:", params);
+
+  const stream = S3.getObject(params).createReadStream();
+
+  const json = await csv().fromStream(stream);
+  console.log(json);
 };
