567608ced665f45de0a10755cde516b06e5e594f
Mark Schnitzius <mark.schnitzius@digital.cabinet-office.gov.uk>
2023-01-12 15:11:03 +0000

    Early work on transformation handler

--- src/handlers/transformation/handler.ts
+++ src/handlers/transformation/handler.ts
@@ -1,5 +1,17 @@
 import { S3Event } from "aws-lambda";
+import * as AWS from "aws-sdk";
+import csv from "csvtojson";
 
 export const handler = async (event: S3Event): Promise<void> => {
-  console.log("event:", JSON.stringify(event.Records[0].s3));
+
+  const details = event.Records[0].s3;
+
+  const params = { Bucket: details.bucket.name, Key: details.object.key };
+
+  const S3 = new AWS.S3();
+  const stream = S3.getObject(params).createReadStream();
+
+  const json = await csv().fromStream(stream);
+  console.log(json);
+
 };
