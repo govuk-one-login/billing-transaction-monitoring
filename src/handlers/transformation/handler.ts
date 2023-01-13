b0289a970b11a6794a03ecf14794fd3f5865b2d9
Melissa Pereira <melissa.pereira@digital.cabinet-office.gov.uk>
2023-01-13 15:05:17 +0000

    BTM-344 WIP

--- src/handlers/transformation/handler.ts
+++ src/handlers/transformation/handler.ts
@@ -1,47 +1,39 @@
-import {S3Event, SQSRecord} from "aws-lambda";
+import { S3Event } from "aws-lambda";
 import * as AWS from "aws-sdk";
 import csv from "csvtojson";
-import {sendRecord} from "../../shared/utils";
-import {Response} from "../../shared/types";
+import { sendRecord } from "../../shared/utils";
 
 interface TransformationEventBodyObject {
-  event_id: string,
-  timestamp: number,
-  timestamp_formatted: string,
-  event_name: string,
-  component_id: string,
-  client_id: string
+  event_id: string;
+  timestamp: number;
+  timestamp_formatted: string;
+  event_name: string;
+  component_id: string;
+  client_id: string;
 }
 
 export const handler = async (event: S3Event): Promise<void> => {
-  const S3 = new AWS.S3();
+  const rows = await transformCsvToJson(event);
 
+  const promises = rows.map(async (row) => {
+    await transformRow(row);
+    await Promise.all(promises);
+  });
+};
+
+async function transformCsvToJson(event: S3Event): Promise<any[]> {
+  const S3 = new AWS.S3();
   const params = {
     Bucket: event.Records[0].s3.bucket.name,
     Key: event.Records[0].s3.object.key,
   };
-
   const stream = S3.getObject(params).createReadStream();
-
   const rows = await csv().fromStream(stream);
-
-  const response: Response = { batchItemFailures: [] };
-
-  const promises = rows.map(async (row) => {
-    try {
-      await transformRow(row);
-    } catch (e) {
-      response.batchItemFailures.push({itemIdentifier: row.messageId});
-    }
-
-
-    await Promise.all(promises);
-    return response;
-  });
-};
+  console.log("transformCsvToJson rows", rows);
+  return rows;
+}
 
 async function transformRow(row: any): Promise<void> {
-
   if (
     process.env.OUTPUT_QUEUE_URL === undefined ||
     process.env.OUTPUT_QUEUE_URL.length === 0
@@ -55,7 +47,7 @@ async function transformRow(row: any): Promise<void> {
 
   const idpEntityId = row["Idp Entity Id"];
   const requestId = row["Request Id"];
-  const timestamp = row.Timestamp;
+  const timestamp = 1671117135;
   const timestampFormatted = row.Timestamp;
   const rpEntityId = row["RP Entity Id"];
 
@@ -65,7 +57,7 @@ async function transformRow(row: any): Promise<void> {
     timestamp_formatted: timestampFormatted,
     event_name: "NEW_EVENT_NAME",
     component_id: rpEntityId,
-    client_id: idpEntityId
+    client_id: idpEntityId,
   };
 
   await sendRecord(
--- src/shared/utils/sqs.ts
+++ src/shared/utils/sqs.ts
@@ -1,8 +1,14 @@
 import { SQSClient, SendMessageCommand } from "@aws-sdk/client-sqs";
 
-const sqs = new SQSClient({region: 'eu-west-2', endpoint: process.env.LOCAL_ENDPOINT});
+const sqs = new SQSClient({
+  region: "eu-west-2",
+  endpoint: process.env.LOCAL_ENDPOINT,
+});
 
-export async function sendRecord(queueUrl: string, messageBody: string) {
+export async function sendRecord(
+  queueUrl: string,
+  messageBody: string
+): Promise<void> {
   console.log("sending messageBody " + messageBody);
   console.log("queueurl " + queueUrl);
 
@@ -11,10 +17,13 @@ export async function sendRecord(queueUrl: string, messageBody: string) {
     QueueUrl: queueUrl,
   });
 
-  return await sqs.send(params).then((data) => {
-    console.log(data);
-  }).catch(err => {
-    console.log(err, err.stack);
-    throw(err);
-  });
+  return await sqs
+    .send(params)
+    .then((data) => {
+      console.log("SQS data", data);
+    })
+    .catch((err) => {
+      console.log(err, err.stack);
+      throw err;
+    });
 }
