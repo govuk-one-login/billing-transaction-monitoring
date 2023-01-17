28b759872bdeee17d2282a8cbe3837b79f75ce0e
Mark Schnitzius <mark.schnitzius@digital.cabinet-office.gov.uk>
2023-01-17 15:58:24 +0000

    start of test mocking

--- src/handlers/transformation/handler.test.ts
+++ src/handlers/transformation/handler.test.ts
@@ -1,9 +1,11 @@
 import { handler } from "./handler";
 import { createEvent } from "../../../test-helpers/S3";
-import { sendRecord } from "../../shared/utils";
+import {readJsonFromS3, sendRecord} from "../../shared/utils";
 
 jest.mock("../../shared/utils");
 const mockedSendRecord = sendRecord as jest.MockedFunction<typeof sendRecord>;
+
+let mockReadJsonFromS3 = readJsonFromS3 as jest.MockedFunction<typeof readJsonFromS3>;
 
 describe("Transformation handler tests", () => {
   const OLD_ENV = process.env;
@@ -16,6 +18,8 @@ describe("Transformation handler tests", () => {
     console.log = jest.fn();
     mockedSendRecord.mockClear();
     process.env.OUTPUT_QUEUE_URL = "output-queue-url";
+
+    mockReadJsonFromS3.mockResolvedValue()
   });
 
   afterAll(() => {
