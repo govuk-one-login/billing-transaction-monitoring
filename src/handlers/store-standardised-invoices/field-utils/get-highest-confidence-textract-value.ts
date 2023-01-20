5d2a48160616f30e7cc30217830c99bed3c94752
GJ <110972007+gdsgj@users.noreply.github.com>
2023-01-20 11:28:21 +0000

    Refactor highest-confidence field finding

--- src/handlers/store-standardised-invoices/field-utils/get-highest-confidence-textract-value.ts
+++ src/handlers/store-standardised-invoices/field-utils/get-highest-confidence-textract-value.ts
@@ -14,12 +14,16 @@ export const getHighestConfidenceTextractValue = (
 
   if (candidateFields.length === 0) return;
 
-  let highestConfidenceFieldFound = candidateFields[0];
-  for (const field of candidateFields.slice(1))
-    if (getConfidence(field) > getConfidence(highestConfidenceFieldFound))
-      highestConfidenceFieldFound = field;
+  // Sort confidence highest to lowest
+  candidateFields.sort((field1, field2) => {
+    const confidence1 = getConfidence(field1);
+    const confidence2 = getConfidence(field2);
 
-  return highestConfidenceFieldFound.ValueDetection?.Text;
+    return confidence1 > confidence2 ? -1 : confidence1 < confidence2 ? 1 : 0;
+  });
+
+  const highestConfidenceField = candidateFields[0];
+  return highestConfidenceField.ValueDetection?.Text;
 };
 
 const getConfidence = (field: Textract.ExpenseField): number => {
