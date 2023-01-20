9fcb0c75914f51a322ffb7aa1199099e9f7b49b2
GJ <110972007+gdsgj@users.noreply.github.com>
2023-01-20 11:58:14 +0000

    Refactor more synchronous error tests

--- src/shared/utils/s3.test.ts
+++ src/shared/utils/s3.test.ts
@@ -246,112 +246,54 @@ describe("S3 event records getter tests", () => {
 
   test("S3 event records getter with invalid body JSON", () => {
     givenQueueRecord.body = "{";
-
-    let resultError;
-    try {
-      getS3EventRecords(givenQueueRecord);
-    } catch (error) {
-      resultError = error;
-    }
-
-    expect(resultError).toBeInstanceOf(Error);
+    expect(() => getS3EventRecords(givenQueueRecord)).toThrowError("JSON");
   });
 
   test("S3 event records getter with body JSON not object", () => {
     givenQueueRecord.body = "1234";
-
-    let resultError;
-    try {
-      getS3EventRecords(givenQueueRecord);
-    } catch (error) {
-      resultError = error;
-    }
-
-    expect(resultError).toBeInstanceOf(Error);
+    expect(() => getS3EventRecords(givenQueueRecord)).toThrowError("object");
   });
 
   test("S3 event records getter with body records not array", () => {
     givenBody.Records = 1234;
     givenQueueRecord.body = JSON.stringify(givenBody);
 
-    let resultError;
-    try {
-      getS3EventRecords(givenQueueRecord);
-    } catch (error) {
-      resultError = error;
-    }
-
-    expect(resultError).toBeInstanceOf(Error);
+    expect(() => getS3EventRecords(givenQueueRecord)).toThrowError("S3 event");
   });
 
   test("S3 event records getter with body record empty object", () => {
     givenBody.Records[0] = {};
     givenQueueRecord.body = JSON.stringify(givenBody);
 
-    let resultError;
-    try {
-      getS3EventRecords(givenQueueRecord);
-    } catch (error) {
-      resultError = error;
-    }
-
-    expect(resultError).toBeInstanceOf(Error);
+    expect(() => getS3EventRecords(givenQueueRecord)).toThrowError("S3 event");
   });
 
   test("S3 event records getter with body record without bucket", () => {
     givenBody.Records[0].s3.bucket = undefined;
     givenQueueRecord.body = JSON.stringify(givenBody);
 
-    let resultError;
-    try {
-      getS3EventRecords(givenQueueRecord);
-    } catch (error) {
-      resultError = error;
-    }
-
-    expect(resultError).toBeInstanceOf(Error);
+    expect(() => getS3EventRecords(givenQueueRecord)).toThrowError("S3 event");
   });
 
   test("S3 event records getter with body record bucket without name", () => {
     givenBody.Records[0].s3.bucket.name = undefined;
     givenQueueRecord.body = JSON.stringify(givenBody);
 
-    let resultError;
-    try {
-      getS3EventRecords(givenQueueRecord);
-    } catch (error) {
-      resultError = error;
-    }
-
-    expect(resultError).toBeInstanceOf(Error);
+    expect(() => getS3EventRecords(givenQueueRecord)).toThrowError("S3 event");
   });
 
   test("S3 event records getter with body record without S3 object", () => {
     givenBody.Records[0].s3.object = undefined;
     givenQueueRecord.body = JSON.stringify(givenBody);
 
-    let resultError;
-    try {
-      getS3EventRecords(givenQueueRecord);
-    } catch (error) {
-      resultError = error;
-    }
-
-    expect(resultError).toBeInstanceOf(Error);
+    expect(() => getS3EventRecords(givenQueueRecord)).toThrowError("S3 event");
   });
 
   test("S3 event records getter with body record S3 object without key", () => {
     givenBody.Records[0].s3.object.key = undefined;
     givenQueueRecord.body = JSON.stringify(givenBody);
 
-    let resultError;
-    try {
-      getS3EventRecords(givenQueueRecord);
-    } catch (error) {
-      resultError = error;
-    }
-
-    expect(resultError).toBeInstanceOf(Error);
+    expect(() => getS3EventRecords(givenQueueRecord)).toThrowError("S3 event");
   });
 
   test("S3 event records getter with valid record", () => {
