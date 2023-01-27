c2c12bc414360cfa211037107c9199642f702d59
Melissa Pereira <melissa.pereira@digital.cabinet-office.gov.uk>
2023-01-27 17:38:36 +0000

    BTM-344 Use existing S3 functions from utils

--- src/handlers/transformation/transform-csv-to-json.ts
+++ src/handlers/transformation/transform-csv-to-json.ts
@@ -1,15 +1,15 @@
 import { S3Event } from "aws-lambda";
-import * as AWS from "aws-sdk";
-import csv from "csvtojson";
+import getCsvConverter from "csvtojson";
+import { fetchS3 } from "../../shared/utils";
 
 // Unsure how to test this function or indeed if it even needs testing given that it basically just using S3 and the csvtojson library. Feedback/Help welcome!
+
 export async function transformCsvToJson(event: S3Event): Promise<any[]> {
-  const S3 = new AWS.S3();
-  const params = {
-    Bucket: event.Records[0].s3.bucket.name,
-    Key: event.Records[0].s3.object.key,
-  };
-  const stream = S3.getObject(params).createReadStream();
-  const rows = await csv().fromStream(stream);
-  return rows;
+  const csvString = await fetchS3(
+    event.Records[0].s3.bucket.name,
+    event.Records[0].s3.object.key
+  );
+
+  if (csvString === undefined) throw Error("No csv found");
+  return await getCsvConverter().fromString(csvString);
 }
