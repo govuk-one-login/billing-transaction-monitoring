6585cbe346c6bdf42970dd147d9d0f2f02fc7f8b
NKannan14 <nithya.kannan@digital.cabinet-office.gov.uk>
2023-02-02 14:53:26 +0000

    fixed invoice athena tests

--- integration_tests/tests/invoice-athena-tests.ts
+++ integration_tests/tests/invoice-athena-tests.ts
@@ -1,11 +1,9 @@
-import path from "path";
-import fs from "fs";
 import {
   BillingStandardised,
   checkIfS3ObjectExists,
   deleteS3Objects,
   getS3ObjectsAsArray,
-  putS3Object,
+  listS3Objects,
   S3Object,
 } from "../../src/handlers/int-test-support/helpers/s3Helper";
 import { resourcePrefix } from "../../src/handlers/int-test-support/helpers/envHelper";
@@ -14,33 +12,48 @@ import {
   startQueryExecutionCommand,
 } from "../../src/handlers/int-test-support/helpers/athenaHelper";
 
+import { createInvoiceInS3 } from "../../src/handlers/int-test-support/helpers/mock-data/invoice/helpers";
+import { waitForTrue } from "../../src/handlers/int-test-support/helpers/commonHelpers";
+import { randomInvoice } from "../../src/handlers/int-test-support/helpers/mock-data/invoice";
+
 const prefix = resourcePrefix();
+const invoice = randomInvoice();
 
 describe("\nExecute athena query to retrieve invoice data and validate that it matches invoice files in storage s3 bucket\n", () => {
   const folderPrefix = "btm_billing_standardised";
-  const testObject: S3Object = {
-    bucket: `${prefix}-storage`,
-    key: `${folderPrefix}/receipt.txt`,
-  };
+  const bucket = `${prefix}-storage`
   const databaseName = `${prefix}-calculations`;
+
+  const checkS3FolderNotEmpty = async (): Promise<boolean> => {
+    const result = await listS3Objects({
+      bucketName: bucket,
+      prefix: folderPrefix,
+    });
+    if (result.Contents === undefined || result.Contents.length === 0) {
+      console.log("S3 Folder is empty")
+      return false;
+    } else {
+      return true;
+    }
+  };
 
   beforeAll(async () => {
     await deleteS3Objects({
-      bucketName: testObject.bucket,
+      bucketName: bucket,
       prefix: folderPrefix,
     });
-    // uploading file to s3 will be removed once BTM-276 implemented
-    const file = "../payloads/receipt.txt";
-    const filePath = path.join(__dirname, file);
-    const fileData = fs.readFileSync(filePath);
-    await putS3Object({ data: fileData, target: testObject });
-    const checkFileExists = await checkIfS3ObjectExists(testObject);
-    expect(checkFileExists).toBeTruthy();
+
+    await createInvoiceInS3(invoice);
+    const checkFileExistsInStandardisedFolder = await waitForTrue(
+      checkS3FolderNotEmpty,
+      1000,
+      25000
+    );
+    expect(checkFileExistsInStandardisedFolder).toBe(true);
   });
 
   test("retrieved invoice details should match invoice data in s3 bucket", async () => {
-    const queryString =
-      'SELECT * FROM "btm_billing_standardised" ORDER BY service_name ASC';
+    const queryString = `SELECT * FROM "btm_billing_standardised" where invoice_receipt_id='${invoice.invoiceNumber}'`;
     const queryId = await startQueryExecutionCommand({
       databaseName,
       queryString,
@@ -48,9 +61,10 @@ describe("\nExecute athena query to retrieve invoice data and validate that it m
     const queryObj = await queryObject(queryId);
     const athenaQueryValues: BillingStandardised[] = Object.values(queryObj);
     const s3InvoiceValues = await getS3ObjectsAsArray(
-      testObject.bucket,
+      bucket,
       folderPrefix
     );
+
     for (let i = 0; i < s3InvoiceValues.length; i++) {
       expect(s3InvoiceValues[i].invoice_receipt_id).toEqual(
         athenaQueryValues[i].invoice_receipt_id
@@ -70,9 +84,7 @@ describe("\nExecute athena query to retrieve invoice data and validate that it m
       expect(s3InvoiceValues[i].due_date).toEqual(
         athenaQueryValues[i].due_date
       );
-      expect(s3InvoiceValues[i].item_id.toString()).toEqual(
-        athenaQueryValues[i].item_id
-      );
+
       expect(s3InvoiceValues[i].item_description).toEqual(
         athenaQueryValues[i].item_description
       );
@@ -91,27 +103,7 @@ describe("\nExecute athena query to retrieve invoice data and validate that it m
     }
   });
 
-  test("retrieved view query results should match s3", async () => {
-    const s3Response = await getS3ObjectsAsArray(
-      testObject.bucket,
-      folderPrefix
-    );
-    const s3Objects = s3Response.map(
-      (element: {
-        vendor_name: string;
-        service_name: string;
-        price: number;
-        quantity: number;
-      }) => {
-        return {
-          vendor_name: element.vendor_name,
-          service_name: element.service_name,
-          price: element.price,
-          quantity: element.quantity,
-        };
-      }
-    );
-
+  test("retrieved details from billing_curated athena view query should match with mock invoice data", async () => {
     const queryString =
       'SELECT * FROM "btm_billing_curated" ORDER BY vendor_name DESC, year DESC';
     const queryId = await startQueryExecutionCommand({
@@ -119,13 +111,17 @@ describe("\nExecute athena query to retrieve invoice data and validate that it m
       queryString,
     });
     const queryObjects: BillingCurated[] = await queryObject(queryId);
-    for (let i = 0; i < s3Response.length; i++) {
-      expect(s3Objects[i].vendor_name).toEqual(queryObjects[i].vendor_name);
-      expect(s3Objects[i].service_name).toEqual(queryObjects[i].service_name);
-      expect(s3Objects[i].quantity.toString()).toEqual(
-        queryObjects[i].quantity
-      );
-      expect(s3Objects[i].price.toFixed(4)).toEqual(queryObjects[i].price);
+    const invoiceQty = invoice.lineItems.reduce((accumulator, currentValue) => {
+      return accumulator + currentValue.quantity;
+    }, 0);
+    for (let i = 0; i < queryObjects.length; i++) {
+      expect(invoice.vendor.name).toEqual(queryObjects[i].vendor_name);
+      expect(invoiceQty.toString()).toEqual(queryObjects[i].quantity);
+      expect(invoice.getSubtotal().toFixed(4)).toEqual(queryObjects[i].price);
+     expect(invoice.date.getFullYear().toString()).toEqual(queryObjects[i].year)
+     const getMonthFromInvoiceDate= `0${(invoice.date.getMonth() + 1)}`.toString()
+     expect(getMonthFromInvoiceDate).toEqual(queryObjects[i].month)
+
     }
   });
 });
