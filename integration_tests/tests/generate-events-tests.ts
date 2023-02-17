8969b3d7af10476d94f2771719691fdeb3e9d396
NKannan14 <nithya.kannan@digital.cabinet-office.gov.uk>
2023-02-17 14:33:55 +0000

    added timestampFormatted key

--- integration_tests/tests/generate-events-tests.ts
+++ integration_tests/tests/generate-events-tests.ts
@@ -9,7 +9,8 @@ const makeMessages = (
   numberOfEvents: number,
   vendorId: VendorId,
   eventName: string,
-  humanDate: string
+  humanDate: string,
+  timestampFormatted: string
 ): PublishBatchRequestEntry[] => {
   const events = [];
   for (let i = 0; i < numberOfEvents; i++) {
@@ -25,9 +26,11 @@ const makeMessages = (
         }`,
         timestamp: new Date(humanDate).getTime() / 1000,
         event_name: eventName,
+        timestamp_formatted: timestampFormatted,
       }),
     });
   }
+  console.log(events);
   return events;
 };
 
@@ -37,30 +40,35 @@ const cases = [
     count: 11527,
     name: "VENDOR_1_EVENT_1",
     date: "2023/01/01 10:00",
+    timeStampFormatted: "2023/01/01",
   },
   {
     vendor: VendorId.vendor_testvendor1,
     count: 18052,
     name: "VENDOR_1_EVENT_1",
     date: "2023/02/01 10:00",
+    timeStampFormatted: "2023/02/01",
   },
   {
     vendor: VendorId.vendor_testvendor2,
     count: 15103,
     name: "VENDOR_2_EVENT_2",
     date: "2023/01/01 10:00",
+    timeStampFormatted: "2023/01/01",
   },
   {
     vendor: VendorId.vendor_testvendor2,
     count: 38271,
     name: "VENDOR_2_EVENT_2",
     date: "2023/02/01 10:00",
+    timeStampFormatted: "2023/02/01",
   },
   {
     vendor: VendorId.vendor_testvendor3,
     count: 5000,
     name: "VENDOR_3_EVENT_4",
     date: "2023/02/01 10:00",
+    timeStampFormatted: "2023/02/01",
   },
 
   {
@@ -68,12 +76,14 @@ const cases = [
     count: 9,
     name: "VENDOR_4_EVENT_5",
     date: "2023/01/01 10:00",
+    timeStampFormatted: "2023/01/01",
   },
   {
     vendor: VendorId.vendor_testvendor4,
     count: 7,
     name: "VENDOR_4_EVENT_5",
     date: "2023/02/01 10:00",
+    timeStampFormatted: "2023/02/01",
   },
 ];
 
@@ -100,8 +110,14 @@ describe("\nGENERATE EVENTS\n", () => {
   test("S3 EVENTS", async () => {
     const batchPromises = cases.reduce<
       Array<() => Promise<PublishCommandOutput>>
-    >((acc, { vendor, count, name, date }) => {
-      const messages = makeMessages(count, vendor, name, date);
+    >((acc, { vendor, count, name, date, timeStampFormatted }) => {
+      const messages = makeMessages(
+        count,
+        vendor,
+        name,
+        date,
+        timeStampFormatted
+      );
 
       const chunkedMessages = chunkArray(messages, 10);
       // eslint-disable-next-line @typescript-eslint/promise-function-async
