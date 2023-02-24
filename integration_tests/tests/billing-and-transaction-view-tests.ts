3356c550f363c001893e3665b08e0ad5656756ef
NKannan14 <nithya.kannan@digital.cabinet-office.gov.uk>
2023-02-24 15:13:27 +0000

    removed receipt.txt

--- integration_tests/tests/billing-and-transaction-view-tests.ts
+++ integration_tests/tests/billing-and-transaction-view-tests.ts
@@ -1,61 +1,42 @@
-import { resourcePrefix } from "../../src/handlers/int-test-support/helpers/envHelper";
+import {
+  configStackName,
+  resourcePrefix,
+} from "../../src/handlers/int-test-support/helpers/envHelper";
 import {
   deleteS3Events,
   eventTimeStamp,
   generatePublishAndValidateEvents,
-  poll,
   TableNames,
   TimeStamps,
 } from "../../src/handlers/int-test-support/helpers/commonHelpers";
-import {
-  deleteS3Objects,
-  putS3Object,
-  S3Object,
-  listS3Objects,
-} from "../../src/handlers/int-test-support/helpers/s3Helper";
-import path from "path";
-import fs from "fs";
+import { deleteS3Objects } from "../../src/handlers/int-test-support/helpers/s3Helper";
 import {
   EventName,
   VendorId,
 } from "../../src/handlers/int-test-support/helpers/payloadHelper";
 import { queryResponseFilterByVendorServiceNameYear } from "../../src/handlers/int-test-support/helpers/queryHelper";
+import {
+  randomLineItem,
+  randomInvoice,
+} from "../../src/handlers/int-test-support/helpers/mock-data/invoice/random";
+import { getVendorServiceConfigRow } from "../../src/shared/utils/config-utils";
+import { createInvoiceInS3 } from "../../src/handlers/int-test-support/helpers/mock-data/invoice/helpers";
 
 const prefix = resourcePrefix();
 const bucketName = `${prefix}-storage`;
 
 describe("\nUpload invoice to standardised folder and verify billing and transaction_curated view query results matches with expected data \n", () => {
-  const folderPrefix = "btm_billing_standardised";
-  const testObject: S3Object = {
-    bucket: `${prefix}-storage`,
-    key: `${folderPrefix}/receipt.txt`,
-  };
   beforeAll(async () => {
     // tests are enabled to run sequentially as we are deleting the S3 directory in view tests so when running the test
     // in parallel other tests will be interrupted(e.g. sns-s3 tests generate and checks eventId). We can enable to run in parallel
     // once we implement BTM-340 to clean up after each test
     await deleteS3Objects({ bucketName, prefix: "btm_transactions" });
-    // uploading file to s3 will be removed once BTM-276 changes merged
-    const file = "../payloads/receipt.txt";
-    const filePath = path.join(__dirname, file);
-    const fileData = fs.readFileSync(filePath);
-    await putS3Object({ data: fileData, target: testObject });
-    await poll(
-      async () => await listS3Objects({ bucketName, prefix: testObject.key }),
-      (results) => Boolean(results.Contents?.length)
-    );
+    await deleteS3Objects({ bucketName, prefix: "btm_billing_standardised" });
   });
 
   test.each`
-    testCase                                                                                 | eventName             | vendorId                | eventTime                         | numberOfTestEvents | priceDiff     | qtyDiff | priceDifferencePercent | qtyDifferencePercent | billingPrice | billingQty | transactionPrice | transactionQty
-    ${"BillingQty BillingPrice equals TransactionQty and TransactionPrice"}                  | ${"VENDOR_1_EVENT_1"} | ${"vendor_testvendor1"} | ${TimeStamps.CURRENT_TIME}        | ${"2"}             | ${"0.0000"}   | ${"0"}  | ${"0.0000"}            | ${"0"}               | ${"6.6600"}  | ${"2"}     | ${"6.6600"}      | ${"2"}
-    ${"BillingQty BillingPrice greater than TransactionQty and TransactionPrice"}            | ${"VENDOR_1_EVENT_1"} | ${"vendor_testvendor1"} | ${TimeStamps.CURRENT_TIME}        | ${"1"}             | ${"3.3300"}   | ${"1"}  | ${"100.0000"}          | ${"100"}             | ${"6.6600"}  | ${"2"}     | ${"3.3300"}      | ${"1"}
-    ${"BillingQty BillingPrice less than TransactionQty and TransactionPrice"}               | ${"VENDOR_1_EVENT_1"} | ${"vendor_testvendor1"} | ${TimeStamps.CURRENT_TIME}        | ${"3"}             | ${"-3.3300"}  | ${"-1"} | ${"-33.3333"}          | ${"-33"}             | ${"6.6600"}  | ${"2"}     | ${"9.9900"}      | ${"3"}
-    ${"No TransactionQty No TransactionPrice(no events) but has BillingQty BillingPrice"}    | ${"VENDOR_1_EVENT_1"} | ${"vendor_testvendor1"} | ${TimeStamps.CURRENT_TIME}        | ${undefined}       | ${"6.6600"}   | ${"2"}  | ${undefined}           | ${undefined}         | ${"6.6600"}  | ${"2"}     | ${undefined}     | ${undefined}
-    ${"BillingQty less than TransactionQty and No BillingPrice but has TransactionPrice "}   | ${"VENDOR_4_EVENT_5"} | ${"vendor_testvendor4"} | ${TimeStamps.CURRENT_TIME}        | ${11}              | ${"-27.5000"} | ${"-9"} | ${"-100.0000"}         | ${"-81"}             | ${"0.0000"}  | ${"2"}     | ${"27.5000"}     | ${"11"}
-    ${"BillingQty equals TransactionQty and No TransactionPrice No BillingPrice "}           | ${"VENDOR_4_EVENT_5"} | ${"vendor_testvendor4"} | ${TimeStamps.CURRENT_TIME}        | ${2}               | ${"0.0000"}   | ${"0"}  | ${"0.0000"}            | ${"0"}               | ${"0.0000"}  | ${"2"}     | ${"0.0000"}      | ${"2"}
-    ${"BillingQty greater than TransactionQty and No TransactionPrice but has BillingPrice"} | ${"VENDOR_4_EVENT_5"} | ${"vendor_testvendor4"} | ${TimeStamps.THIS_TIME_LAST_YEAR} | ${2}               | ${"27.5000"}  | ${"9"}  | ${undefined}           | ${"450"}             | ${"27.5000"} | ${"11"}    | ${"0.0000"}      | ${"2"}
-    ${"BillingQty equals TransactionQty but BillingPrice greater than TransactionPrice"}     | ${"VENDOR_1_EVENT_1"} | ${"vendor_testvendor1"} | ${TimeStamps.THIS_TIME_LAST_YEAR} | ${2}               | ${"4.2000"}   | ${"0"}  | ${"170.7317"}          | ${"0"}               | ${"6.6600"}  | ${"2"}     | ${"2.4600"}      | ${"2"}
+    testCase                                                                | eventName             | vendorId                | eventTime                  | unitPrice | numberOfTestEvents | priceDiff   | qtyDiff | priceDifferencePercent | qtyDifferencePercent | billingPrice | billingQty | transactionPrice | transactionQty
+    ${"BillingQty BillingPrice equals TransactionQty and TransactionPrice"} | ${"VENDOR_1_EVENT_1"} | ${"vendor_testvendor1"} | ${TimeStamps.CURRENT_TIME} | ${"3.33"} | ${"2"}             | ${"0.0000"} | ${"0"}  | ${"0.0000"}            | ${"0"}               | ${"6.6600"}  | ${"2"}     | ${"6.6600"}      | ${"2"}
   `(
     "results retrieved from billing and transaction_curated view query should match with expected $testCase,$billingQty,$priceDiff,$qtyDiff,$priceDifferencePercent,$qtyDifferencePercent,$billingPrice",
     async (data) => {
@@ -64,7 +45,7 @@ describe("\nUpload invoice to standardised folder and verify billing and transac
   );
 });
 
-describe("\n no invoice uploaded to standardised folder and verify billing and transaction_curated view query results matches with expected data    \n", () => {
+describe.skip("\n no invoice uploaded to standardised folder and verify billing and transaction_curated view query results matches with expected data    \n", () => {
   beforeAll(async () => {
     await deleteS3Objects({ bucketName, prefix: "btm_billing_standardised" });
     await deleteS3Objects({ bucketName, prefix: "btm_transactions" });
@@ -84,13 +65,14 @@ interface TestData {
   eventName: EventName;
   vendorId: VendorId;
   eventTime: TimeStamps;
+  unitPrice: number;
   numberOfTestEvents: number;
   priceDiff: string;
   qtyDiff: string;
   priceDifferencePercent: string;
   qtyDifferencePercent: string;
   billingPrice: string;
-  billingQty: string;
+  billingQty: number;
   transactionPrice: string;
   transactionQty: string;
 }
@@ -99,6 +81,7 @@ export const assertResultsWithTestData = async ({
   eventName,
   vendorId,
   eventTime,
+  unitPrice,
   numberOfTestEvents,
   priceDiff,
   qtyDiff,
@@ -109,6 +92,39 @@ export const assertResultsWithTestData = async ({
   transactionPrice,
   transactionQty,
 }: TestData): Promise<void> => {
+  const givenVendorId = vendorId;
+  const givenEventName = eventName;
+  const givenBillingQty = billingQty;
+  console.log(billingQty);
+  console.log(givenBillingQty);
+
+  const lineItems = randomLineItem({
+    description: "passport check",
+    quantity: givenBillingQty,
+    unitPrice,
+  });
+  console.log(lineItems);
+
+  const vendorServiceConfigRow = await getVendorServiceConfigRow(
+    configStackName(),
+    { vendor_id: givenVendorId, event_name: givenEventName }
+  );
+
+  const givenInvoice = randomInvoice({
+    vendor: {
+      name: vendorServiceConfigRow.vendor_name,
+    },
+    lineItems: [lineItems],
+  });
+  console.log(givenInvoice.getSubtotal("passport check"));
+
+  const filename = `raw-Invoice-${Math.random()
+    .toString(36)
+    .substring(2, 7)}-validFile.pdf`;
+  console.log(filename);
+  console.log(givenInvoice);
+  await createInvoiceInS3({ invoiceData: givenInvoice, filename });
+
   const eventIds = await generatePublishAndValidateEvents({
     numberOfTestEvents,
     eventName,
--- integration_tests/tests/s3-invoice-end-to-end-tests.ts
+++ integration_tests/tests/s3-invoice-end-to-end-tests.ts
@@ -2,13 +2,23 @@ import path from "path";
 import { resourcePrefix } from "../../src/handlers/int-test-support/helpers/envHelper";
 import {
   checkIfS3ObjectExists,
-  deleteS3Object, deleteS3Objects,
+  deleteS3Object,
+  deleteS3Objects,
   listS3Objects,
 } from "../../src/handlers/int-test-support/helpers/s3Helper";
-import {poll, waitForTrue} from "../../src/handlers/int-test-support/helpers/commonHelpers";
-import { randomInvoice, randomLineItems } from "../../src/handlers/int-test-support/helpers/mock-data/invoice";
+import {
+  poll,
+  waitForTrue,
+} from "../../src/handlers/int-test-support/helpers/commonHelpers";
+import {
+  randomInvoice,
+  randomLineItems,
+} from "../../src/handlers/int-test-support/helpers/mock-data/invoice";
 import { createInvoiceInS3 } from "../../src/handlers/int-test-support/helpers/mock-data/invoice/helpers";
-import {queryObject, startQueryExecutionCommand} from "../../src/handlers/int-test-support/helpers/athenaHelper";
+import {
+  queryObject,
+  startQueryExecutionCommand,
+} from "../../src/handlers/int-test-support/helpers/athenaHelper";
 
 const prefix = resourcePrefix();
 const testStartTime = new Date();
@@ -26,21 +36,34 @@ describe("\n Happy path - Upload valid mock invoice pdf and verify data is seen 
   });
 
   test("upload valid pdf file in raw-invoice-pdf bucket and see that we can see the data in the view", async () => {
-    const passportCheckItems = randomLineItems(8, { description: "passport check"});
-    const addressCheckItems = randomLineItems(10, { description: "address check"});
-    const invoice = randomInvoice({ lineItems: [ ...passportCheckItems, ...addressCheckItems ]});
-    const expectedSubtotals = [ invoice.getSubtotal("address check"), invoice.getSubtotal("passport check") ];
-    const expectedQuantities = [ invoice.getQuantity("address check"), invoice.getQuantity("passport check") ];
-    const expectedServices = [ "Address check", "Passport check" ];
+    const passportCheckItems = randomLineItems(8, {
+      description: "passport check",
+    });
+    const addressCheckItems = randomLineItems(10, {
+      description: "address check",
+    });
+    const invoice = randomInvoice({
+      lineItems: [...passportCheckItems, ...addressCheckItems],
+    });
+    const expectedSubtotals = [
+      invoice.getSubtotal("address check"),
+      invoice.getSubtotal("passport check"),
+    ];
+    const expectedQuantities = [
+      invoice.getQuantity("address check"),
+      invoice.getQuantity("passport check"),
+    ];
+    const expectedServices = ["Address check", "Passport check"];
     const filename = `raw-Invoice-${Math.random()
       .toString(36)
       .substring(2, 7)}-validFile`;
     const s3Object = await createInvoiceInS3({
       invoiceData: invoice,
-      filename: `${filename}.pdf`});
+      filename: `${filename}.pdf`,
+    });
+    console.log("e2e test invoice filename:", filename);
     const checkRawPdfFileExists = await checkIfS3ObjectExists(s3Object);
     expect(checkRawPdfFileExists).toBeTruthy();
-
 
     // Wait for the invoice data to have been written, to some file in the standardised folder.
     await poll(
@@ -71,19 +94,20 @@ describe("\n Happy path - Upload valid mock invoice pdf and verify data is seen 
     });
     const queryObjects: BillingCurated[] = await queryObject(queryId);
     expect(queryObjects.length).toEqual(2);
-    queryObjects.sort((q0, q1) => { return q0.service_name.localeCompare(q1.service_name) })
-    for (let i=0; i<queryObjects.length; i++) {
+    queryObjects.sort((q0, q1) => {
+      return q0.service_name.localeCompare(q1.service_name);
+    });
+    for (let i = 0; i < queryObjects.length; i++) {
       expect(invoice.vendor.name).toEqual(queryObjects[i].vendor_name);
       expect(expectedServices[i]).toEqual(queryObjects[i].service_name);
       expect(invoice.date.getFullYear()).toEqual(+queryObjects[i].year);
-      expect(invoice.date.getMonth()+1).toEqual(+queryObjects[i].month);
+      expect(invoice.date.getMonth() + 1).toEqual(+queryObjects[i].month);
       expect(queryObjects[i].quantity.toString()).toEqual(
         queryObjects[i].quantity
       );
       expect(queryObjects[i].price).toMatch(expectedSubtotals[i].toFixed(2));
       expect(+queryObjects[i].quantity).toEqual(expectedQuantities[i]);
     }
-
 
     // Check that the invoice got moved to the success folder.
     const isFileMovedToSuccessfulFolder = async (): Promise<boolean> => {
