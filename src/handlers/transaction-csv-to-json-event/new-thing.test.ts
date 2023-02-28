1a95ef0c01d2d0fe03889d9465bd55146f7ddc5f
Mark Potter <si3rr4wow@gmail.com>
2023-02-28 15:52:05 +0000

    transform stuff

--- src/handlers/transaction-csv-to-json-event/new-thing.test.ts
+++ src/handlers/transaction-csv-to-json-event/new-thing.test.ts
@@ -1,4 +1,13 @@
-import { convert, infer, orchestrate, Inference } from "./new-thing";
+import {
+  convert,
+  infer,
+  orchestrate,
+  Inference,
+  Transformations,
+  Operations,
+  Constructables,
+  transform,
+} from "./new-thing";
 
 // using a type here because interface won't be interpreted
 // as an extension of Record<string, unknown>
@@ -114,6 +123,154 @@ describe("infer", () => {
           color: "red",
           isOriginal: false,
           eyelashLength: 12,
+        },
+      ]);
+    });
+  });
+});
+
+describe("transform", () => {
+  describe("given an array of objects and a set of transformations", () => {
+    it("performs the transformations", () => {
+      type AugmentedHoover = RawHoover & {
+        eyelashLength: number;
+        isOriginal: boolean;
+        dateOfManufacture: string | number;
+      };
+      const givenArray: AugmentedHoover[] = [
+        {
+          id: "1",
+          name: "henry",
+          suction: "135000",
+          color: "red",
+          isOriginal: true,
+          eyelashLength: 12,
+          dateOfManufacture: "2022-11-01T00:27:41.186Z",
+        },
+        {
+          id: "2",
+          name: "henrietta",
+          suction: "155000",
+          color: "pink",
+          isOriginal: false,
+          eyelashLength: 16,
+          dateOfManufacture: "2022-11-01T00:27:41.186Z",
+        },
+        {
+          id: "3",
+          name: "clive",
+          suction: "135000",
+          color: "red",
+          isOriginal: false,
+          eyelashLength: 12,
+          dateOfManufacture: "1667262461186",
+        },
+        {
+          id: "4",
+          name: "sonny",
+          suction: "145000",
+          color: "blue",
+          isOriginal: false,
+          eyelashLength: 6,
+          dateOfManufacture: "1667262461",
+        },
+      ];
+
+      const givenTransformations: Transformations<
+        AugmentedHoover,
+        { timestampOfManufacture: number }
+      > = [
+        {
+          inputKey: "dateOfManufacture",
+          outputKey: "timestampOfManufacture",
+          condition: /\d+-\d+-\d+T\d+:\d+:\d+.\d+Z/,
+          operations: [
+            {
+              operation: Operations.construct,
+              parameter: Constructables.date,
+            },
+            {
+              operation: Operations.multiply,
+              parameter: 0.001,
+            },
+            {
+              operation: Operations.floor,
+            },
+          ],
+        },
+        {
+          inputKey: "dateOfManufacture",
+          outputKey: "timestampOfManufacture",
+          condition: /^\d{13}$/,
+          operations: [
+            {
+              operation: Operations.construct,
+              parameter: Constructables.number,
+            },
+            {
+              operation: Operations.multiply,
+              parameter: 0.001,
+            },
+            {
+              operation: Operations.floor,
+            },
+          ],
+        },
+        {
+          inputKey: "dateOfManufacture",
+          outputKey: "timestampOfManufacture",
+          condition: /^\d{10}$/,
+          operations: [
+            {
+              operation: Operations.construct,
+              parameter: Constructables.number,
+            },
+          ],
+        },
+      ];
+
+      const result = transform(givenArray, givenTransformations);
+
+      expect(result).toEqual([
+        {
+          id: "1",
+          name: "henry",
+          suction: "135000",
+          color: "red",
+          isOriginal: true,
+          eyelashLength: 12,
+          dateOfManufacture: "2022-11-01T00:27:41.186Z",
+          timestampOfManufacture: 1667262461,
+        },
+        {
+          id: "2",
+          name: "henrietta",
+          suction: "155000",
+          color: "pink",
+          isOriginal: false,
+          eyelashLength: 16,
+          dateOfManufacture: "2022-11-01T00:27:41.186Z",
+          timestampOfManufacture: 1667262461,
+        },
+        {
+          id: "3",
+          name: "clive",
+          suction: "135000",
+          color: "red",
+          isOriginal: false,
+          eyelashLength: 12,
+          dateOfManufacture: "1667262461186",
+          timestampOfManufacture: 1667262461,
+        },
+        {
+          id: "4",
+          name: "sonny",
+          suction: "145000",
+          color: "blue",
+          isOriginal: false,
+          eyelashLength: 6,
+          dateOfManufacture: "1667262461",
+          timestampOfManufacture: 1667262461,
         },
       ]);
     });
--- src/handlers/transaction-csv-to-json-event/new-thing.ts
+++ src/handlers/transaction-csv-to-json-event/new-thing.ts
@@ -45,7 +45,7 @@ type Infer = <
   TObject extends Record<string, unknown>,
   TFields extends Record<string, unknown>
 >(
-  array: TObject[],
+  entries: TObject[],
   inferences: Inferences<TObject, TFields>
 ) => Array<TObject & TFields>;
 
@@ -53,10 +53,10 @@ export const infer: Infer = <
   TObject extends Record<string, unknown>,
   TFields extends Record<string, unknown>
 >(
-  array: TObject[],
+  entries: TObject[],
   inferences: Array<Inference<TObject, TFields>>
 ): Array<TObject & TFields> => {
-  return array.map((entry) => {
+  return entries.map((entry) => {
     return inferences.reduce<TObject & TFields>(
       (augmentedEntry: any, { field, rules, defaultValue }) => {
         const value = rules.reduce((inferredValue, rule) => {
@@ -75,6 +75,177 @@ export const infer: Infer = <
     );
   });
 };
+
+export enum Operations {
+  add = "add",
+  multiply = "multiply",
+  exponentiate = "exponentiate",
+  floor = "floor",
+  ceil = "ceil",
+  construct = "construct",
+}
+
+export enum Constructables {
+  date = "date",
+  number = "number",
+}
+
+interface Transformation<
+  TObject extends Record<string, unknown>,
+  TFields extends Record<string, unknown>
+> {
+  inputKey: keyof TObject;
+  outputKey: keyof TFields;
+  condition: RegExp;
+  operations: Array<{
+    operation: Operations;
+    parameter?: number | Constructables;
+  }>;
+}
+
+export type Transformations<
+  TObject extends Record<string, unknown>,
+  TFields extends Record<string, unknown>
+> = Array<Transformation<TObject, TFields>>;
+
+type InteroperablyTransformable = string | number | Date;
+
+const constructablesDict = {
+  [Constructables.date]: (input: InteroperablyTransformable) => new Date(input),
+  [Constructables.number]: (input: InteroperablyTransformable) =>
+    Number(input).valueOf(),
+};
+
+const operationsDict: Record<
+  Operations,
+  (
+    input: InteroperablyTransformable,
+    parameter?: string | number
+  ) => string | number | Date
+> = {
+  [Operations.add]: (input, parameter) => {
+    if (typeof parameter !== "number") {
+      throw new Error("Invalid parameter");
+    }
+    if (input instanceof Date) {
+      return input.getTime() + parameter;
+    }
+    if (typeof input !== "number") {
+      throw new Error("Invalid input");
+    }
+    return input + parameter;
+  },
+  [Operations.multiply]: (input, parameter) => {
+    if (typeof parameter !== "number") {
+      throw new Error("Invalid parameter");
+    }
+    if (input instanceof Date) {
+      return input.getTime() * parameter;
+    }
+    if (typeof input !== "number") {
+      throw new Error("Invalid input");
+    }
+    return input * parameter;
+  },
+  [Operations.exponentiate]: (input, parameter) => {
+    if (typeof parameter !== "number") {
+      throw new Error("Invalid parameter");
+    }
+    if (input instanceof Date) {
+      return input.getTime() ** parameter;
+    }
+    if (typeof input !== "number") {
+      throw new Error("Invalid input");
+    }
+    return input ** parameter;
+  },
+  [Operations.floor]: (input) => {
+    if (input instanceof Date) {
+      return input.getTime();
+    }
+    if (typeof input !== "number") {
+      throw new Error("Invalid input");
+    }
+    return Math.floor(input);
+  },
+  [Operations.ceil]: (input) => {
+    if (input instanceof Date) {
+      return input.getTime();
+    }
+    if (typeof input !== "number") {
+      throw new Error("Invalid input");
+    }
+    return Math.ceil(input);
+  },
+  [Operations.construct]: (input, parameter) => {
+    if (
+      !parameter ||
+      typeof parameter !== "string" ||
+      !(parameter in Constructables)
+    )
+      throw new Error("Invalid parameter");
+    return constructablesDict[parameter as Constructables](input);
+  },
+};
+
+const shouldOperate = <TObject extends Record<string, unknown>>(
+  value: TObject[keyof TObject],
+  condition: RegExp
+): boolean => !!String(value).match(condition);
+
+const operate = <
+  TObject extends Record<string, unknown>,
+  TFields extends Record<string, unknown>
+>(
+  entry: TObject,
+  {
+    inputKey,
+    outputKey,
+    condition,
+    operations,
+  }: Transformation<TObject, TFields>
+): TObject & { [outputKey: string]: InteroperablyTransformable } => {
+  const inputValue = entry[inputKey] as unknown as InteroperablyTransformable;
+  if (!shouldOperate(entry[inputKey], condition)) {
+    return {
+      ...entry,
+      [outputKey]: inputValue,
+    };
+  }
+
+  let outputValueV2: InteroperablyTransformable = inputValue;
+  for (const { operation, parameter } of operations) {
+    outputValueV2 = operationsDict[operation](outputValueV2, parameter);
+  }
+
+  // const outputValue = operations.reduce<InteroperablyTransformable>(
+  //   (transformedValue, { operation, parameter }) => {
+  //     return operationsDict[operation](transformedValue, parameter);
+  //   },
+  //   inputValue
+  // );
+  return {
+    ...entry,
+    [outputKey]: outputValueV2,
+  };
+};
+
+export const transform = <
+  TObject extends Record<string, unknown>,
+  TFields extends Record<string, unknown>
+>(
+  entries: TObject[],
+  transformations: Transformations<TObject, TFields>
+): Array<TObject & TFields> =>
+  entries.map<TObject & TFields>((entry) =>
+    transformations.reduce<TObject & TFields>(
+      (transformedEntry, transformation) => {
+        return operate(transformedEntry, transformation);
+      },
+      // eslint-disable-next-line @typescript-eslint/prefer-reduce-type-parameter
+      entry as TObject & TFields
+    )
+  );
 
 export interface CsvOptions<
   TObject extends Record<string, unknown>,
