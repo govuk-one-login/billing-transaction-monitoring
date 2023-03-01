7fbcf4c7d5f4e5a890628e45e4aec2239a41a774
Mark Potter <si3rr4wow@gmail.com>
2023-03-01 13:09:49 +0000

    reqire handler

--- src/handlers/transaction-csv-to-json-event/handler.ts
+++ src/handlers/transaction-csv-to-json-event/handler.ts
@@ -1,9 +1,6 @@
 import { S3Event } from "aws-lambda";
 import { fetchS3, sendRecord } from "../../shared/utils";
-
-import { transformCsvToJson } from "./transform-csv-to-json";
-import { processRow } from "./process-row";
-import { getEventNameFromRules } from "./get-event-name-from-rules";
+import { convert } from "./convert";
 
 export const handler = async (event: S3Event): Promise<void> => {
   const outputQueueUrl = process.env.OUTPUT_QUEUE_URL;
@@ -18,35 +15,32 @@ export const handler = async (event: S3Event): Promise<void> => {
   }
 
   try {
-    // Set up dependencies
-
-    const [idpVendorLookup, eventNameRules] = await Promise.all([
-      fetchS3(configBucket, "idp_vendors/idp-vendors.json"),
-      fetchS3(configBucket, "idp_event_name_rules/idp-event-name-rules.json"),
+    const [csv, renamingMap, inferences, transformations] = await Promise.all([
+      fetchS3(event.Records[0].s3.bucket.name, event.Records[0].s3.object.key), // should we we getting all the records or is the zeroth one ok?
+      fetchS3(configBucket, "csv_transactions/renaming-map.json"),
+      fetchS3(configBucket, "csv_transactions/inferences.json"),
+      fetchS3(configBucket, "csv_transactions/transformations.json"),
     ]).then((results) => results.map((result) => JSON.parse(result)));
 
-    const source = await transformCsvToJson(event);
+    const transactions = await convert(csv, {
+      renamingMap,
+      inferences,
+      transformations,
+    });
 
-    // Transform data and send to SQS
-    const promises = source.reduce<Array<Promise<void>>>((accumulator, row) => {
-      const isInvalidEvent =
-        getEventNameFromRules(eventNameRules, row["Idp Entity Id"], row) ===
-        "Unknown";
-      if (isInvalidEvent) return accumulator;
-      const transactionEvent = processRow(row, idpVendorLookup, eventNameRules);
-      return [
-        ...accumulator,
-        sendRecord(outputQueueUrl, JSON.stringify(transactionEvent)),
-      ];
-    }, []);
+    const sendRecordPromises = transactions.map(
+      async (transaction) =>
+        await sendRecord(outputQueueUrl, JSON.stringify(transaction))
+    );
 
-    const results = await Promise.allSettled(promises);
-    if (results.some((result) => result.status === "rejected")) {
-      throw new Error(
-        `Failed to process all rows: ${
-          results.filter((result) => result.status === "rejected").length
-        } out of ${results.length} failed`
-      );
+    const results = await Promise.allSettled(sendRecordPromises);
+    for (const result of results) {
+      if (result.status === "rejected")
+        throw new Error(
+          `Failed to process all rows: ${
+            results.filter((result) => result.status === "rejected").length
+          } out of ${results.length} failed`
+        );
     }
   } catch (error) {
     console.error(error);
