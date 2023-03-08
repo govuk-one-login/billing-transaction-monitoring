ad48b572f4e03fae41664b02a9ae4c3692ee29c6
NKannan14 <nithya.kannan@digital.cabinet-office.gov.uk>
2023-03-08 20:11:27 +0000

    added invokeLambda function and refactored sendLambda

--- src/handlers/int-test-support/helpers/lambdaHelper.ts
+++ src/handlers/int-test-support/helpers/lambdaHelper.ts
@@ -12,22 +12,18 @@ export const sendLambdaCommand = async <THelper extends IntTestHelpers>(
   command: THelper,
   parameters: Parameters<HelperDict[THelper]>[0]
 ): Promise<string> => {
-  const commandInput: InvokeCommandInput = {
-    FunctionName: `${resourcePrefix()}-int-test-support-function`,
-    InvocationType: "RequestResponse",
-    LogType: "Tail",
-    Payload: fromUtf8(
-      JSON.stringify({
-        environment: envName(),
-        config: configName(),
-        command,
-        parameters,
-      })
-    ),
-  };
+  const payload = JSON.stringify({
+    environment: envName(),
+    config: configName(),
+    command,
+    parameters,
+  });
 
   // console.log(toUtf8(commandInput.Payload as Uint8Array));
-  const result = await lambdaClient.send(new InvokeCommand(commandInput));
+  const result = await invokeLambda(
+    `${resourcePrefix()}-int-test-support-function`,
+    payload
+  );
   // console.log(toUtf8(result.Payload as Uint8Array));
 
   if (result.StatusCode === 200 && result.Payload != null) {
@@ -38,14 +34,15 @@ export const sendLambdaCommand = async <THelper extends IntTestHelpers>(
 };
 
 export const invokeLambda = async (
+  functionName: string,
   payload: string
 ): Promise<InvocationResponse> => {
-  const params = {
-    FunctionName: `${resourcePrefix()}-filter-function`,
+  const command: InvokeCommandInput = {
+    FunctionName: functionName,
     InvocationType: "RequestResponse",
+    LogType: "Tail",
     Payload: fromUtf8(payload),
   };
-  const command: InvokeCommandInput = params;
   try {
     const response = await lambdaClient.send(new InvokeCommand(command));
     return response;
