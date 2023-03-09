a71bafa4bc8c8fa600f1de310c78e47551bece24
Melissa Pereira <melissa.pereira@digital.cabinet-office.gov.uk>
2023-03-09 17:16:13 +0000

    Set up test

---
+++ src/handlers/csv-extract/get-csv-standardised-invoice.test.ts
@@ -0,0 +1,5 @@
+describe("Get CSV Standardised Invoice", () => {
+  test("should throw an error if the csvObject is not in the expected format", () => {});
+
+  test("should build up the standardised invoice with a valid csvObject", () => {});
+});
