dad3e905157c70ccad4fcef8333fd2b19a293485
Melissa Pereira <melissa.pereira@digital.cabinet-office.gov.uk>
2023-03-10 17:22:59 +0000

    Complete getCsvStandardisedInvoice test

--- src/handlers/csv-extract/get-csv-standardised-invoice.test.ts
+++ src/handlers/csv-extract/get-csv-standardised-invoice.test.ts
@@ -1,10 +1,107 @@
-describe("Get CSV Standardised Invoice", () => {
-  // TODO Not needed if the type guard is in place in the handler
-  test("should throw an error if the csvObject is not in the expected format", () => {});
+import { VendorServiceConfigRows } from "../../shared/utils";
+import {
+  CsvObject,
+  getCsvStandardisedInvoice,
+} from "./get-csv-standardised-invoice";
 
-  test("should build up the standardised invoice with a valid csvObject with one line item", () => {});
+describe("CSV Standardised invoice getter", () => {
+  let givenCsvObject: CsvObject;
+  let givenVendorServiceConfigRows: VendorServiceConfigRows;
 
-  test("should build up the standardised invoice with a valid csvObject with multiple line items", () => {});
+  beforeEach(() => {
+    jest.resetAllMocks();
 
-  // TODO Is a test needed that will infer the sevice name based on the regex?
+    givenCsvObject = {
+      Vendor: "Vendor One",
+      "Invoice Date": "2023/02/28",
+      "Due Date": "2023/03/28",
+      "VAT Number": "123 4567 89",
+      "PO Number": "370 000",
+      Version: "1.1.1",
+      lineItems: [
+        {
+          "Service Name": "Check one",
+          "Unit Price": "0.34",
+          Quantity: "13788",
+          Tax: "937.584",
+          Subtotal: "4687.92",
+          Total: "5625.504",
+        },
+        {
+          "Service Name": "Check two",
+          "Unit Price": "3.95",
+          Quantity: "83",
+          Tax: "65.57",
+          Subtotal: "327.85",
+          Total: "393.42",
+        },
+        {
+          "Service Name": "Check three",
+          "Unit Price": "20",
+          Quantity: "3",
+          Tax: "12",
+          Subtotal: "60",
+          Total: "72",
+        },
+      ],
+    };
+
+    givenVendorServiceConfigRows = [
+      {
+        vendor_name: "Vendor One",
+        vendor_id: "vendor_testvendor1",
+        service_name: "Check one",
+        service_regex: "Check one",
+        event_name: "VENDOR_1_EVENT_1",
+      },
+      {
+        vendor_name: "Vendor One",
+        vendor_id: "vendor_testvendor1",
+        service_name: "Check two",
+        service_regex: "Check two",
+        event_name: "VENDOR_1_EVENT_3",
+      },
+    ];
+  });
+
+  test("should return StandardisedLineItems when given a valid CsvObject that has 3 line items, with two that have service_names in the vendor service config", () => {
+    const result = getCsvStandardisedInvoice(
+      givenCsvObject,
+      "vendor_testvendor1",
+      givenVendorServiceConfigRows
+    );
+
+    expect(result).toEqual([
+      {
+        invoice_receipt_id: "370 000",
+        vendor_id: "vendor_testvendor1",
+        vendor_name: "Vendor One",
+        invoice_receipt_date: "2023-02-28",
+        due_date: "2023-03-28",
+        tax_payer_id: "123 4567 89",
+        parser_version: "1.1.1",
+        item_description: "Check one",
+        subtotal: 4687.92,
+        quantity: 13788,
+        service_name: "Check one",
+        unit_price: 0.34,
+        total: 5625.504,
+      },
+      {
+        invoice_receipt_id: "370 000",
+        vendor_id: "vendor_testvendor1",
+        vendor_name: "Vendor One",
+        invoice_receipt_date: "2023-02-28",
+        due_date: "2023-03-28",
+        tax_payer_id: "123 4567 89",
+        parser_version: "1.1.1",
+        item_description: "Check two",
+        subtotal: 327.85,
+        quantity: 83,
+        service_name: "Check two",
+        unit_price: 3.95,
+        total: 393.42,
+      },
+    ]);
+  });
 });
--- src/handlers/csv-extract/get-csv-standardised-invoice.ts
+++ src/handlers/csv-extract/get-csv-standardised-invoice.ts
@@ -1,6 +1,4 @@
 import { VendorServiceConfigRows } from "../../shared/utils";
-import { getNumberFromMoneyText } from "../store-standardised-invoices/field-utils/get-number-from-money-text";
-import { getStandardisedDateText } from "../store-standardised-invoices/field-utils/get-standardised-date-text";
 import { StandardisedLineItem } from "../store-standardised-invoices/get-standardised-invoice";
 
 export interface LineItem {
@@ -31,15 +29,14 @@ export const getCsvStandardisedInvoice = (
     invoice_receipt_id: csvObject["PO Number"],
     vendor_id: vendorId,
     vendor_name: csvObject.Vendor,
-    invoice_receipt_date: getStandardisedDateText(csvObject["Invoice Date"]),
-    due_date: getStandardisedDateText(csvObject["Due Date"]),
+    invoice_receipt_date: formatDate(csvObject["Invoice Date"]),
+    due_date: formatDate(csvObject["Due Date"]),
     tax_payer_id: csvObject["VAT Number"],
     parser_version: csvObject.Version,
   };
 
   return csvObject.lineItems.reduce<StandardisedLineItem[]>((acc, item) => {
     const itemDescription = item["Service Name"];
-
     let nextAcc = [...acc];
 
     for (const {
@@ -47,7 +44,7 @@ export const getCsvStandardisedInvoice = (
       service_regex: serviceRegexPattern,
     } of vendorServiceConfigRows) {
       const serviceRegex = new RegExp(serviceRegexPattern, "i");
-
+      console.log(serviceRegex);
       if (!itemDescription?.match(serviceRegex)) {
         continue;
       }
@@ -57,15 +54,22 @@ export const getCsvStandardisedInvoice = (
         {
           ...summary,
           item_description: itemDescription,
-          subtotal: getNumberFromMoneyText(item.Subtotal),
+          subtotal: Number(item.Subtotal),
           quantity: Number(item.Quantity),
           service_name: serviceName,
-          unit_price: getNumberFromMoneyText(item["Unit Price"]),
-          total: getNumberFromMoneyText(item.Total),
+          unit_price: Number(item["Unit Price"]),
+          total: Number(item.Total),
         },
       ];
     }
-
     return nextAcc;
   }, []);
 };
+
+function formatDate(dateStr: string): string {
+  const date = new Date(dateStr);
+  const year = date.getFullYear();
+  const month = `${(date.getMonth() + 1).toString().padStart(2, "0")}`; // Add leading zero if necessary
+  const day = `${date.getDate().toString().padStart(2, "0")}`; // Add leading zero if necessary
+  return `${year}-${month}-${day}`;
+}
