c9ac243d9cb16012bf22f695d413e3a6c8469532
Melissa Pereira <melissa.pereira@digital.cabinet-office.gov.uk>
2023-03-13 14:16:33 +0000

    Add hardcoded integration test

--- integration_tests/tests/s3-invoice-end-to-end-tests.ts
+++ integration_tests/tests/s3-invoice-end-to-end-tests.ts
@@ -5,6 +5,8 @@ import {
   deleteS3Object,
   deleteS3Objects,
   listS3Objects,
+  putS3Object,
+  S3Object,
 } from "../../src/handlers/int-test-support/helpers/s3Helper";
 import { poll } from "../../src/handlers/int-test-support/helpers/commonHelpers";
 import {
@@ -12,6 +14,7 @@ import {
   randomLineItems,
 } from "../../src/handlers/int-test-support/helpers/mock-data/invoice";
 import { createInvoiceInS3 } from "../../src/handlers/int-test-support/helpers/mock-data/invoice/helpers";
+import fs from "fs";
 import {
   queryObject,
   startQueryExecutionCommand,
@@ -20,7 +23,7 @@ import {
 const prefix = resourcePrefix();
 const testStartTime = new Date();
 
-describe("\n Happy path - Upload valid mock invoice pdf and verify data is seen in the billing view\n", () => {
+describe("\n Happy path - Upload valid mock invoice and verify data is seen in the billing view\n", () => {
   const storageBucket = `${prefix}-storage`;
   const standardisedFolderPrefix = "btm_billing_standardised";
   const databaseName = `${prefix}-calculations`;
@@ -32,7 +35,7 @@ describe("\n Happy path - Upload valid mock invoice pdf and verify data is seen 
     });
   });
 
-  test("upload valid pdf file in raw-invoice bucket and see that we can see the data in the view", async () => {
+  test("upload valid pdf file in raw-invoice bucket and check that we can see the data in the view", async () => {
     const passportCheckItems = randomLineItems(8, {
       description: "passport check",
     });
@@ -135,6 +138,68 @@ describe("\n Happy path - Upload valid mock invoice pdf and verify data is seen 
       bucket: s3Object.bucket,
       key: `successful/${String(path)}`,
     });
+  });
+
+  test("upload valid csv file in raw-invoice bucket and check that we can see the data in the view", async () => {
+    // Step 1: Put the test csv file in the raw-invoice bucket. A further ticket will handle the random creation of a csv invoice, similar to the pdf invoice.
+    // Note: For the csv invoice flow, the original does not get moved to a 'successful folder' like it does for the pdf invoice flow that invokes Textract.
+
+    const folderPrefix = "vendor_testvendor1";
+    const testObject: S3Object = {
+      bucket: `${prefix}-raw-invoice`,
+      key: `${folderPrefix}/valid-invoice.csv`,
+    };
+    const csv = "../payloads/valid-invoice.csv";
+    const filePath = path.join(__dirname, csv);
+    const fileData = fs.readFileSync(filePath);
+    await putS3Object({ data: fileData, target: testObject });
+
+    const checkRawCsvFileExists = await checkIfS3ObjectExists(testObject);
+    expect(checkRawCsvFileExists).toBeTruthy();
+
+    // Step 2: Wait for the invoice data to be standardised and saved in the storage bucket/btm_billing_standardised.
+
+    await poll(
+      async () =>
+        await listS3Objects({
+          bucketName: storageBucket,
+          prefix: standardisedFolderPrefix,
+        }),
+      ({ Contents }) =>
+        !!Contents?.some(
+          (s3Object) =>
+            s3Object.Key !== undefined &&
+            s3Object.Key === `btm_billing_standardised/valid-invoice.txt`
+        ),
+      {
+        timeout: 60000,
+        nonCompleteErrorMessage:
+          "Invoice data never appeared in standardised folder",
+      }
+    );
+
+    // Step 3: Check the view results match the original csv invoice. Hard coded for now based on the csv in the payloads folder.
+    const queryString = `SELECT * FROM "btm_billing_curated" where vendor_id = 'vendor_testvendor1'`;
+    const queryId = await startQueryExecutionCommand({
+      databaseName,
+      queryString,
+    });
+    const queryObjects: BillingCurated[] = await queryObject(queryId);
+    expect(queryObjects.length).toEqual(2);
+
+    expect(queryObjects[0].vendor_name).toEqual("Vendor One");
+    expect(queryObjects[0].service_name).toEqual("Passport check");
+    expect(queryObjects[0].quantity).toEqual("13788");
+    expect(queryObjects[0].price).toEqual("4687.9200");
+    expect(queryObjects[0].year).toEqual("2023");
+    expect(queryObjects[0].month).toEqual("02");
+
+    expect(queryObjects[1].vendor_name).toEqual("Vendor One");
+    expect(queryObjects[1].service_name).toEqual("Fraud check");
+    expect(queryObjects[1].quantity).toEqual("83");
+    expect(queryObjects[1].price).toEqual("327.8500");
+    expect(queryObjects[1].year).toEqual("2023");
+    expect(queryObjects[1].month).toEqual("02");
   });
 });
 
