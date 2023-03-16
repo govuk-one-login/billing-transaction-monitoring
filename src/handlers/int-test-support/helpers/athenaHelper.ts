5e69f8706485de8dfa667465dcba926cc9c2d5e1
NKannan14 <nithya.kannan@digital.cabinet-office.gov.uk>
2023-03-16 16:12:40 +0000

    added timeout for invoice moved to failed folder

--- src/handlers/int-test-support/helpers/athenaHelper.ts
+++ src/handlers/int-test-support/helpers/athenaHelper.ts
@@ -84,7 +84,7 @@ const waitAndGetQueryResults = async (
     return result?.State?.match("SUCCEEDED") !== null;
   };
   const queryStatusSuccess = await poll(checkState, (state) => state, {
-    timeout: 60000,
+    timeout: 65000,
     interval: 5000,
     nonCompleteErrorMessage: "Query did not succeed within the given timeout",
   });
