01725091033175845d8f4f4f8868e7640a1e80a6
Melissa Pereira <melissa.pereira@digital.cabinet-office.gov.uk>
2023-03-20 13:30:15 +0000

    Fix SyntaxError

--- webpack.config.js
+++ webpack.config.js
@@ -6,20 +6,19 @@ const __dirname = path.dirname(fileURLToPath(import.meta.url));
 export default {
   devtool: "source-map",
   entry: {
+    filter: "./src/handlers/filter/handler.ts",
     clean: "./src/handlers/clean/handler.ts",
+    storeTransactions: "./src/handlers/store-transactions/handler.ts",
+    transactionCsvToJsonEvent:
+      "./src/handlers/transaction-csv-to-json-event/handler.ts",
     customAthenaViewResource:
       "./src/handlers/custom-athena-view-resource/handler.ts",
     intTestSupport: "./src/handlers/int-test-support/handler.ts",
     pdfExtract: "./src/handlers/pdf-extract/handler.ts",
-    csvExtract: "./src/handlers/csv-extract/handler.ts",
-    filter: "./src/handlers/filter/handler.ts",
     storeRawInvoiceTextractData:
       "./src/handlers/store-raw-invoice-textract-data/handler.ts",
-    storeTransactions: "./src/handlers/store-transactions/handler.ts",
-    storeStandardisedInvoices:
-      "./src/handlers/store-standardised-invoices/handler.ts",
-    transactionCsvToJsonEvent:
-      "./src/handlers/transaction-csv-to-json-event/handler.ts",
+    pdfStandardisation: "./src/handlers/pdf-standardisation/handler.ts",
+    csvExtract: "./src/handlers/csv-extract/handler.ts",
   },
   externals: "aws-sdk",
   mode: process.env.NODE_ENV === "dev" ? "development" : "production",
