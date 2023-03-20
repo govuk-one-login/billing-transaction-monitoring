683892c0b7324dab2a054090b7247e531240c705
NKannan14 <nithya.kannan@digital.cabinet-office.gov.uk>
2023-03-20 11:09:50 +0000

    added condition to invokeLambda to execute via int-test

--- src/handlers/int-test-support/helpers/lambdaHelper.ts
+++ src/handlers/int-test-support/helpers/lambdaHelper.ts
@@ -6,7 +6,7 @@ import {
 import { fromUtf8, toUtf8 } from "@aws-sdk/util-utf8-node";
 import { lambdaClient } from "../clients";
 import { HelperDict, IntTestHelpers } from "../handler";
-import { configName, envName, resourcePrefix } from "./envHelper";
+import { configName, envName, resourcePrefix, runViaLambda } from "./envHelper";
 
 export const sendLambdaCommand = async <THelper extends IntTestHelpers>(
   command: THelper,
@@ -20,10 +20,10 @@ export const sendLambdaCommand = async <THelper extends IntTestHelpers>(
   });
 
   // logger.info(toUtf8(commandInput.Payload as Uint8Array));
-  const result = await invokeLambda(
-    `${resourcePrefix()}-int-test-support-function`,
-    payload
-  );
+  const result = await invokeLambda({
+    functionName: `${resourcePrefix()}-int-test-support-function`,
+    payload,
+  });
   // logger.info(toUtf8(result.Payload as Uint8Array));
 
   if (result.StatusCode === 200 && result.Payload != null) {
@@ -33,15 +33,25 @@ export const sendLambdaCommand = async <THelper extends IntTestHelpers>(
   }
 };
 
+export interface InvokeLambdaParams {
+  functionName: string;
+  payload: string;
+  forceWithoutLambda?: false;
+}
+
 export const invokeLambda = async (
-  functionName: string,
-  payload: string
+  params: InvokeLambdaParams
 ): Promise<InvocationResponse> => {
+  if (runViaLambda() && !params.forceWithoutLambda)
+    return (await sendLambdaCommand(
+      IntTestHelpers.invokeLambda,
+      params
+    )) as unknown as InvocationResponse;
   const command: InvokeCommandInput = {
-    FunctionName: functionName,
+    FunctionName: params.functionName,
     InvocationType: "RequestResponse",
     LogType: "Tail",
-    Payload: fromUtf8(payload),
+    Payload: fromUtf8(params.payload),
   };
   try {
     const response = await lambdaClient.send(new InvokeCommand(command));
