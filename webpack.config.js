653de1bea5a48fc6e19b742307c0df4ef84b0beb
MelissaPereira <116570124+MelAtGDS@users.noreply.github.com>
2023-03-21 15:29:27 +0000

    BTM-465 Standardised Invoice Store Function Infrastructure (#254)
    * Rename extraction to be pdf specific
    * Add infra for store-standardised-invoices
    * Move StorageBucket from calculation-s3 to standardised-invoice-storage
    * separate gtihub actions

--- webpack.config.js
+++ webpack.config.js
@@ -6,20 +6,21 @@ const __dirname = path.dirname(fileURLToPath(import.meta.url));
 export default {
   devtool: "source-map",
   entry: {
+    filter: "./src/handlers/filter/handler.ts",
     clean: "./src/handlers/clean/handler.ts",
+    storeTransactions: "./src/handlers/store-transactions/handler.ts",
+    transactionCsvToJsonEvent:
+      "./src/handlers/transaction-csv-to-json-event/handler.ts",
     customAthenaViewResource:
       "./src/handlers/custom-athena-view-resource/handler.ts",
     intTestSupport: "./src/handlers/int-test-support/handler.ts",
-    extract: "./src/handlers/extract/handler.ts",
-    csvExtract: "./src/handlers/csv-extract/handler.ts",
-    filter: "./src/handlers/filter/handler.ts",
+    pdfExtract: "./src/handlers/pdf-extract/handler.ts",
     storeRawInvoiceTextractData:
       "./src/handlers/store-raw-invoice-textract-data/handler.ts",
-    storeTransactions: "./src/handlers/store-transactions/handler.ts",
+    pdfStandardisation: "./src/handlers/pdf-standardisation/handler.ts",
+    csvExtract: "./src/handlers/csv-extract/handler.ts",
     storeStandardisedInvoices:
       "./src/handlers/store-standardised-invoices/handler.ts",
-    transactionCsvToJsonEvent:
-      "./src/handlers/transaction-csv-to-json-event/handler.ts",
   },
   externals: "aws-sdk",
   mode: process.env.NODE_ENV === "dev" ? "development" : "production",
