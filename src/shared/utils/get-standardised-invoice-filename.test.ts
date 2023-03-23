d4eb418644e4cf3c22dedf599275a04361719adf
Melissa Pereira <melissa.pereira@digital.cabinet-office.gov.uk>
2023-03-23 14:42:17 +0000

    BTM-464 Write helper function to build filename

---
+++ src/shared/utils/get-standardised-invoice-filename.test.ts
@@ -0,0 +1,35 @@
+import {
+  getStandardisedInvoiceFileName,
+  getYearMonth,
+} from "./get-standardised-invoice-filename";
+
+test("Filename getter with  standardised line item", () => {
+  const standardisedLineItem = {
+    invoice_receipt_id: "370 000",
+    vendor_id: "vendor_testvendor1",
+    vendor_name: "Vendor One",
+    invoice_receipt_date: "2023-02-28",
+    due_date: "2023-03-28",
+    tax_payer_id: "123 4567 89",
+    parser_version: "1.0.0",
+    originalInvoiceFile: "vendorOne.csv",
+    item_description: "Check one",
+    subtotal: 4687.92,
+    tax: 937.584,
+    price: 4687.92,
+    quantity: 13788,
+    service_name: "Check one",
+    unit_price: 0.34,
+    total: 5625.504,
+    event_name: "VENDOR_1_EVENT_1",
+  };
+  const filename = getStandardisedInvoiceFileName(standardisedLineItem);
+  expect(filename.length).toEqual(46); // <- expects 6 alphanumeric chars for the uuid
+  expect(filename).toMatch(/^2023-02-Vendor One-VENDOR_1_EVENT_1-.*\.txt$/);
+});
+
+test("Year, month, and day are formatted to yyyy-mm", () => {
+  const date: string = "2023/03/28";
+  const yearMonth = getYearMonth(date);
+  expect(yearMonth).toEqual("2023-03");
+});
---
+++ src/shared/utils/get-standardised-invoice-filename.ts
@@ -0,0 +1,21 @@
+import { StandardisedLineItem } from "../types";
+import crypto from "crypto";
+
+export function getStandardisedInvoiceFileName(
+  standardisedInvoice: StandardisedLineItem
+): string {
+  const yearMonth = getYearMonth(standardisedInvoice.invoice_receipt_date);
+  const vendorName = standardisedInvoice.vendor_name;
+  const eventName = standardisedInvoice.event_name;
+  const uuid = crypto.randomBytes(3).toString("hex");
+  const fileName = `${yearMonth}-${vendorName}-${eventName}-${uuid}.txt`;
+
+  return fileName;
+}
+
+export function getYearMonth(dateStr: string): string {
+  const date = new Date(dateStr);
+  const year = date.getFullYear();
+  const month = (date.getMonth() + 1).toString().padStart(2, "0");
+  return `${year}-${month}`;
+}
