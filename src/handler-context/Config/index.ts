913b12ff95a1ecd51d7555b82316d9603b77853a
Mark Potter <si3rr4wow@gmail.com>
2023-03-28 17:39:09 +0100

    tighten up config class

--- src/handler-context/Config/index.ts
+++ src/handler-context/Config/index.ts
@@ -6,54 +6,35 @@ export class Config<TFileName extends ConfigFileNames> {
   private readonly client: ConfigClient;
   private readonly files: ConfigFileNames[];
   private readonly logger: Logger;
-  private hasCacheBeenPopulated: boolean = false;
-  private cache!: PickedFiles<TFileName>;
-  private promises!: Array<
-    Promise<{
-      file: Json;
-      fileName: ConfigFileNames;
-    }>
-  >;
+  private readonly promises: Array<Promise<[ConfigFileNames, Json]>>;
+  private cache: PickedFiles<TFileName> | undefined;
 
   constructor(client: ConfigClient, files: TFileName[], logger: Logger) {
     this.client = client;
     this.files = files;
     this.logger = logger;
-    this.populatePromises();
+    this.promises = this.spawnPromises();
   }
 
-  private readonly populatePromises = (): void => {
+  private readonly spawnPromises = (): Array<
+    Promise<[ConfigFileNames, Json]>
+  > => {
     if (process.env.CONFIG_BUCKET === undefined)
       throw new Error("No CONFIG_BUCKET defined in this environment");
 
-    this.promises = this.files.map(async (fileName) => ({
-      file: await this.client.getConfigFile(fileName),
-      fileName,
-    }));
+    return this.files.map<Promise<[ConfigFileNames, Json]>>(
+      async (fileName) => [fileName, await this.client.getConfigFile(fileName)]
+    );
   };
 
   public readonly populateCache = async (): Promise<void> => {
-    const resolutions = await Promise.allSettled(this.promises);
-    // @ts-expect-error
-    this.cache = resolutions.reduce<PickedFiles<TFileName>>(
-      (config, resolution) => {
-        if (resolution.status === "rejected")
-          throw new Error(resolution.reason);
-        const { file, fileName } = resolution.value;
-        return {
-          ...config,
-          [fileName]: file,
-        };
-      },
-      // @ts-expect-error
-      {}
-    );
-    this.hasCacheBeenPopulated = true;
+    const cacheEntries = await Promise.all(this.promises);
+    this.cache = Object.fromEntries(cacheEntries) as PickedFiles<TFileName>;
   };
 
   public readonly getCache = (): PickedFiles<TFileName> => {
-    if (!this.hasCacheBeenPopulated) {
-      this.logger.warn(
+    if (this.cache === undefined) {
+      throw new Error(
         "Called getCache before awaiting populateCache. Ensue the cache is populated before reading it."
       );
     }
