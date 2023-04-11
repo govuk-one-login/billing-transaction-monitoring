6064f0caa73987fd861beaa941c8debf28c3e4fa
NKannan14 <nithya.kannan@digital.cabinet-office.gov.uk>
2023-04-11 17:18:54 +0100

    added logs

--- integration_tests/run-cleanup-jest-html-reports.sh
+++ integration_tests/run-cleanup-jest-html-reports.sh
@@ -2,8 +2,17 @@
 # Set the number of directories to keep
  num_of_dir_to_keep=$1
 
+ if [[ $GITHUB_WORKFLOW ]]; then
+    cd $GITHUB_WORKSPACE
+    cd jest-html-report
+    ls
+ else
+   cd ./reports/jest-html-report
+   ls
+fi
+
 # List all directories and sort by modification time in descending order
-dirs=( $(ls -d -t reports/jest-html-report/*/ | tail -n +$((num_of_dir_to_keep+1))) )
+dirs=( $(ls -d -t */ | tail -n +$((num_of_dir_to_keep+1))) )
 # Remove all jest-html-reports except for the last 1
 for dir in "${dirs[@]}"; do
   rm -rf "$dir"
