5b504f390855e89c1418706a20d2c4fd188751d4
NKannan14 <nithya.kannan@digital.cabinet-office.gov.uk>
2023-04-11 16:07:08 +0100

    fixed dir path in script

--- integration_tests/run-cleanup-jest-html-reports.sh
+++ integration_tests/run-cleanup-jest-html-reports.sh
@@ -2,13 +2,12 @@
 # Set the number of directories to keep
  num_of_dir_to_keep=$1
 
-cd ./reports/jest-html-report
 # List all directories and sort by modification time in descending order
-dirs=( $(ls -d -t */ | tail -n +$((num_of_dir_to_keep+1))) )
+dirs=( $(ls -d -t reports/jest-html-report/*/ | tail -n +$((num_of_dir_to_keep+1))) )
 # Remove all jest-html-reports except for the last 1
 for dir in "${dirs[@]}"; do
   rm -rf "$dir"
 done
 
 # List remaining directories
-ls -d *
+ls -d reports/jest-html-report/*
