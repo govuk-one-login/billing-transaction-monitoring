5836e9d6613878d68f395b4babe5004339f1a13c
Nithya Kannan <112889496+NKannan14@users.noreply.github.com>
2023-04-13 13:45:39 +0100

    added poll condition to check bucket empty (#275)

--- src/handlers/int-test-support/helpers/testSetup.ts
+++ src/handlers/int-test-support/helpers/testSetup.ts
@@ -1,5 +1,6 @@
+import { poll } from "./commonHelpers";
 import { resourcePrefix } from "./envHelper";
-import { deleteS3Objects } from "./s3Helper";
+import { deleteS3Objects, listS3Objects } from "./s3Helper";
 
 const prefix = resourcePrefix();
 const storageBucket = `${prefix}-storage`;
@@ -8,5 +9,18 @@ export default async function globalSetup(): Promise<void> {
   await deleteS3Objects({
     bucketName: storageBucket,
   });
+
+  await poll(
+    async () =>
+      await listS3Objects({
+        bucketName: storageBucket,
+      }),
+    (s3Objects) => s3Objects.Contents === undefined,
+    {
+      timeout: 60000,
+      interval: 10000,
+      nonCompleteErrorMessage: "Bucket is not empty after deleting objects",
+    }
+  );
   console.log("Deleted storage bucket before running the tests");
 }
