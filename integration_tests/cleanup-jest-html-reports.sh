7f12d0b28f08b0fe0a60153228a4a28348c08fce
NKannan14 <nithya.kannan@digital.cabinet-office.gov.uk>
2023-04-17 14:40:12 +0100

    updated cleanup script

--- integration_tests/cleanup-jest-html-reports.sh
+++ integration_tests/cleanup-jest-html-reports.sh
@@ -1,14 +1,11 @@
 #!/bin/bash
-# Set the number of directories to keep
 num_of_dir_to_keep=$1
 
-# List all directories and sort by modification time in descending order
-dirs=$(ls -d -t test-report*/ | tail -n +$((num_of_dir_to_keep+1)))
+echo Keeping at max $num_of_dir_to_keep test-report directories
 
-# Remove all jest-html-reports except for the last 5
-for dir in "${dirs[@]}"; do
-  rm -rf "$dir"
-done
+ls -dt1 test-report*/ |
+  tail -n +$((num_of_dir_to_keep+1)) |
+  xargs rm -rfv
 
-# List remaining directories
-ls -d */
+echo Remaining test-report directories:
+ls -dt1 test-report*/
