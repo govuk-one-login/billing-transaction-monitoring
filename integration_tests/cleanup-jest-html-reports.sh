fe6e21ba8b6b221c13ac763f5762fb3a31681847
NKannan14 <nithya.kannan@digital.cabinet-office.gov.uk>
2023-04-19 12:03:40 +0100

    added condition to check script argument set or not

--- integration_tests/cleanup-jest-html-reports.sh
+++ integration_tests/cleanup-jest-html-reports.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
+if [[ -z "$1" ]]; then
+  echo "Please provide the number of directories to keep."
+  exit 1
+fi
 num_of_dir_to_keep=$1
 
 echo Keeping at max $num_of_dir_to_keep test-report directories
