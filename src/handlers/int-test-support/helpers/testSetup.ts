8e5dcfb260737b11941c418e3a407132b47f0504
NKannan14 <nithya.kannan@digital.cabinet-office.gov.uk>
2023-04-21 16:07:18 +0100

    refactored testsetup

--- src/handlers/int-test-support/helpers/testSetup.ts
+++ src/handlers/int-test-support/helpers/testSetup.ts
@@ -6,21 +6,52 @@ const prefix = resourcePrefix();
 const storageBucket = `${prefix}-storage`;
 
 export default async function globalSetup(): Promise<void> {
-  await deleteS3Objects({
-    bucketName: storageBucket,
-  });
+  const currentDate = new Date();
+  const currentYear = currentDate.getFullYear();
+  const currentMonth = currentDate.getMonth();
+
+  for (let year = 2022; year <= currentYear; year++) {
+    const startMonth = year === 2022 ? 1 : 0;
+    const endMonth = year === currentYear ? currentMonth : 11;
+    for (let month = startMonth; month <= endMonth; month++) {
+      const prefix = `btm_event_data/${year}/${(month + 1)
+        .toString()
+        .padStart(2, "0")}/`;
+      console.log(prefix);
+      await deleteS3Objects({ bucketName: storageBucket, prefix });
+    }
+  }
 
   await poll(
     async () =>
       await listS3Objects({
         bucketName: storageBucket,
+        prefix: "btm_event_data",
       }),
     (s3Objects) => s3Objects.Contents === undefined,
     {
       timeout: 60000,
       interval: 10000,
-      notCompleteErrorMessage: "Bucket is not empty after deleting objects",
+      notCompleteErrorMessage:
+        " event_data folder not empty after deleting objects",
     }
   );
-  console.log("Deleted storage bucket before running the tests");
+  await deleteS3Objects({
+    bucketName: storageBucket,
+    prefix: "btm_invoice_data",
+  });
+  await poll(
+    async () =>
+      await listS3Objects({
+        bucketName: storageBucket,
+        prefix: "btm_invoice_data",
+      }),
+    (s3Objects) => s3Objects.Contents === undefined,
+    {
+      timeout: 60000,
+      interval: 10000,
+      notCompleteErrorMessage:
+        " invoice_data folder not empty after deleting objects",
+    }
+  );
 }
