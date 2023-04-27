ec78eb656aefe208d424321686f03ee8c28a563b
Paul Flood <paul.flood@digital.cabinet-office.gov.uk>
2023-04-27 07:12:32 +0100

    Added jest.performance.config.ts

---
+++ jest.performance.config.ts
@@ -0,0 +1,34 @@
+import type { Config } from "@jest/types";
+
+const config: Config.InitialOptions = {
+  roots: ["<rootDir>/integration_tests"],
+  testMatch: ["**/tests/*tests.ts"],
+  coveragePathIgnorePatterns: ["/node_modules/"],
+  preset: "ts-jest",
+  testRunner: "jasmine2",
+  globalSetup: "./src/handlers/int-test-support/helpers/testSetup.ts",
+  verbose: true,
+  testTimeout: 1600000,
+  reporters: [
+    "default",
+    [
+      "jest-junit",
+      {
+        outputDirectory: "reports",
+        outputName: "testReport.xml",
+      },
+    ],
+    [
+      "jest-html-reporters",
+      {
+        publicPath: `./reports/jest-html-reports/test-report-${new Date().toISOString()}`,
+        filename: "index.html",
+        expand: true,
+        openReport: true,
+        pageTitle: "BTM PERFORMANCE TEST REPORT",
+      },
+    ],
+  ],
+};
+
+export default config;
