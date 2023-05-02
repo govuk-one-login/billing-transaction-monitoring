bae7b2ee9c3e56c686c7e9112e35d1727594607d
Mark Potter <si3rr4wow@gmail.com>
2023-05-02 15:21:25 +0100

    never types

--- src/handlers/store-transactions/business-logic.ts
+++ src/handlers/store-transactions/business-logic.ts
@@ -1,11 +1,11 @@
 import { BusinessLogic } from "../../handler-context";
 import { CleanedEventBody } from "../store-transactions/types";
-import { ConfigCache, Env } from "./types";
+import { Env } from "./types";
 
 export const businessLogic: BusinessLogic<
   CleanedEventBody,
   Env,
-  ConfigCache,
+  never,
   CleanedEventBody
 > = async (event, { logger }) => {
   logger.info(`Storing event ${event.event_id}`);
--- src/handlers/store-transactions/store.ts
+++ src/handlers/store-transactions/store.ts
@@ -1,11 +1,11 @@
 import { HandlerCtx } from "../../handler-context";
 import { formatDate, putTextS3 } from "../../shared/utils";
-import { CleanedEventBody, ConfigCache, Env } from "./types";
+import { CleanedEventBody, Env } from "./types";
 
 export const store = async (
   bucket: string,
   message: CleanedEventBody,
-  { env }: HandlerCtx<Env, ConfigCache, CleanedEventBody>
+  { env }: HandlerCtx<Env, never, CleanedEventBody>
 ): Promise<void> => {
   const { event_id, timestamp } = message;
   const key = `${env.EVENT_DATA_FOLDER}/${formatDate(
