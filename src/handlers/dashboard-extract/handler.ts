cb8466e62182a0767b583c51aea13b486f2699a4
Mark Schnitzius <mark.schnitzius@digital.cabinet-office.gov.uk>
2023-05-22 09:07:00 +0100

    Now successfully writing file to S3

--- src/handlers/dashboard-extract/handler.ts
+++ src/handlers/dashboard-extract/handler.ts
@@ -4,13 +4,6 @@ import { QueryExecutionState } from "aws-sdk/clients/athena";
 
 // import {Context, SQSEvent} from "aws-lambda";
 import { Response } from "../../shared/types";
-// import {
-//   fetchS3,
-//   getS3EventRecordsFromSqs,
-//   getVendorServiceConfigRows,
-//   logger,
-//   sendRecord,
-// } from "../../shared/utils";
 
 export const handler = async (): Promise<Response> => {
   const response: Response = {
@@ -19,15 +12,16 @@ export const handler = async (): Promise<Response> => {
 
   const fetchDataSql = `SELECT * FROM "${process.env.DATABASE_NAME}".btm_billing_and_transactions_curated`;
   console.log("Query", fetchDataSql);
-  const results = await query(fetchDataSql);
+  const results: QueryResults = (await query(fetchDataSql)) as QueryResults;
   console.log("Fetch result", results);
+  await writeExtractToS3(results);
 
   return response;
 };
 
 const athena: Athena = new AWS.Athena();
 
-export async function query(sql: string): Promise<any> {
+export async function query(sql: string): Promise<object> {
   const params = {
     QueryString: sql,
     ResultConfiguration: {
@@ -61,4 +55,89 @@ export async function query(sql: string): Promise<any> {
   return await athena
     .getQueryResults({ QueryExecutionId: queryExecutionId })
     .promise();
+}
+
+interface Cell {
+  VarCharValue: string;
+}
+
+interface Row {
+  Data: Cell[];
+}
+
+interface ResultSet {
+  Rows: Row[];
+}
+
+interface QueryResults {
+  ResultSet: ResultSet;
+  UpdateCount: number;
+}
+
+async function writeExtractToS3(results: QueryResults): Promise<void> {
+  const body = getExtractData(results);
+
+  const s3Client = new AWS.S3({
+    region: "eu-west-2",
+  });
+
+  // We use multipart upload here because, surprisingly, s3Client.putObject
+  // fails to upload the file but also doesn't throw an error (see
+  // https://stackoverflow.com/questions/54874350/silent-failure-of-s3-putobject).
+  // As there is a 5MB limit on the part size in a multipart upload, we can
+  // get away with just uploading it as a single part though.
+
+  const { UploadId } = await s3Client
+    .createMultipartUpload({
+      Bucket: process.env.STORAGE_BUCKET as string,
+      Key: "btm_extract_data/full-extract.json",
+    })
+    .promise();
+
+  if (UploadId === undefined) {
+    throw new Error("Upload id undefined");
+  }
+
+  // upload parts of the file
+  const { ETag } = await s3Client
+    .uploadPart({
+      Bucket: process.env.STORAGE_BUCKET as string,
+      Key: "btm_extract_data/full-extract.json",
+      PartNumber: 1,
+      UploadId,
+      Body: body,
+    })
+    .promise();
+
+  // complete the multipart upload
+  await s3Client
+    .completeMultipartUpload({
+      Bucket: process.env.STORAGE_BUCKET as string,
+      Key: "btm_extract_data/full-extract.json",
+      MultipartUpload: {
+        Parts: [{ ETag, PartNumber: 1 }],
+      },
+      UploadId,
+    })
+    .promise();
+}
+
+function getExtractData(results: QueryResults): string {
+  // The column headers are returned as the 0th element of result set.
+  // For each of the remaining rows, we need to generate key/value pairs
+  // where the keys are the column header names.
+  const rows = results.ResultSet.Rows;
+  const columnHeaders = rows[0].Data;
+  const lines = [];
+
+  for (let i = 1; i < rows.length; i++) {
+    const row = rows[i].Data;
+    const outputRow = {} as any;
+    for (let j = 0; j < columnHeaders.length; j++) {
+      outputRow[columnHeaders[j].VarCharValue] = row[j].VarCharValue;
+    }
+    lines.push(JSON.stringify(outputRow));
+  }
+
+  return lines.join("\n");
 }
