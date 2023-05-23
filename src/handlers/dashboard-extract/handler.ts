926432659c6945a5f9e0393e2f639b56c28cb3a3
Mark Schnitzius <mark.schnitzius@digital.cabinet-office.gov.uk>
2023-05-23 09:21:20 +0100

    Code review fixes

--- src/handlers/dashboard-extract/handler.ts
+++ src/handlers/dashboard-extract/handler.ts
@@ -2,23 +2,18 @@ import AWS from "aws-sdk";
 import { Athena } from "aws-sdk/clients/all";
 import { QueryExecutionState } from "aws-sdk/clients/athena";
 
-import { Response } from "../../shared/types";
+import { ResultSet } from "@aws-sdk/client-athena";
+import { putTextS3 } from "../../shared/utils";
 
-export const handler = async (): Promise<Response> => {
-  const response: Response = {
-    batchItemFailures: [],
-  };
-
+export const handler = async (): Promise<void> => {
   const fetchDataSql = `SELECT * FROM "${process.env.DATABASE_NAME}".btm_billing_and_transactions_curated`;
-  const results: QueryResults = (await query(fetchDataSql)) as QueryResults;
+  const results: ResultSet = await query(fetchDataSql);
   await writeExtractToS3(results);
-
-  return response;
 };
 
 const athena: Athena = new AWS.Athena();
 
-export async function query(sql: string): Promise<object> {
+export async function query(sql: string): Promise<Athena.ResultSet> {
   const params = {
     QueryString: sql,
     ResultConfiguration: {
@@ -29,7 +24,7 @@ export async function query(sql: string): Promise<object> {
   const queryExecution = await athena.startQueryExecution(params).promise();
   const queryExecutionId = queryExecution.QueryExecutionId;
 
-  if (queryExecutionId === null || queryExecutionId === undefined) {
+  if (queryExecutionId === undefined) {
     throw new Error("Failed to start execution");
   }
 
@@ -47,89 +42,59 @@ export async function query(sql: string): Promise<object> {
     throw new Error(`Query execution failed: ${reason}`);
   }
 
-  return await athena
+  const results = await athena
     .getQueryResults({ QueryExecutionId: queryExecutionId })
     .promise();
+  if (results.ResultSet === undefined) {
+    throw new Error("Failed to fetch results");
+  }
+  return results.ResultSet;
 }
 
-interface Cell {
-  VarCharValue: string;
-}
+async function writeExtractToS3(results: ResultSet): Promise<void> {
+  const storageBucket = process.env.STORAGE_BUCKET;
 
-interface Row {
-  Data: Cell[];
-}
-
-interface ResultSet {
-  Rows: Row[];
-}
-
-interface QueryResults {
-  ResultSet: ResultSet;
-  UpdateCount: number;
-}
-
-async function writeExtractToS3(results: QueryResults): Promise<void> {
-  const body = getExtractData(results);
-
-  const s3Client = new AWS.S3({
-    region: "eu-west-2",
-  });
-
-  // We use multipart upload here because, surprisingly, s3Client.putObject
-  // fails to upload the file but also doesn't throw an error (see
-  // https://stackoverflow.com/questions/54874350/silent-failure-of-s3-putobject).
-  // As there is a 5MB limit on the part size in a multipart upload, we can
-  // get away with just uploading it as a single part though.
-
-  const { UploadId } = await s3Client
-    .createMultipartUpload({
-      Bucket: process.env.STORAGE_BUCKET as string,
-      Key: "btm_extract_data/full-extract.json",
-    })
-    .promise();
-
-  if (UploadId === undefined) {
-    throw new Error("Upload id undefined");
+  if (storageBucket === undefined) {
+    throw new Error("STORAGE_BUCKET is undefined");
   }
 
-  // upload parts of the file
-  const { ETag } = await s3Client
-    .uploadPart({
-      Bucket: process.env.STORAGE_BUCKET as string,
-      Key: "btm_extract_data/full-extract.json",
-      PartNumber: 1,
-      UploadId,
-      Body: body,
-    })
-    .promise();
+  const body = getExtractData(results);
 
-  // complete the multipart upload
-  await s3Client
-    .completeMultipartUpload({
-      Bucket: process.env.STORAGE_BUCKET as string,
-      Key: "btm_extract_data/full-extract.json",
-      MultipartUpload: {
-        Parts: [{ ETag, PartNumber: 1 }],
-      },
-      UploadId,
-    })
-    .promise();
+  await putTextS3(storageBucket, "btm_extract_data/full-extract.json", body);
 }
 
-function getExtractData(results: QueryResults): string {
+function getExtractData(results: ResultSet): string {
+  if (results.Rows === undefined) {
+    throw new Error("No results in result set");
+  }
+  const rows = results.Rows;
+
   // The column headers are returned as the 0th element of result set.
   // For each of the remaining rows, we need to generate key/value pairs
   // where the keys are the column header names.
-  const rows = results.ResultSet.Rows;
   const columnHeaders = rows[0].Data;
+  if (columnHeaders === undefined) {
+    throw new Error("No column headers found");
+  }
+
   const lines = [];
 
   for (let i = 1; i < rows.length; i++) {
     const row = rows[i].Data;
-    const outputRow = {} as any;
+    if (row === undefined) {
+      throw new Error("Row missing");
+    }
+    const outputRow: Record<string, string> = {};
     for (let j = 0; j < columnHeaders.length; j++) {
-      outputRow[columnHeaders[j].VarCharValue] = row[j].VarCharValue;
+      const key = columnHeaders[j].VarCharValue;
+      if (key === undefined) {
+        throw new Error("Column header missing");
+      }
+      const value = row[j].VarCharValue;
+      if (value === undefined) {
+        throw new Error("Cell value missing");
+      }
+      outputRow[key] = value;
     }
     lines.push(JSON.stringify(outputRow));
   }
