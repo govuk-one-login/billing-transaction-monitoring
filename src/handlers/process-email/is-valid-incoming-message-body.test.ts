7409703f1d3b23ce45edfa95b4405b793f2db209
Melissa Pereira <melissa.pereira@digital.cabinet-office.gov.uk>
2023-05-24 14:06:36 +0100

    Amend incoming message typeguard to just check if the type is string, and not if it includes MIME-Version

--- src/handlers/process-email/is-valid-incoming-message-body.test.ts
+++ src/handlers/process-email/is-valid-incoming-message-body.test.ts
@@ -3,11 +3,6 @@ import { isValidIncomingMessageBody } from "./is-valid-incoming-message-body";
 describe("Valid incoming message body type guard", () => {
   let givenMessage: any;
 
-  beforeEach(() => {
-    givenMessage =
-      "MIME-Version: 1.0\\r\\nDate: Tue, 16 May 2023 12:57:44 +0100\\r\\nMessage-ID: <CALCsedWjXJtAw_oerRBF6aL_hQRKGcYxME+za_wYCwDWP_Lsuw@mail.gmail.com>\\r\\nSubject: emailwithtestcsv\\r\\nFrom: xyz <xyz@digital.cabinet-office.gov.uk>\\r\\nTo: abc <abc@digital.cabinet-office.gov.uk>\\r\\nContent-Type: text/csv;";
-  });
-
   test("Valid incoming message body type guard with null message", () => {
     givenMessage = null;
     const result = isValidIncomingMessageBody(givenMessage);
@@ -26,13 +21,9 @@ describe("Valid incoming message body type guard", () => {
     expect(result).toBe(false);
   });
 
-  test("Valid incoming message body type guard with no MIME-Version", () => {
-    givenMessage = "a random string";
-    const result = isValidIncomingMessageBody(givenMessage);
-    expect(result).toBe(false);
-  });
-
   test("Valid incoming message body type guard with valid message", () => {
+    givenMessage =
+      "MIME-Version: 1.0\\r\\nDate: Tue, 16 May 2023 12:57:44 +0100\\r\\nMessage-ID: <CALCsedWjXJtAw_oerRBF6aL_hQRKGcYxME+za_wYCwDWP_Lsuw@mail.gmail.com>\\r\\nSubject: emailwithtestcsv\\r\\nFrom: xyz <xyz@digital.cabinet-office.gov.uk>\\r\\nTo: abc <abc@digital.cabinet-office.gov.uk>\\r\\nContent-Type: text/csv;";
     const result = isValidIncomingMessageBody(givenMessage);
     expect(result).toBe(true);
   });
--- src/handlers/process-email/is-valid-incoming-message-body.ts
+++ src/handlers/process-email/is-valid-incoming-message-body.ts
@@ -1,5 +1,4 @@
 export const isValidIncomingMessageBody = (
   maybeIncomingMessageBody: unknown
 ): maybeIncomingMessageBody is string =>
-  typeof maybeIncomingMessageBody === "string" &&
-  maybeIncomingMessageBody.includes("MIME-Version"); // This will need updating as part of BTM-575 that will extend the functionality of this handler.
+  typeof maybeIncomingMessageBody === "string"; // This will need updating as part of BTM-575 that will extend the functionality of this handler.
