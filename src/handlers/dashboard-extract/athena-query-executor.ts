6f1643bb9727bef6b9fa5d2541b4a9be31de844b
Mark Schnitzius <mark.schnitzius@digital.cabinet-office.gov.uk>
2023-05-25 09:28:47 +0100

    Added workgroup

--- src/handlers/dashboard-extract/athena-query-executor.ts
+++ src/handlers/dashboard-extract/athena-query-executor.ts
@@ -1,5 +1,6 @@
 import { Athena } from "aws-sdk/clients/all";
 import { QueryExecutionState } from "aws-sdk/clients/athena";
+import { resourcePrefix } from "../int-test-support/helpers/envHelper";
 
 export class AthenaQueryExecutor {
   athena: Athena;
@@ -14,6 +15,7 @@ export class AthenaQueryExecutor {
       ResultConfiguration: {
         OutputLocation: process.env.RESULTS_BUCKET,
       },
+      WorkGroup: `${resourcePrefix()}-athena-dashboard-extract-workgroup`,
     };
 
     const queryExecution = await this.athena
