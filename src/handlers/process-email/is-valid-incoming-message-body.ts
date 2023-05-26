b49ada7c35b70b9292f36f3b7ee102be09fe846a
Melissa Pereira <melissa.pereira@digital.cabinet-office.gov.uk>
2023-05-26 16:07:41 +0100

    Removed comment on the typeguard. to be fixed at the handler context level in another ticket.

--- src/handlers/process-email/is-valid-incoming-message-body.ts
+++ src/handlers/process-email/is-valid-incoming-message-body.ts
@@ -1,4 +1,4 @@
 export const isValidIncomingMessageBody = (
   maybeIncomingMessageBody: unknown
 ): maybeIncomingMessageBody is string =>
-  typeof maybeIncomingMessageBody === "string"; // This will need updating as part of BTM-575 that will extend the functionality of this handler.
+  typeof maybeIncomingMessageBody === "string";
