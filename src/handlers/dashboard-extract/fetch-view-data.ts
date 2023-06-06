221b85ce46b0a485af70594cd015fd91bb2891c9
Mark Schnitzius <mark.schnitzius@digital.cabinet-office.gov.uk>
2023-06-06 09:56:18 +0100

    Added longer wait when fetching view results, and longer timeout on the lambda

--- src/handlers/dashboard-extract/fetch-view-data.ts
+++ src/handlers/dashboard-extract/fetch-view-data.ts
@@ -6,10 +6,12 @@ import { AthenaQueryExecutor } from "../../shared/utils/athena";
 
 const athena = new Athena({ region: "eu-west-2" });
 
+const QUERY_WAIT = 30 * 1000; // Thirty seconds
+
 export async function fetchViewData(
   env: Record<Env, string>
 ): Promise<ResultSet> {
   const fetchDataSql = `SELECT * FROM "${env.DATABASE_NAME}".btm_billing_and_transactions_curated`;
-  const executor = new AthenaQueryExecutor(athena);
+  const executor = new AthenaQueryExecutor(athena, QUERY_WAIT);
   return await executor.fetchResults(fetchDataSql, env.QUERY_RESULTS_BUCKET);
 }
