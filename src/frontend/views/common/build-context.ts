eea7b365beb3868b382361ff2ccddc2209d11e11
Mark Schnitzius <mark.schnitzius@digital.cabinet-office.gov.uk>
2023-06-19 15:18:32 +0100

    WIP

---
+++ src/frontend/views/common/build-context.ts
@@ -0,0 +1,13 @@
+import { HandlerOptions } from "../types";
+import { ConfigElements, makeCtxConfig } from "../../../handler-context/context-builder";
+import {PickedConfigCache} from "../../../handler-context/config";
+
+export const appConfig = async <
+  TConfigElements extends ConfigElements,
+>(
+  {
+    ConfigCache,
+  }: HandlerOptions<TConfigElements>
+): Promise<PickedConfigCache<TConfigElements>> => {
+  return await makeCtxConfig(ConfigCache);
+};
