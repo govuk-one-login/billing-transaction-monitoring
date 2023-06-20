983ab9f8d2ecece39c99ba8d82538294c46bde6b
Melissa Pereira <melissa.pereira@digital.cabinet-office.gov.uk>
2023-06-20 13:41:25 +0100

    Add lint rule for @typescript-eslint/no-misused-promises

--- src/frontend/handlers/contracts.ts
+++ src/frontend/handlers/contracts.ts
@@ -1,7 +1,6 @@
 import { RequestHandler } from "express";
 import { getContracts } from "../config";
 
-// eslint-disable-next-line @typescript-eslint/no-misused-promises
 export const contractsHandler: RequestHandler = async (_, response) => {
   response.render("contracts.njk", {
     contracts: await getContracts(),
