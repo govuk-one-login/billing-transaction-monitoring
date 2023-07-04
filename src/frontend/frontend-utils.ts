c75f46e32bd267100a7d1104a345991ad92b2f61
Melissa Pereira <melissa.pereira@digital.cabinet-office.gov.uk>
2023-07-04 17:09:03 +0100

    Add StatusLabel to frontend-utils

--- src/frontend/frontend-utils.ts
+++ src/frontend/frontend-utils.ts
@@ -12,31 +12,59 @@ export const MONTHS = [
   "Nov",
   "Dec",
 ];
-
-export class PriceDifferencePercentageSpecialCase {
+export class StatusLabel {
   constructor(
-    public readonly magicNumber: string,
-    public readonly bannerText: string
+    public readonly statusMessage: string,
+    public readonly statusClasses: string
   ) {}
 }
+export const STATUS_LABEL_WITHIN_THRESHOLD = new StatusLabel(
+  "Within Threshold",
+  "govuk-tag--green"
+);
+export const STATUS_LABEL_BELOW_THRESHOLD = new StatusLabel(
+  "Below Threshold",
+  "govuk-tag--blue"
+);
+export const STATUS_LABEL_ABOVE_THRESHOLD = new StatusLabel(
+  "Above Threshold",
+  "govuk-tag--red"
+);
+export const STATUS_LABEL_PENDING = new StatusLabel(
+  "Pending",
+  "govuk-tag--grey"
+);
+export const STATUS_LABEL_ERROR = new StatusLabel("Error", "govuk-tag--grey");
 
-export const MN_NO_CHARGE = new PriceDifferencePercentageSpecialCase(
+export class PercentageDiscrepancySpecialCase {
+  constructor(
+    public readonly magicNumber: string,
+    public readonly bannerText: string,
+    public readonly statusLabel: StatusLabel
+  ) {}
+}
+export const MN_NO_CHARGE = new PercentageDiscrepancySpecialCase(
   "-1234567.01",
-  "No charge"
+  "No charge",
+  STATUS_LABEL_WITHIN_THRESHOLD
 );
-export const MN_RATES_MISSING = new PriceDifferencePercentageSpecialCase(
+export const MN_RATES_MISSING = new PercentageDiscrepancySpecialCase(
   "-1234567.02",
-  "Unable to find rate"
+  "Unable to find rate",
+  STATUS_LABEL_ERROR
 );
-export const MN_INVOICE_MISSING = new PriceDifferencePercentageSpecialCase(
+export const MN_INVOICE_MISSING = new PercentageDiscrepancySpecialCase(
   "-1234567.03",
-  "Invoice missing"
+  "Invoice data missing",
+  STATUS_LABEL_PENDING
 );
-export const MN_EVENTS_MISSING = new PriceDifferencePercentageSpecialCase(
+export const MN_EVENTS_MISSING = new PercentageDiscrepancySpecialCase(
   "-1234567.04",
-  "Events missing"
+  "Events missing",
+  STATUS_LABEL_ERROR
 );
-export const MN_UNEXPECTED_CHARGE = new PriceDifferencePercentageSpecialCase(
+export const MN_UNEXPECTED_CHARGE = new PercentageDiscrepancySpecialCase(
   "-1234567.05",
-  "Invoice has unexpected charge"
+  "Unexpected invoice charge",
+  STATUS_LABEL_ABOVE_THRESHOLD
 );
