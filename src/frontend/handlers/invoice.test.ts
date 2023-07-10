9a9d05e3179c3b15bae2a9ffbb9377d223a4e9e1
Melissa Pereira <melissa.pereira@digital.cabinet-office.gov.uk>
2023-07-10 12:04:07 +0100

    Tidy up invoice handler test

--- src/frontend/handlers/invoice.test.ts
+++ src/frontend/handlers/invoice.test.ts
@@ -4,13 +4,8 @@ import { makeCtxConfig } from "../../handler-context/context-builder";
 import { initApp } from "../init-app";
 import { fetchS3 } from "../../shared/utils";
 import {
-  MN_EVENTS_MISSING,
-  MN_INVOICE_MISSING,
-  MN_RATES_MISSING,
-  MN_UNEXPECTED_CHARGE,
   STATUS_LABEL_ABOVE_THRESHOLD,
   STATUS_LABEL_BELOW_THRESHOLD,
-  STATUS_LABEL_ERROR,
   STATUS_LABEL_WITHIN_THRESHOLD,
 } from "../frontend-utils";
 
@@ -63,67 +58,6 @@ describe("invoice handler", () => {
     mockedFetchS3.mockResolvedValue(givenExtractResults);
   });
 
-  test("Page displays invoice page header", async () => {
-    const request = supertest(app);
-    const response = await request.get(
-      `/invoice?contract_id=1&year=2023&month=03`
-    );
-    expect(response.status).toBe(200);
-    expect(response.text).toContain("Vendor One Mar 2023 Invoice"); // page heading
-    expect(response.text).toMatchSnapshot();
-  });
-
-  test("Page displays 'no data' message", async () => {
-    const request = supertest(app);
-    const response = await request.get(
-      "/invoice?contract_id=1&year=2023&month=06" // No data for this month
-    );
-    expect(response.status).toBe(200);
-    expect(response.text).toContain("Vendor One Jun 2023 Invoice"); // page heading
-    expect(response.text).toContain("Invoice and events missing"); // banner
-  });
-
-  test("Page displays Events Missing Banner Text and both line items in the Reconciliation Table for Mar 2023", async () => {
-    // There are two line items for Mar 2023, but only the invoice
-    // missing message should appear as it's the highest priority.
-    givenExtractResults = `{"vendor_id":"vendor_testvendor1","vendor_name":"Vendor One","service_name":"Passport check","month":"03","year":"2023","contract_id":"1","contract_name":"C01234","billing_price_formatted":"£0.00","transaction_price_formatted":"","price_difference":"","billing_quantity":"2822", "transaction_quantity":"", "quantity_difference":"","billing_amount_with_tax":"£0","price_difference_percentage":"${MN_EVENTS_MISSING.magicNumber}"}
-{"vendor_id":"vendor_testvendor1","vendor_name":"Vendor One","service_name":"ABC Charge","month":"03","year":"2023","contract_id":"1","contract_name":"C01234","billing_price_formatted":"16,029.00","transaction_price_formatted":"15,828.30","price_difference":"200.70","billing_quantity":"53430", "transaction_quantity":"52761", "quantity_difference":"669.00","billing_amount_with_tax":"£19,234.80","price_difference_percentage":"1.268"}`;
-    mockedFetchS3.mockResolvedValue(givenExtractResults);
-
-    const request = supertest(app);
-    const response = await request.get(
-      `/invoice?contract_id=1&year=2023&month=03`
-    );
-    expect(response.status).toBe(200);
-    expect(response.text).toContain("Vendor One Mar 2023 Invoice"); // page heading
-    expect(response.text).toContain(MN_EVENTS_MISSING.bannerText); // banner
-    expect(response.text).not.toContain("Invoice above threshold"); // banner
-    expect(response.text).toContain("1.268"); // percentage discrepancy in table
-    expect(response.text).toContain(STATUS_LABEL_ABOVE_THRESHOLD.statusMessage); // status in table for discrepancy 1.268
-    expect(response.text).toContain(STATUS_LABEL_ERROR.statusMessage); // status in table for Events missing
-  });
-
-  test("Page displays Invoice data missing Banner Text and three line items in the Reconciliation Table for Mar 2023", async () => {
-    // There are three different line items with problems, but only the
-    // Invoice data missing should appear in the banner as it has higher priority than the others. All three line items will be in the Reconciliation Table
-    givenExtractResults = `{"vendor_id":"vendor_testvendor1","vendor_name":"Vendor One","service_name":"Passport check","month":"03","year":"2023","contract_id":"1","contract_name":"C01234","billing_price_formatted":"","transaction_price_formatted":"2,698.80","price_difference":"","billing_quantity":"", "transaction_quantity":"8996", "quantity_difference":"","billing_amount_with_tax":"","price_difference_percentage":"${MN_INVOICE_MISSING.magicNumber}"}
-  {"vendor_id":"vendor_testvendor1","vendor_name":"Vendor One","service_name":"ABC Charge","month":"03","year":"2023","contract_id":"1","contract_name":"C01234","billing_price_formatted":"","transaction_price_formatted":"","price_difference":"","billing_quantity":"", "transaction_quantity":"", "quantity_difference":"","billing_amount_with_tax":"","price_difference_percentage":"${MN_RATES_MISSING.magicNumber}"}
-{"vendor_id":"vendor_testvendor1","vendor_name":"Vendor One","service_name":"ABC Charge","month":"03","year":"2023","contract_id":"1","contract_name":"C01234","billing_price_formatted":"27.50","transaction_price_formatted":"0.00","price_difference":"27.50","billing_quantity":"11", "transaction_quantity":"2", "quantity_difference":"9.00","billing_amount_with_tax":"£19,234.80","price_difference_percentage":"${MN_UNEXPECTED_CHARGE.magicNumber}"}`;
-    mockedFetchS3.mockResolvedValue(givenExtractResults);
-
-    const request = supertest(app);
-    const response = await request.get(
-      `/invoice?contract_id=1&year=2023&month=03`
-    );
-    expect(response.status).toBe(200);
-    expect(response.text).toContain("Vendor One Mar 2023 Invoice"); // page heading
-    expect(response.text).toContain(MN_INVOICE_MISSING.bannerText); // banner
-    expect(response.text).toContain(MN_RATES_MISSING.bannerText); // table
-    expect(response.text).toContain(STATUS_LABEL_ERROR.statusMessage); // status in table for Unable to find rate
-    expect(response.text).toContain(MN_UNEXPECTED_CHARGE.bannerText); // table
-    expect(response.text).toContain(STATUS_LABEL_ABOVE_THRESHOLD.statusMessage); // status in table for Unexpected Charge
-  });
-
   test("Page displays Invoice above threshold banner text and three line items in the Reconciliation Table for Mar 2023", async () => {
     // The single line item that's over threshold should cause the error banner
     // even though the other line items are within or below threshold.
@@ -149,5 +83,6 @@ describe("invoice handler", () => {
     expect(response.text).toContain(
       STATUS_LABEL_WITHIN_THRESHOLD.statusMessage
     ); // status in table for Within Threshold
+    expect(response.text).toMatchSnapshot();
   });
 });
