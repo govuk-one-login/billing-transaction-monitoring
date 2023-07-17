0e79beb1458f2c012c4383aa625f8bb47012d63f
NKannan14 <nithya.kannan@digital.cabinet-office.gov.uk>
2023-07-17 13:34:03 +0100

    hard coded resource prefix in testsetup file

--- ui-tests/testData/test.setup.ts
+++ ui-tests/testData/test.setup.ts
@@ -1,9 +1,7 @@
-import { resourcePrefix } from "../../src/handlers/int-test-support/helpers/envHelper.js";
 import { putS3Object } from "../helpers/s3Helper.js";
 import { geJsonDataFromFile } from "../utils/extractTestDatajson.js";
 
-const prefix = resourcePrefix();
-const storageBucket = `${prefix}-storage`;
+const storageBucket = `di-btm-pr-368-storage`;
 
 export const uploadExtractDataFileForUITest = async (): Promise<void> => {
   const filePath = "./ui-tests/testData/testData.json";
