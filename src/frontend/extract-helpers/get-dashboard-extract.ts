d602d100125e306e07e65027c41e775cf411fbd6
Melissa Pereira <melissa.pereira@digital.cabinet-office.gov.uk>
2023-07-24 09:42:58 +0100

    Refactor getDashboardExtract to cache

--- src/frontend/extract-helpers/get-dashboard-extract.ts
+++ src/frontend/extract-helpers/get-dashboard-extract.ts
@@ -1,15 +1,19 @@
 import { fetchS3 } from "../../shared/utils";
 import { FullExtractLineItem } from "./types";
 
+let promise: Promise<string>;
+
 export const getDashboardExtract = async (): Promise<FullExtractLineItem[]> => {
   if (process.env.STORAGE_BUCKET === undefined)
     throw new Error("No STORAGE_BUCKET defined in this environment");
 
-  const extract = await fetchS3(
-    process.env.STORAGE_BUCKET,
-    "btm_extract_data/full-extract.json"
-  );
+  if (promise === undefined)
+    promise = fetchS3(
+      process.env.STORAGE_BUCKET,
+      "btm_extract_data/full-extract.json"
+    );
 
+  const extract = await promise;
   const jsonArray = "[" + extract.replace(/\n/g, ",") + "]";
   return JSON.parse(jsonArray);
 };
