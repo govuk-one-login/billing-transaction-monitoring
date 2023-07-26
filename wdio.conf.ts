4ffc1b5fddeef83244fb9d3b86fb41cea8914550
NKannan14 <nithya.kannan@digital.cabinet-office.gov.uk>
2023-07-26 14:58:19 +0100

    ui-tests-reporting

--- wdio.conf.ts
+++ wdio.conf.ts
@@ -1,4 +1,6 @@
 import { uploadExtractDataFileForUITest } from "./ui-tests/testData/test.setup";
+import { ReportAggregator } from "wdio-html-nice-reporter";
+let reportAggregator: ReportAggregator;
 
 export const config = {
   runner: "local",
@@ -21,18 +23,42 @@ export const config = {
   ],
   logLevel: "error",
   bail: 0,
-  baseUrl: "",
+  baseUrl: "http://localhost:3000",
   waitforTimeout: 10000,
   connectionRetryTimeout: 120000,
   connectionRetryCount: 3,
   services: ["chromedriver"],
   framework: "mocha",
-  reporters: ["spec"],
+  reporters: [
+    "spec",
+    [
+      "html-nice",
+      {
+        outputDir: "./ui-tests/reports",
+      },
+    ],
+  ],
   mochaOpts: {
     ui: "bdd",
     timeout: 60000,
   },
-  beforeTest: async function () {
+  beforeTest: async function (): Promise<void> {
     await uploadExtractDataFileForUITest();
+  },
+
+  onPrepare: async function (capabilities: { browser: any }): Promise<void> {
+    reportAggregator = new ReportAggregator({
+      outputDir: "./ui-tests/reports/",
+      filename: "ui-test-report.html",
+      reportTitle: "Billing and Transaction Monitoring UI Tests",
+      browserName: capabilities.browser,
+      collapseTests: false,
+      showInBrowser: true,
+    });
+    reportAggregator.clean();
+  },
+
+  onComplete: async function (): Promise<void> {
+    await reportAggregator.createReport();
   },
 };
