281d7eed05294abb75ab7ec66648c9b21b4d1cd7
Nithya Kannan <112889496+NKannan14@users.noreply.github.com>
2023-07-27 10:22:01 +0100

    BTM-708 UI tests invoice page (#389)
    * added status color test
    ---------
    
    Signed-off-by: NKannan14 <nithya.kannan@digital.cabinet-office.gov.uk>

---
+++ ui-tests/utils/generateExpectedStatusBannerDetails.ts
@@ -0,0 +1,30 @@
+import {
+  percentageDiscrepancySpecialCase,
+  Color,
+  InvoiceStates,
+} from "./constants";
+
+export const generateExpectedBannerDetailsFromPercentagePriceDifference = (
+  percentageDifference: number
+): { bannerColor: string; bannerMessage: string } => {
+  let bannerColor = "";
+  let bannerMessage = "";
+  if (percentageDiscrepancySpecialCase[percentageDifference]) {
+    bannerColor =
+      percentageDiscrepancySpecialCase[percentageDifference].bannerColor;
+    bannerMessage =
+      percentageDiscrepancySpecialCase[percentageDifference].bannerText;
+  } else if (percentageDifference > -1 && percentageDifference < 1) {
+    bannerColor = Color.green;
+    bannerMessage = InvoiceStates.invoiceWithinThreshold;
+  } else if (percentageDifference > 1) {
+    bannerColor = Color.red;
+    bannerMessage = InvoiceStates.invoiceAboveThreshold;
+  } else if (percentageDifference < -1) {
+    bannerColor = Color.blue;
+    bannerMessage = InvoiceStates.invoiceBelowThreshold;
+  } else {
+    throw new Error(`Invalid percentageDifference: ${percentageDifference}`);
+  }
+  return { bannerColor, bannerMessage };
+};
