001e7f6246ad670169f6f1fd705ee1e68cc72397
Mark Schnitzius <mark.schnitzius@digital.cabinet-office.gov.uk>
2023-07-31 09:45:14 +0100

    Code review fixes

--- src/frontend/utils/index.ts
+++ src/frontend/utils/index.ts
@@ -2,4 +2,3 @@ export * from "./months";
 export * from "./status-label";
 export * from "./percentage-discrepancy-special-case";
 export * from "./links";
-export * from "./title";
--- src/frontend/utils/title.ts
+++
@@ -1,3 +0,0 @@
-// For `<title>`, not `<h1>`
-export const pageTitleFormatter = (title: string): string =>
-  `${title} – Billings and Reconciliation – GOV.UK`;
