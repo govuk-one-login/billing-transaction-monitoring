e386966acf2512a4acbaa6b3974efcdea0ab2cdb
Mark Schnitzius <mark.schnitzius@digital.cabinet-office.gov.uk>
2023-08-09 11:30:22 +0100

    Code review fixes

--- src/tools/batch-month.sh
+++ src/tools/batch-month.sh
@@ -5,7 +5,14 @@ if [[ -z "$2" ]]; then
   exit 1
 fi
 
-
+echo This script will back up a month\'s worth of single-event files in production, then combine these files
+echo into multi-event files that are interpreted equivelently by the Athena layer that reads them.  The overall
+echo result is the same event data set, but with much fewer files \(which provides significant speedup on most
+echo operations\).
+echo
+echo Any events which arrive for the month and year that this script is bucketing can be lost, so care should
+echo be taken not to run this script against a year and month that are too close to the present, or for which
+echo there might otherwise be event data being generated.
 echo
 echo "About to run:"
 echo aws s3 sync "s3://di-btm-production-storage/btm_event_data/$1/$2/" "s3://di-btm-production-storage/btm_event_data_copy/btm_event_data/$1/$2/"
