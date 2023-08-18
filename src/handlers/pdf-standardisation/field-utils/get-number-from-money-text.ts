67fdd12e75af7ae081547a7c5bc7667024c129a3
Mark Potter <si3rr4wow@gmail.com>
2023-08-18 15:54:20 +0100

    fix smell

--- src/handlers/pdf-standardisation/field-utils/get-number-from-money-text.ts
+++ src/handlers/pdf-standardisation/field-utils/get-number-from-money-text.ts
@@ -1,6 +1,6 @@
 export const getNumberFromMoneyText = (text: string): number => {
   // This'll be fine so long as we never receive a trillion pound invoice
-  const numberText = text.match(/^£? ?((?:\d{1,12},?)+\.\d\d+)(?: .*)?$/);
+  const numberText = /^£? ?((?:\d{1,12},?)+\.\d\d+)(?: .*)?$/.exec(text);
   if (numberText === null) throw new Error(`Unsupported money format: ${text}`);
   return Number(numberText[1].replace(",", ""));
 };
