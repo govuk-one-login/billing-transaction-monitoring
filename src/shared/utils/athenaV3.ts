ca8bb070e3a06f8748887149f48e261a984124fa
Mark Potter <si3rr4wow@gmail.com>
2023-08-18 11:58:00 +0100

    wait, not that

--- src/shared/utils/athenaV3.ts
+++ src/shared/utils/athenaV3.ts
@@ -6,7 +6,7 @@ import {
   StartQueryExecutionCommand,
 } from "@aws-sdk/client-athena";
 
-const INTERVAL_MS = 5000;
+const INTERVAL_MS = 1000;
 
 export class AthenaQueryExecutor {
   athena: AthenaClient;
