53f320bd54ccb70140aab1f948fbdc197db340e6
Mark Schnitzius <mark.schnitzius@digital.cabinet-office.gov.uk>
2023-08-30 09:17:07 +0100

    Small cleanups

--- src/handlers/synthetic-events/business-logic.ts
+++ src/handlers/synthetic-events/business-logic.ts
@@ -10,33 +10,32 @@ export const businessLogic: BusinessLogic<
   Env,
   never,
   CleanedEventBody
-> = async (_: unknown, { config, logger }: HandlerCtx<Env, any, any>) => {
-  logger.info("config", JSON.stringify(config));
-
+> = async (_: unknown, { config }: HandlerCtx<Env, any, any>) => {
   const syntheticEventsConfig = config[
     ConfigElements.syntheticEvents
   ] as ConfigSyntheticEventsRow[];
 
   const now = new Date();
+  const nowTime = now.getTime();
+  const nowFormatted = now.toLocaleString("en-gb");
   const events: CleanedEventBody[] = [];
 
   syntheticEventsConfig.forEach((configLine) => {
     if (
-      new Date(configLine.start_date).getTime() < now.getTime() &&
+      new Date(configLine.start_date).getTime() < nowTime &&
       (!configLine.end_date ||
-        new Date(configLine.end_date).getTime() > now.getTime()) &&
+        new Date(configLine.end_date).getTime() > nowTime) &&
       configLine.frequency === "monthly"
     ) {
       const event: CleanedEventBody = {
         vendor_id: configLine.vendor_id,
         event_id: crypto.randomUUID(),
         event_name: configLine.event_name,
-        timestamp: now.getTime(),
-        timestamp_formatted: now.toLocaleString("en-gb"),
+        timestamp: nowTime,
+        timestamp_formatted: nowFormatted,
         credits: configLine.quantity,
         component_id: configLine.component_id,
       };
-      logger.info("event", JSON.stringify(event));
       events.push(event);
     }
   });
