f66c05ea295789a675e0c8cee750f9a5270f3bef
gdsgj <110972007+gdsgj@users.noreply.github.com>
2023-09-08 14:17:46 +0100

    BTM-769 & BTM-797: CSV fixes (#452)

--- src/handlers/pdf-standardisation/field-utils/get-quantity.ts
+++ src/handlers/pdf-standardisation/field-utils/get-quantity.ts
@@ -1,12 +1,13 @@
 import { Textract } from "aws-sdk";
+import { getNumberFromWholeQuantityText } from "../../../shared/utils";
 import { getHighestConfidenceTextractValue } from "./get-highest-confidence-textract-value";
 
 export const getQuantity = (
   fields: Textract.ExpenseField[]
 ): number | undefined => {
-  const quantity = getHighestConfidenceTextractValue(fields, "QUANTITY")
-    ?.replace(/[.,]00?$/g, "")
-    .replace(/[.,]/g, "");
+  const quantity = getHighestConfidenceTextractValue(fields, "QUANTITY");
 
-  return quantity === undefined ? undefined : Number(quantity);
+  return quantity === undefined
+    ? undefined
+    : getNumberFromWholeQuantityText(quantity);
 };
