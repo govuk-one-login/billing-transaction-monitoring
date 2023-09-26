10068a9087ec6ee31f57a2672d34496e207a265e
NKannan14 <nithya.kannan@digital.cabinet-office.gov.uk>
2023-09-26 01:05:41 +0100

    updated cleanup script

--- integration_tests/cleanup-jest-html-reports.sh
+++ integration_tests/cleanup-jest-html-reports.sh
@@ -7,9 +7,11 @@ num_of_dir_to_keep=$1
 
 echo Keeping at max $num_of_dir_to_keep test-report directories
 
-ls -dt1 test-report* |
+ls
+
+ls -dt1 test-report-* |
   tail -n +$((num_of_dir_to_keep+1)) |
   xargs rm -rfv
 
 echo Remaining test-report directories:
-ls -dt1 test-report*
+ls -dt1 test-report-*
