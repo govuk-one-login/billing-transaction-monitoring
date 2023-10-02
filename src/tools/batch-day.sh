e84bace52fcb061fa6be43e4d55edbc41c349e24
Mark Potter <35689158+Si3rr4wow@users.noreply.github.com>
2023-10-02 14:13:14 +0100

    Add script to batch a single day (#485)
    * add script to batch a single day

--- src/tools/batch-day.sh
+++ src/tools/batch-day.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-if [[ -z "$2" ]]; then
-  echo "Please provide the YYYY and MM."
+if [[ -z "$3" ]]; then
+  echo "Please provide the YYYY, MM & DD"
   exit 1
 fi
 
-echo This script will back up a month\'s worth of single-event files in production, then combine these files
+echo This script will back up a days\'s worth of single-event files in production, then combine these files
 echo into multi-event files that are interpreted equivelently by the Athena layer that reads them.  The overall
 echo result is the same event data set, but with much fewer files \(which provides significant speedup on most
 echo operations\).
@@ -15,9 +15,9 @@ echo be taken not to run this script against a year and month that are too close
 echo there might otherwise be event data being generated.
 echo
 echo "About to run:"
-echo aws s3 sync "s3://di-btm-production-storage/btm_event_data/$1/$2/" "s3://di-btm-production-storage/btm_event_data_copy/btm_event_data/$1/$2/"
+echo aws s3 sync "s3://di-btm-production-storage/btm_event_data/$1/$2/$3" "s3://di-btm-production-storage/btm_event_data_copy/btm_event_data/$1/$2/$3"
 read -p 'Press Enter to proceed...' var
-aws s3 sync "s3://di-btm-production-storage/btm_event_data/$1/$2/" "s3://di-btm-production-storage/btm_event_data_copy/btm_event_data/$1/$2/"
+aws s3 sync "s3://di-btm-production-storage/btm_event_data/$1/$2/$3" "s3://di-btm-production-storage/btm_event_data_copy/btm_event_data/$1/$2/$3"
 
 mkdir -p downloaded-events
 echo
@@ -28,9 +28,9 @@ rm -rf downloaded-events/*
 
 echo
 echo "About to run:"
-echo aws s3 sync "s3://di-btm-production-storage/btm_event_data/$1/$2/" downloaded-events/$1/$2
+echo aws s3 sync "s3://di-btm-production-storage/btm_event_data/$1/$2/$3" downloaded-events/$1/$2/$3
 read -p 'Press Enter to proceed...' var
-aws s3 sync "s3://di-btm-production-storage/btm_event_data/$1/$2/" downloaded-events/$1/$2
+aws s3 sync "s3://di-btm-production-storage/btm_event_data/$1/$2/$3" downloaded-events/$1/$2/$3
 
 mkdir -p batched-events
 echo
@@ -46,12 +46,12 @@ read -p 'Press Enter to proceed...' var
 npm run batch-events
 
 echo
-echo "About to delete files in s3://di-btm-production-storage/btm_event_data/$1/$2"
+echo "About to delete files in s3://di-btm-production-storage/btm_event_data/$1/$2/$3"
 read -p 'Press Enter to proceed...' var
-aws s3 rm --recursive s3://di-btm-production-storage/btm_event_data/$1/$2
+aws s3 rm --recursive s3://di-btm-production-storage/btm_event_data/$1/$2/$3
 
 echo
 echo "About to run:"
-echo aws s3 sync batched-events/$1/$2 "s3://di-btm-production-storage/btm_event_data/$1/$2"
+echo aws s3 sync batched-events/$1/$2/$3 "s3://di-btm-production-storage/btm_event_data/$1/$2/$3"
 read -p 'Press Enter to proceed...' var
-aws s3 sync batched-events/$1/$2 "s3://di-btm-production-storage/btm_event_data/$1/$2"
+aws s3 sync batched-events/$1/$2/$3 "s3://di-btm-production-storage/btm_event_data/$1/$2/$3"
